6322c9343dc7f22b6ec848f3189aa3f08e1dc88a
Dimitri Stolnikov <horiz0n@gmx.net>
2012-04-01 16:52:41 +0200

    add preliminary cmake build system

---
+++ cmake/Modules/FindLibUSB.cmake
@@ -0,0 +1,28 @@
+if(NOT LIBUSB_FOUND)
+  pkg_check_modules (LIBUSB_PKG libusb-1.0)
+  find_path(LIBUSB_INCLUDE_DIR NAMES libusb.h
+    PATHS
+    ${LIBUSB_PKG_INCLUDE_DIRS}
+    /usr/include/libusb-1.0
+    /usr/include
+    /usr/local/include
+  )
+
+  find_library(LIBUSB_LIBRARIES NAMES usb-1.0
+    PATHS
+    ${LIBUSB_PKG_LIBRARY_DIRS}
+    /usr/lib
+    /usr/local/lib
+  )
+
+if(LIBUSB_INCLUDE_DIR AND LIBUSB_LIBRARIES)
+  set(LIBUSB_FOUND TRUE CACHE INTERNAL "libusb-1.0 found")
+  message(STATUS "Found libusb-1.0: ${LIBUSB_INCLUDE_DIR}, ${LIBUSB_LIBRARIES}")
+else(LIBUSB_INCLUDE_DIR AND LIBUSB_LIBRARIES)
+  set(LIBUSB_FOUND FALSE CACHE INTERNAL "libusb-1.0 found")
+  message(STATUS "libusb-1.0 not found.")
+endif(LIBUSB_INCLUDE_DIR AND LIBUSB_LIBRARIES)
+
+mark_as_advanced(LIBUSB_INCLUDE_DIR LIBUSB_LIBRARIES)
+
+endif(NOT LIBUSB_FOUND)
