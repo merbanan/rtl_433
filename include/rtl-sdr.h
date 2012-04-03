f13cf0e56c18c76c77417ae1004d5cfab0f1d210
Dimitri Stolnikov <horiz0n@gmx.net>
2012-04-03 19:50:03 +0200

    adjust project slogan
    Signed-off-by: Steve Markgraf <steve@steve-m.de>

--- include/rtl-sdr.h
+++ include/rtl-sdr.h
@@ -1,5 +1,5 @@
 /*
- * rtl-sdr, a poor man's SDR using a Realtek RTL2832 based DVB-stick
+ * rtl-sdr, turns your Realtek RTL2832 based DVB dongle into a SDR receiver
  * Copyright (C) 2012 by Steve Markgraf <steve@steve-m.de>
  * Copyright (C) 2012 by Dimitri Stolnikov <horiz0n@gmx.net>
  *
