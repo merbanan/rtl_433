0af094070f3aaf7f0394523025c61b0aaf25f2b6
Steve Markgraf <steve@steve-m.de>
2012-05-03 20:58:14 +0200

    add Compro Videomate U680F
    Thanks to Marek Kroemeke for submitting the patch.
    
    Signed-off-by: Steve Markgraf <steve@steve-m.de>

--- src/rtl-sdr.c
+++ src/rtl-sdr.c
@@ -171,6 +171,7 @@ static rtlsdr_dongle_t known_devices[] = {
 	{ 0x0ccd, 0x00e0, "Terratec NOXON DAB/DAB+ USB dongle (rev 2)" },
 	{ 0x185b, 0x0620, "Compro Videomate U620F"},
 	{ 0x185b, 0x0650, "Compro Videomate U650F"},
+	{ 0x185b, 0x0680, "Compro Videomate U680F"},
 	{ 0x1f4d, 0xb803, "GTek T803" },
 	{ 0x1f4d, 0xc803, "Lifeview LV5TDeluxe" },
 	{ 0x1b80, 0xd3a4, "Twintech UT-40" },
