54fe3eca01797a137957be5ba2f2a7f8b440cf71
Steve Markgraf <steve@steve-m.de>
2012-07-21 20:50:51 +0200

    don't print i2c read/write errors for now
    During autodetection of the tuner they are
    normal and may cause confusion.
    
    Signed-off-by: Steve Markgraf <steve@steve-m.de>

--- src/librtlsdr.c
+++ src/librtlsdr.c
@@ -279,10 +279,10 @@ int rtlsdr_read_array(rtlsdr_dev_t *dev, uint8_t block, uint16_t addr, uint8_t *
 	uint16_t index = (block << 8);
 
 	r = libusb_control_transfer(dev->devh, CTRL_IN, 0, addr, index, array, len, CTRL_TIMEOUT);
-
+#if 0
 	if (r < 0)
 		fprintf(stderr, "%s failed with %d\n", __FUNCTION__, r);
-
+#endif
 	return r;
 }
 
@@ -292,10 +292,10 @@ int rtlsdr_write_array(rtlsdr_dev_t *dev, uint8_t block, uint16_t addr, uint8_t 
 	uint16_t index = (block << 8) | 0x10;
 
 	r = libusb_control_transfer(dev->devh, CTRL_OUT, 0, addr, index, array, len, CTRL_TIMEOUT);
-
+#if 0
 	if (r < 0)
 		fprintf(stderr, "%s failed with %d\n", __FUNCTION__, r);
-
+#endif
 	return r;
 }
 
