9eae8b42218214808fc5dfaec0e4144b8a5a78e0
Helge Weissig <helgew@grajagan.org>
2015-01-13 16:08:02 -0800

    modularized device specifications

---
+++ include/rtl_433.h
@@ -0,0 +1,60 @@
+/*
+ * rtl_433.h
+ *
+ *  Created on: Jan 13, 2015
+ *      Author: helgew
+ */
+
+#ifndef INCLUDE_RTL_433_H_
+#define INCLUDE_RTL_433_H_
+
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifndef _WIN32
+#include <unistd.h>
+#else
+#include <Windows.h>
+#include <io.h>
+#include <fcntl.h>
+#include "getopt/getopt.h"
+#endif
+
+#define DEFAULT_SAMPLE_RATE     250000
+#define DEFAULT_FREQUENCY       433920000
+#define DEFAULT_HOP_TIME        (60*10)
+#define DEFAULT_HOP_EVENTS      2
+#define DEFAULT_ASYNC_BUF_NUMBER    32
+#define DEFAULT_BUF_LENGTH      (16 * 16384)
+#define DEFAULT_LEVEL_LIMIT     10000
+#define DEFAULT_DECIMATION_LEVEL 0
+#define MINIMAL_BUF_LENGTH      512
+#define MAXIMAL_BUF_LENGTH      (256 * 16384)
+#define FILTER_ORDER            1
+#define MAX_PROTOCOLS           10
+#define SIGNAL_GRABBER_BUFFER   (12 * DEFAULT_BUF_LENGTH)
+#define BITBUF_COLS             34
+#define BITBUF_ROWS             50
+
+/* Supported modulation types */
+#define     OOK_PWM_D   1   /* Pulses are of the same length, the distance varies */
+#define     OOK_PWM_P   2   /* The length of the pulses varies */
+
+typedef struct {
+    unsigned int    id;
+    char            name[256];
+    unsigned int    modulation;
+    unsigned int    short_limit;
+    unsigned int    long_limit;
+    unsigned int    reset_limit;
+    int     (*json_callback)(uint8_t bits_buffer[BITBUF_ROWS][BITBUF_COLS]) ;
+} r_device;
+
+static int debug_output = 0;
+int debug_callback(uint8_t buffer[BITBUF_ROWS][BITBUF_COLS]);
+
+#endif /* INCLUDE_RTL_433_H_ */
---
+++ src/devices/acurite.c
@@ -0,0 +1,138 @@
+#include "rtl_433.h"
+
+// ** Acurite 5n1 functions **
+
+const float acurite_winddirections[] =
+    { 337.5, 315.0, 292.5, 270.0, 247.5, 225.0, 202.5, 180,
+      157.5, 135.0, 112.5, 90.0, 67.5, 45.0, 22.5, 0.0 };
+
+static int acurite_raincounter = 0;
+
+static int acurite_crc(uint8_t row[BITBUF_COLS], int cols) {
+    // sum of first n-1 bytes modulo 256 should equal nth byte
+    int i;
+    int sum = 0;
+    for ( i=0; i < cols; i++)
+        sum += row[i];
+    if ( sum % 256 == row[cols] )
+        return 1;
+    else
+        return 0;
+}
+
+static int acurite_detect(uint8_t *pRow) {
+    int i;
+    if ( pRow[0] != 0x00 ) {
+        // invert bits due to wierd issue
+        for (i = 0; i < 8; i++)
+            pRow[i] = ~pRow[i] & 0xFF;
+        pRow[0] |= pRow[8];  // fix first byte that has mashed leading bit
+
+        if (acurite_crc(pRow, 7))
+            return 1;  // passes crc check
+    }
+    return 0;
+}
+
+static float acurite_getTemp (uint8_t highbyte, uint8_t lowbyte) {
+    // range -40 to 158 F
+    int highbits = (highbyte & 0x0F) << 7 ;
+    int lowbits = lowbyte & 0x7F;
+    int rawtemp = highbits | lowbits;
+    float temp = (rawtemp - 400) / 10.0;
+    return temp;
+}
+
+static int acurite_getWindSpeed (uint8_t highbyte, uint8_t lowbyte) {
+    // range: 0 to 159 kph
+    int highbits = ( highbyte & 0x1F) << 3;
+    int lowbits = ( lowbyte & 0x70 ) >> 4;
+    int speed = highbits | lowbits;
+    return speed;
+}
+
+static float acurite_getWindDirection (uint8_t byte) {
+    // 16 compass points, ccw from (NNW) to 15 (N)
+    int direction = byte & 0x0F;
+    return acurite_winddirections[direction];
+}
+
+static int acurite_getHumidity (uint8_t byte) {
+    // range: 1 to 99 %RH
+    int humidity = byte & 0x7F;
+    return humidity;
+}
+
+static int acurite_getRainfallCounter (uint8_t highbyte, uint8_t lowbyte) {
+    // range: 0 to 99.99 in, 0.01 in incr., rolling counter?
+    int highbits = (highbyte & 0x3F) << 7 ;
+    int lowbits = lowbyte & 0x7F;
+    int raincounter = highbits | lowbits;
+    return raincounter;
+}
+
+static int acurite5n1_callback(uint8_t bb[BITBUF_ROWS][BITBUF_COLS],int16_t bits_per_row[BITBUF_ROWS]) {
+    // acurite 5n1 weather sensor decoding for rtl_433
+    // Jens Jensen 2014
+    int i;
+    uint8_t *buf = NULL;
+    // run through rows til we find one with good crc (brute force)
+    for (i=0; i < BITBUF_ROWS; i++) {
+        if (acurite_detect(bb[i])) {
+            buf = bb[i];
+            break; // done
+        }
+    }
+
+    if (buf) {
+        // decode packet here
+        fprintf(stderr, "Detected Acurite 5n1 sensor, %d bits\n",bits_per_row[1]);
+        if (debug_output) {
+            for (i=0; i < 8; i++)
+                fprintf(stderr, "%02X ", buf[i]);
+            fprintf(stderr, "CRC OK\n");
+        }
+
+        if ((buf[2] & 0x0F) == 1) {
+            // wind speed, wind direction, rainfall
+
+            float rainfall = 0.00;
+            int raincounter = acurite_getRainfallCounter(buf[5], buf[6]);
+            if (acurite_raincounter > 0) {
+                // track rainfall difference after first run
+                rainfall = ( raincounter - acurite_raincounter ) * 0.01;
+            } else {
+                // capture starting counter
+                acurite_raincounter = raincounter;
+            }
+
+            fprintf(stderr, "wind speed: %d kph, ",
+                acurite_getWindSpeed(buf[3], buf[4]));
+            fprintf(stderr, "wind direction: %0.1f°, ",
+                acurite_getWindDirection(buf[4]));
+            fprintf(stderr, "rain gauge: %0.2f in.\n", rainfall);
+
+        } else if ((buf[2] & 0x0F) == 8) {
+            // wind speed, temp, RH
+            fprintf(stderr, "wind speed: %d kph, ",
+                acurite_getWindSpeed(buf[3], buf[4]));
+            fprintf(stderr, "temp: %2.1f° F, ",
+                acurite_getTemp(buf[4], buf[5]));
+            fprintf(stderr, "humidity: %d%% RH\n",
+                acurite_getHumidity(buf[6]));
+        }
+    }
+    //if (debug_output)
+    //   debug_callback(bb);
+    return 1;
+}
+
+r_device acurite5n1 = {
+    /* .id             = */ 10,
+    /* .name           = */ "Acurite 5n1 Weather Station",
+    /* .modulation     = */ OOK_PWM_P,
+    /* .short_limit    = */ 75,
+    /* .long_limit     = */ 220,
+    /* .reset_limit    = */ 20000,
+    /* .json_callback  = */ &acurite5n1_callback,
+};
