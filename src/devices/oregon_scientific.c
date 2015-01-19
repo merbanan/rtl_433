2a65728012f2f41d001fd237f22eda02d22ec8b1
deennoo <deennoo33@gmail.com>
2015-01-19 15:41:30 +0100

    Adding osv3 WGR800 WIND GAUGE
    Adding WGR 800 Wind Gauge
    
    Code based on Lostbyte arduino skecth
    
    Miss Average speed wind, i'm working on it

--- src/devices/oregon_scientific.c
+++ src/devices/oregon_scientific.c
@@ -248,6 +248,13 @@ static int oregon_scientific_v3_parser(uint8_t bb[BITBUF_ROWS][BITBUF_COLS], int
 		 fprintf(stderr,"Weather Sensor THGR810  Channel %d Temp: %3.1f¬∞C  %3.1f¬∞F   Humidity: %d%%\n", channel, temp_c, ((temp_c*9)/5)+32, humidity);
 	   }
 	   return 1;
+	else if ((msg[0] == 0x19) && (msg[1] == 0x84)) {
+	   if (validate_os_checksum(msg, 17) == 0) {
+	     float gustWindspeed = (msg[11]+msg[10])/100;
+             float quadrant = msg[8]*22.5;
+	   fprintf(stderr, "Weather Sensor WGR800   Wind Gauge  Gust Wind Speed : %2.0f m/s Wind direction %3.0f dgrs\n", gustWindspeed, quadrant);
+	   }
+	   return 1;
     } else if ((msg[0] != 0) && (msg[1]!= 0)) { //  sync nibble was found  and some data is present...
 fprintf(stderr, "Message received from unrecognized Oregon Scientific v3 sensor.\n");
 fprintf(stderr, "Message: "); for (i=0 ; i<BITBUF_COLS ; i++) fprintf(stderr, "%02x ", msg[i]); fprintf(stderr, "\n");
