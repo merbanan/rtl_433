f473e7f9e3c68a807eaba16ac977019b90afe887
johan <johan@E6410>
2015-07-02 03:07:07 +0200

    Changed Alecto output Corrected errors in rain data routine

--- src/devices/alecto.c
+++ src/devices/alecto.c
@@ -46,6 +46,7 @@
  *   F = Checksum
  *********************************************************************************************
  */
+#define LOCAL_TIME_BUFLEN	32
 uint8_t reverse8(uint8_t x) {
     x = (x & 0xF0) >> 4 | (x & 0x0F) << 4;
     x = (x & 0xCC) >> 2 | (x & 0x33) << 2;
@@ -63,6 +64,8 @@ static int alectov1_callback(uint8_t bb[BITBUF_ROWS][BITBUF_COLS], int16_t bits_
     int16_t temp;
     uint8_t humidity, csum = 0, csum2 = 0;
     int i;
+    time_t time_now;
+    char time_str[LOCAL_TIME_BUFLEN];
     if (bb[1][0] == bb[5][0] && bb[2][0] == bb[6][0] && (bb[1][4] & 0xf) == 0 && (bb[5][4] & 0xf) == 0
             && (bb[5][0] != 0 && bb[5][1] != 0)) {
 
@@ -81,7 +84,12 @@ static int alectov1_callback(uint8_t bb[BITBUF_ROWS][BITBUF_COLS], int16_t bits_
         csum2 = reverse8((csum2 & 0xf) << 4);
         /* Quit if checksup does not work out */
         if (csum != (bb[1][4] >> 4) || csum2 != (bb[5][4] >> 4)) {
-            fprintf(stdout, "\nAlectoV1 CRC error");
+            //fprintf(stdout, "\nAlectoV1 CRC error");
+            time(&time_now);
+            local_time_str(time_now, time_str);
+            fprintf(stdout,
+            "%s AlectoV1 Checksum/Parity error\n",
+            time_str);
             return 0;
         } //Invalid checksum
 
@@ -90,12 +98,20 @@ static int alectov1_callback(uint8_t bb[BITBUF_ROWS][BITBUF_COLS], int16_t bits_
 
         if ((bb[1][1] & 0xe0) == 0x60) {
             wind = ((bb[1][1] & 0xf) == 0xc) ? 0 : 1;
-
-            fprintf(stdout, "\nSensor        = %s event\n", wind ? "Wind" : "Rain gauge");
-            fprintf(stdout, "Protocol      = AlectoV1 bpr1: %d bpr2: %d\n", bits_per_row[1], bits_per_row[5]);
-            fprintf(stdout, "Device        = %d\n", reverse8(bb[1][0]));
-            fprintf(stdout, "Button        = %d\n", bb[1][1]&0x10 ? 1 : 0);
-            fprintf(stdout, "Battery       = %s\n", bb[1][1]&0x80 ? "Low" : "OK");
+            time(&time_now);
+            local_time_str(time_now, time_str);
+            time(&time_now);
+            local_time_str(time_now, time_str);
+            fprintf(stdout,
+            "%s AlectoV1 %s Sensor %d",
+            time_str,
+            wind ? "Wind" : "Rain",
+            reverse8(bb[1][0])
+            );
+            //left out data (not needed):
+            //bb[1][1]&0x10 ? "timed event":"Button generated ");
+            //fprintf(stdout, "Protocol      = AlectoV1 bpr1: %d bpr2: %d\n", bits_per_row[1], bits_per_row[5]);
+            //fprintf(stdout, "Button        = %d\n", bb[1][1]&0x10 ? 1 : 0);
             if (wind) {
                 int skip = -1;
                 /* Untested code written according to the specification, may not decode correctly  */
@@ -108,14 +124,18 @@ static int alectov1_callback(uint8_t bb[BITBUF_ROWS][BITBUF_COLS], int16_t bits_
                     double speed = reverse8(bb[1 + skip][3]);
                     double gust = reverse8(bb[5 + skip][3]);
                     int direction = (reverse8(bb[5 + skip][2]) << 1) | (bb[5 + skip][1] & 0x1);
-                    fprintf(stdout, "Wind speed    = %.0f units = %.2f m/s\n", speed, speed * 0.2);
-                    fprintf(stdout, "Wind gust     = %.0f units = %.2f m/s\n", gust, gust * 0.2);
-                    fprintf(stdout, "Direction     = %.2i degrees\n", direction);
+                    fprintf(stdout, ": Wind speed %.0f units = %.2f m/s", speed, speed * 0.2);
+                    fprintf(stdout, ": Wind gust %.0f units = %.2f m/s", gust, gust * 0.2);
+                    fprintf(stdout, ": Direction %.2i degrees", direction);
+                    fprintf(stdout, ": Battery %s\n", bb[1][1]&0x80 ? "Low" : "OK");
                 }
             } else {
-                /* Untested code written according to the specification, may not decode correctly  */
-                double rain_mm = (reverse8(bb[1][2]) + (reverse8(bb[1][3] << 8))) * 0.25;
-                fprintf(stdout, "Rainfall      = %f\n", rain_mm);
+                
+                double rain_mm = ((reverse8(bb[1][3]) << 8)+reverse8(bb[1][2])) * 0.25;
+                unsigned int rain1=0;
+                rain1 = (reverse8(bb[1][3]) << 8)+reverse8(bb[1][2]);
+                fprintf(stdout, ": Rain %.02f mm/m2", rain_mm);
+                fprintf(stdout, ": Battery %s\n", bb[1][1]&0x80 ? "Low" : "OK");
             }
         } else if (bb[2][0] == bb[3][0] && bb[3][0] == bb[4][0] && bb[4][0] == bb[5][0] &&
                 bb[5][0] == bb[6][0] && (bb[3][4] & 0xf) == 0 && (bb[5][4] & 0xf) == 0) {
@@ -127,25 +147,28 @@ static int alectov1_callback(uint8_t bb[BITBUF_ROWS][BITBUF_COLS], int16_t bits_
             temperature_before_dec = abs(temp / 10);
             temperature_after_dec = abs(temp % 10);
             humidity = bcd_decode8(reverse8(bb[1][3]));
-            fprintf(stdout, "\nSensor        = Temperature event\n");
-            fprintf(stdout, "Protocol      = AlectoV1 bpr1: %d bpr2: %d\n", bits_per_row[1], bits_per_row[5]);
-            fprintf(stdout, "Device        = %d\n", reverse8(bb[1][0]));
-            fprintf(stdout, "Channel       = %d\n", (bb[1][0] & 0xc) >> 2);
-            fprintf(stdout, "Button        = %d\n", bb[1][1]&0x10 ? 1 : 0);
-            fprintf(stdout, "Battery       = %s\n", bb[1][1]&0x80 ? "Low" : "OK");
-            fprintf(stdout, "Temp          = %s%d.%d\n", temp < 0 ? "-" : "", temperature_before_dec, temperature_after_dec);
-            fprintf(stdout, "Humidity      = %d\n", humidity);
-        }
-        fprintf(stdout, "Checksum      = %01x (calculated %01x)\n", bb[1][4] >> 4, csum);
-
-        fprintf(stdout, "Received Data = %02x %02x %02x %02x %02x\n", bb[1][0], bb[1][1], bb[1][2], bb[1][3], bb[1][4]);
-        if (wind) fprintf(stdout, "Rcvd Data 2   = %02x %02x %02x %02x %02x\n", bb[5][0], bb[5][1], bb[5][2], bb[5][3], bb[5][4]);
-        /*
+            time(&time_now);
+            local_time_str(time_now, time_str);
+            fprintf(stdout,
+            "%s AlectoV1 Sensor %d Channel %d",
+            time_str,
+            reverse8(bb[1][0]),
+            (bb[1][0] & 0xc) >> 2
+            );
+            fprintf(stdout, ": Temperature %s%d.%d C", temp < 0 ? "-" : "", temperature_before_dec, temperature_after_dec);
+            fprintf(stdout, ": Humidity %d %", humidity);
+            fprintf(stdout, ": Battery %s\n", bb[1][1]&0x80 ? "Low" : "OK");
+            
+        }        
+        if (debug_output){
+           fprintf(stdout, "Checksum      = %01x (calculated %01x)\n", bb[1][4] >> 4, csum);
+           fprintf(stdout, "Received Data = %02x %02x %02x %02x %02x\n", bb[1][0], bb[1][1], bb[1][2], bb[1][3], bb[1][4]);
+           if (wind) fprintf(stdout, "Rcvd Data 2   = %02x %02x %02x %02x %02x\n", bb[5][0], bb[5][1], bb[5][2], bb[5][3], bb[5][4]);
+         /*
          * fprintf(stdout, "L2M: %02x %02x %02x %02x %02x\n",reverse8(bb[1][0]),reverse8(bb[1][1]),reverse8(bb[1][2]),reverse8(bb[1][3]),reverse8(bb[1][4]));
          */
-        if (debug_output)
             debug_callback(bb, bits_per_row);
-
+        }
         return 1;
     }
     return 0;
@@ -154,10 +177,11 @@ static int alectov1_callback(uint8_t bb[BITBUF_ROWS][BITBUF_COLS], int16_t bits_
 //Timing based on 250000
 r_device alectov1 = {
     /* .id             = */ 11,
-    /* .name           = */ "AlectoV1 Weather Sensor",
+    /* .name           = */ "AlectoV1 Weather Sensor (Alecto WS3500 WS4500 Ventus W155/W044 Oregon)",
     /* .modulation     = */ OOK_PWM_D,
     /* .short_limit    = */ 3500 / 4, //875
     /* .long_limit     = */ 7000 / 4, //1750
     /* .reset_limit    = */ 15000 / 4, //3750
     /* .json_callback  = */ &alectov1_callback,
+    /* .Disable        = */ 0,
 };
