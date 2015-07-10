c0beea4ad31024a173622f37d4fc3c6ac00a91d8
Tommy Vestermark <tov@vestermark.dk>
2015-07-10 21:29:01 +0200

    Make bitbuffer even less noisy
    Some decoders with large .short_limit may add a lot of bits...

--- src/bitbuffer.c
+++ src/bitbuffer.c
@@ -34,7 +34,7 @@ void bitbuffer_add_bit(bitbuffer_t *bits, int bit) {
 		bits->bits_per_row[bits->row_index]++;
 	}
 	else {
-		fprintf(stderr, "ERROR: bitbuffer:: Could not add more columns\n");
+//		fprintf(stderr, "ERROR: bitbuffer:: Could not add more columns\n");	// Some decoders may add many columns...
 	}
 }
 
