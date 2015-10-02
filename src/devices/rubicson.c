bcfa2628b86760462328616a558493e28a3692af
Benjamin Larsson <benjamin@southpole.se>
2015-10-03 00:52:21 +0200

    rubicson: implement crc and other parameters

--- src/devices/rubicson.c
+++ src/devices/rubicson.c
@@ -4,26 +4,70 @@
  *
  * the sensor sends 36 bits 12 times pwm modulated
  * the data is grouped into 9 nibles
- * [id0] [id1], [unk0] [temp0], [temp1] [temp2], [unk1] [unk2], [unk3]
+ * [id0] [id1], [bat|unk1|chan1|chan2] [temp0], [temp1] [temp2], [F] [crc1], [crc2]
  *
  * The id changes when the battery is changed in the sensor.
- * unk0 is always 1 0 0 0, most likely 2 channel bits as the sensor can recevice 3 channels
- * unk1-3 changes and the meaning is unknown
+ * bat bit is 1 if battery is ok, 0 if battery is low
+ * chan1 and chan2 forms a 2bit value for the used channel
+ * unk1 is always 0 probably unused
  * temp is 12 bit signed scaled by 10
+ * F is always 0xf
+ * crc1 and crc2 forms a 8-bit crc
  *
  * The sensor can be bought at Kjell&Co
  */
 
+
+/* Working routine for checking the crc, lots of magic but it works */
+
+//static uint8_t rp[] = {0xb8, 0x80, 0xea, 0xfe, 0x80};
+static uint8_t rp[] = {0xea, 0x8f, 0x6a, 0xfa, 0x50};
+
+int rubicson_crc_check(bitrow_t *bb) {
+    uint8_t crc, w;
+    uint8_t diff[9];
+    int i, ret;
+
+    // diff against ref packet
+
+    diff[0] = rp[0]^bb[1][0];
+    diff[1] = rp[1]^bb[1][1];
+    diff[2] = rp[2]^bb[1][2];
+    diff[3] = rp[3]^bb[1][3];
+    diff[4] = rp[4]^bb[1][4];
+
+//    fprintf(stdout, "%02x %02x %02x %02x %02x\n",rp[0],rp[1],rp[2],rp[3],rp[4]);
+//    fprintf(stdout, "%02x %02x %02x %02x %02x\n",bb[1][0],bb[1][1],bb[1][2],bb[1][3],bb[1][4]);
+//    fprintf(stdout, "%02x %02x %02x %02x %02x\n",diff[0],diff[1],diff[2],diff[3],diff[4]);
+
+    for (crc = 0, w = 0xf1, i = 0; i<7 ; i++){
+        uint8_t c = diff[i/2];
+        unsigned digit = (i&1) ? c&0xF : (c&0xF0)>>4;
+        unsigned j;
+        for (j=4; j-->0; ) {
+            if ((digit >> j) & 1)
+                crc ^= w;
+            w = (w >> 1) ^ ((w & 1) ? 0x98: 0);
+        }
+    }
+    if (crc == (((diff[3]<<4)&0xF0) | (diff[4]>>4)))
+//      printf ("\ncrc ok: %x\n", crc);
+        ret = 1;
+    else
+//      printf ("\ncrc fail: %x\n", crc);
+        ret = 0;
+
+	return ret;
+};
+
 static int rubicson_callback(bitbuffer_t *bitbuffer) {
     bitrow_t *bb = bitbuffer->bb;
     int temperature_before_dec;
-    int temperature_after_dec;
+    int temperature_after_dec, i;
     int16_t temp;
+    int8_t rh, csum, csum_calc, sum=0;
 
-    /* FIXME validate the received message better, figure out crc */
-    if (bb[1][0] == bb[2][0] && bb[2][0] == bb[3][0] && bb[3][0] == bb[4][0] &&
-        bb[4][0] == bb[5][0] && bb[5][0] == bb[6][0] && bb[6][0] == bb[7][0] && bb[7][0] == bb[8][0] &&
-        bb[8][0] == bb[9][0] && (bb[5][0] != 0 && /*bb[5][1] != 0 &&*/ bb[5][2] != 0)) {
+    if (rubicson_crc_check(bb)) {
 
         /* Nible 3,4,5 contains 12 bits of temperature
          * The temerature is signed and scaled by 10 */
@@ -34,10 +78,13 @@ static int rubicson_callback(bitbuffer_t *bitbuffer) {
         temperature_after_dec = abs(temp % 10);
 
         fprintf(stdout, "Sensor temperature event:\n");
-        fprintf(stdout, "protocol       = Rubicson/Auriol, %d bits\n",bitbuffer->bits_per_row[1]);
-        fprintf(stdout, "rid            = %x\n",bb[0][0]);
+        fprintf(stdout, "protocol       = Rubicson\n");
+        fprintf(stdout, "battery        = %s\n",(bb[0][1]&0x80) ? "ok" : "low");
+        fprintf(stdout, "channel        = %d\n",((bb[0][1]&0x30)>>4)+1);
+        fprintf(stdout, "rid            = %02x\n",bb[0][0]);
         fprintf(stdout, "temp           = %s%d.%d\n",temp<0?"-":"",temperature_before_dec, temperature_after_dec);
-        fprintf(stdout, "%02x %02x %02x %02x %02x\n",bb[1][0],bb[0][1],bb[0][2],bb[0][3],bb[0][4]);
+        fprintf(stdout, "crc            = ok\n");
+//        fprintf(stdout, "%02x %02x %02x %02x %02x = %s%d.%d\n",bb[1][0],bb[0][1],bb[0][2],bb[0][3],bb[0][4],temp<0?"-":"",temperature_before_dec, temperature_after_dec);
 
         return 1;
     }
