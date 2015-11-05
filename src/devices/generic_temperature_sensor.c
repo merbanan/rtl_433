a54167d458b25c0d7e77e97cc479e22636d93f46
AlexandreCo <alexandre.github@gmail.com>
2015-11-05 20:49:37 +0100

    rename r_device->name from "RF Wireless Temp Transmitter" to "Generic temperature sensor 1"

--- src/devices/generic_temperature_sensor.c
+++ src/devices/generic_temperature_sensor.c
@@ -34,7 +34,7 @@ static int generic_temperature_sensor_callback(bitbuffer_t *bitbuffer) {
 	fprintf(stdout, "Device        = %d\n", device);
 	fprintf(stdout, "Battery?      = %02X\n", battery);
 	fprintf(stdout, "Temp          = %f\n",fTemp);
-	fprintf(stdout, "Model         = RF Wireless Transmitter\n");
+	fprintf(stdout, "Model         = Generic temperature sensor 1\n");
 	fprintf(stdout, "Received Data = %02x %02x %02x\n", bb[1][0], bb[1][1], bb[1][2]);
 
 
@@ -44,7 +44,7 @@ static int generic_temperature_sensor_callback(bitbuffer_t *bitbuffer) {
 
 r_device generic_temperature_sensor = {
 
-  .name          = "RF Wireless Temp Transmitter",
+  .name          = "Generic temperature sensor 1",
   .modulation    = OOK_PWM_D,
   .short_limit   = 875,
   .long_limit    = 1200,
