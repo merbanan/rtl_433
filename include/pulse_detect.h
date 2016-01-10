937e85c9240b11f54a7531143ea4532660e4167d
Tommy Vestermark <tov@vestermark.dk>
2016-01-10 23:33:07 +0100

    Pulse_detect: Increase maximum number of pulses
    The Ascot files have 1104 pulses...

--- include/pulse_detect.h
+++ include/pulse_detect.h
@@ -13,7 +13,7 @@
 
 #include <stdint.h>
 
-#define PD_MAX_PULSES 1000			// Maximum number of pulses before forcing End Of Package
+#define PD_MAX_PULSES 1200			// Maximum number of pulses before forcing End Of Package
 #define PD_MIN_PULSES 16			// Minimum number of pulses before declaring a proper package
 #define PD_MIN_PULSE_SAMPLES 10		// Minimum number of samples in a pulse for proper detection
 #define PD_MIN_GAP_MS 10			// Minimum gap size in milliseconds to exceed to declare End Of Package
