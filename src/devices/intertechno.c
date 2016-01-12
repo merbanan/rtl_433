0d26675454ba994e529cc2ade4be3f00b789ccb5
Tommy Vestermark <tov@vestermark.dk>
2016-01-12 21:17:07 +0100

    Convert Intertechno to Pulse Decoder
    No test files!
    Small risk due to changing reset limit below 10ms min gap

--- src/devices/intertechno.c
+++ src/devices/intertechno.c
@@ -27,10 +27,10 @@ static int intertechno_callback(bitbuffer_t *bitbuffer) {
 
 r_device intertechno = {
     .name           = "Intertechno 433",
-    .modulation     = OOK_PWM_D,
+    .modulation     = OOK_PULSE_PPM_RAW,
     .short_limit    = 100,
     .long_limit     = 350,
-    .reset_limit    = 3000,
+    .reset_limit    = 2500,
     .json_callback  = &intertechno_callback,
     .disabled       = 0,
     .demod_arg      = 0,
