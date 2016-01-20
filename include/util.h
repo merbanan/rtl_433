0df00e0fa47d8ce08ba4cf0c743eccf89a5e9416
David Woodhouse <David.Woodhouse@intel.com>
2016-01-20 11:49:10 +0000

    Add crc16() function

--- include/util.h
+++ include/util.h
@@ -46,6 +46,14 @@ uint8_t crc8(uint8_t const message[], unsigned nBytes, uint8_t polynomial, uint8
 /// @return CRC value
 uint8_t crc8le(uint8_t const message[], unsigned nBytes, uint8_t polynomial, uint8_t init);
 
+/// CRC-16
+///
+/// @param message[]: array of bytes to check
+/// @param nBytes: number of bytes in message
+/// @param polynomial: CRC polynomial
+/// @param init: starting crc value
+/// @return CRC value
+uint16_t crc16(uint8_t const message[], unsigned nBytes, uint16_t polynomial, uint16_t init);
 
 /// compute bit parity of a single byte
 ///
