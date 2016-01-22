32aaece877cac3709c9db0d56d0058e96d7a6524
David Woodhouse <David.Woodhouse@intel.com>
2016-01-22 10:54:45 +0000

    valeo: Fix precision of bit limits
    It said "probably 26.5", so let's see if it was right, now that it's actually
    possible to ask for that.

--- src/devices/valeo.c
+++ src/devices/valeo.c
@@ -34,7 +34,7 @@ static int valeo_callback(bitbuffer_t *bitbuffer) {
 r_device valeo = {
 	.name			= "Valeo Car Key",
 	.modulation		= OOK_PULSE_MANCHESTER_ZEROBIT,
-	.short_limit	= 104,  // Probably 106 (26.5*4)
+	.short_limit	= 106,
 	.long_limit		= 0,	// Not used
 	.reset_limit	= 400,
 	.json_callback	= &valeo_callback,
