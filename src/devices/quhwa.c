8c0159c22e713b7e11d53e9e5f678d7c18c7a134
kajksa <ask@macbookpro.local>
2016-03-04 21:34:37 +0100

    Improve text

--- src/devices/quhwa.c
+++ src/devices/quhwa.c
@@ -1,15 +1,9 @@
-/* Chuango Security Technology Corporation
+/* Quhwa
  *
  * Tested devices:
- * G5 GSM/SMS/RFID Touch Alarm System (Alarm, Disarm, ...)
- * DWC-100 Door sensor (Default: Normal Zone)
- * DWC-102 Door sensor (Default: Normal Zone)
- * KP-700 Wireless Keypad (Arm, Disarm, Home Mode, Alarm!)
- * PIR-900 PIR sensor (Default: Home Mode Zone)
- * RC-80 Remote Control (Arm, Disarm, Home Mode, Alarm!)
- * SMK-500 Smoke sensor (Default: 24H Zone)
+ * QH-C-CE-3V (which should be compatible with QH-832AC)
  *
- * Copyright (C) 2015 Tommy Vestermark
+ * Copyright (C) 2016 Ask Jakobsen
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -26,21 +20,13 @@ static int quhwa_callback(bitbuffer_t *bitbuffer) {
 	b[1] = ~b[1];
 	b[2] = ~b[2];
 	
-
 	unsigned bits = bitbuffer->bits_per_row[0];
-
-
-	/* 61 bb c0 */ /* http://goughlui.com/2013/12/20/rtl-sdr-433-92mhz-askook-decoding-of-various-devices-with-rtl_433/ */
-	/* e9 3b c0 */
-	/* 76 0b c0 */
-	/* b8 03 c0 */ /* MY! */
-	
 
 	if ((bits == 18) && (b[2]==0xFF)  && (b[1] & 1) && (b[1] & 2))
 	  {
 	    uint32_t ID = (b[0] << 6) | (b[1] >> 2);
 	    
-	    fprintf(stdout, "Quhwa doorbell\n");
+	    fprintf(stdout, "Quhwa\n");
 	    fprintf(stdout, "ID 14bit = 0x%04X\n", ID);
 	    return 1;
 	}
