6d6a822d5389677bacc49e37f750ef5b3568bc3d
Benjamin Larsson <benjamin@southpole.se>
2016-07-07 23:58:36 +0200

    Fix json output and minor rearrangement of code

--- src/devices/steelmate.c
+++ src/devices/steelmate.c
@@ -7,11 +7,11 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Packet payload: 9 bytes.
  * Bytes 2 to 9 are inverted Manchester with swaped MSB/LSB:
- * 
- *                               0  1  2  3  4  5  6  7  8 
+ *
+ *                               0  1  2  3  4  5  6  7  8
  *                    [00] {72} 00 00 7f 3c f0 d7 ad 8e fa
  * After translating            00 00 01 c3 f0 14 4a 8e a0
  *                              SS SS AA II II PP TT BB CC
@@ -22,9 +22,9 @@
  * T = Temperature in Fahrenheit, 0x4a = 74 'F
  * B = Battery as half the millivolt, 0x8e = 2.84 V
  * C = Checksum, adding bytes 2 to 7 modulo 256 = byte 8,(0x01+0xc3+0xf0+0x14+0x4a+0x8e) modulus 256 = 0xa0
- */ 
+ */
 
- 
+
 #include "rtl_433.h"
 #include "pulse_demod.h"
 #include "util.h"
@@ -43,6 +43,13 @@ static int steelmate_callback(bitbuffer_t *bitbuffer) {
 	//Loop through each row of data
 	for (int i = 0; i < bitbuffer->num_rows; i++)
 	{
+		//Payload is inverted Manchester encoded, and reversed MSB/LSB order
+		uint8_t preAmble, ID1, ID2, p1, tempFahrenheit, tmpbattery_mV, payload_checksum, calculated_checksum;
+		uint16_t sensorID, battery_mV;
+		float pressurePSI;
+		char sensorIDhex[7];
+		data_t *data;
+
 		//Length must be 72 bits to be considered a valid packet
 		if (bitbuffer->bits_per_row[i] != 72)
 			continue;
@@ -51,44 +58,39 @@ static int steelmate_callback(bitbuffer_t *bitbuffer) {
 		if (bb[i][0] != 0x00 || bb[i][1] != 0x00 || bb[i][2] != 0x7f)
 			continue;
 
-		//Payload is inverted Manchester encoded, and reversed MSB/LSB order
-		uint8_t preAmble, ID1, ID2, p1, tempFahrenheit, tmpbattery_mV, payload_checksum, calculated_checksum;
-		uint16_t sensorID, battery_mV;
-		float pressurePSI;
-		char sensorIDhex[7];
-
 		//Preamble
 		preAmble = ~reverse8(bb[i][2]);
 
 		//Sensor ID
 		ID1 = ~reverse8(bb[i][3]);
 		ID2 = ~reverse8(bb[i][4]);
-		sensorID = (ID1 << 8) + ID2;
-		sprintf(sensorIDhex, "0x%04x", sensorID);
-		
+
 		//Pressure is stored as twice the PSI
 		p1 = ~reverse8(bb[i][5]);
-		pressurePSI = (float)p1 / 2;
 
 		//Temperature is stored in Fahrenheit. Note that the datasheet claims operational to -40'C, but can only express values from -17.8'C
 		tempFahrenheit = ~reverse8(bb[i][6]);
 
 		//Battery voltage is stored as half the mV
 		tmpbattery_mV = ~reverse8(bb[i][7]);
-		battery_mV = tmpbattery_mV * 2;
 
 		//Checksum is a sum of all the other values
 		payload_checksum = ~reverse8(bb[i][8]);
 		calculated_checksum = preAmble + ID1 + ID2 + p1 + tempFahrenheit + tmpbattery_mV;
 		if (payload_checksum != calculated_checksum)
 			continue;
-		
-		data_t *data = data_make("time", "", DATA_STRING, time_str,
+
+		sensorID = (ID1 << 8) + ID2;
+		sprintf(sensorIDhex, "0x%04x", sensorID);
+		pressurePSI = (float)p1 / 2;
+		battery_mV = tmpbattery_mV * 2;
+
+		data = data_make("time", "", DATA_STRING, time_str,
 			"type", "", DATA_STRING, "TPMS",
 			"make", "", DATA_STRING, "Steelmate",
 			"id", "", DATA_STRING, sensorIDhex,
 			"pressure_PSI", "", DATA_DOUBLE, pressurePSI,
-			"temperature_F", "", DATA_DOUBLE, tempFahrenheit,
+			"temperature_F", "", DATA_DOUBLE, (float)tempFahrenheit,
 			"battery_mV", "", DATA_INT, battery_mV,
 			"checksum", "", DATA_STRING, "OK",
 			NULL);
