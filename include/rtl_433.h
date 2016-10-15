9ae96bd74991510c016a8319e82503c3ba4f70db
adam1010 <adam1010>
2016-10-15 23:00:06 +0000

    Honeywell/Ademco Door/Window Security Sensor driver

--- include/rtl_433.h
+++ include/rtl_433.h
@@ -32,17 +32,10 @@
 #define DEFAULT_HOP_EVENTS      2
 #define DEFAULT_ASYNC_BUF_NUMBER    32
 #define DEFAULT_BUF_LENGTH      (16 * 16384)
-
-/*
- * Theoretical high level at I/Q saturation is 128x128 = 16384 (above is ripple)
- * 0 = automatic adaptive level limit, else fixed level limit
- * 8000 = previous fixed default
- */
-#define DEFAULT_LEVEL_LIMIT     0
-
+#define DEFAULT_LEVEL_LIMIT     8000		// Theoretical high level at I/Q saturation is 128x128 = 16384 (above is ripple)
 #define MINIMAL_BUF_LENGTH      512
 #define MAXIMAL_BUF_LENGTH      (256 * 16384)
-#define MAX_PROTOCOLS           68
+#define MAX_PROTOCOLS           69
 #define SIGNAL_GRABBER_BUFFER   (12 * DEFAULT_BUF_LENGTH)
 
 /* Supported modulation types */
---
+++ src/devices/honeywell.c
@@ -0,0 +1,114 @@
+/*
+* *** Honeywell (Ademco) Door/Window Sensors ***
+*
+* Tested with the Honeywell 5811 Wireless Door/Window transmitters
+*
+*/
+
+#include "rtl_433.h"
+#include "pulse_demod.h"
+#include "util.h"
+
+uint16_t crc16_buypass(const uint8_t *data){
+  uint16_t crc = 0xfffe;
+  int i;
+  size_t j;
+
+  for(j=0; j<6; j++){
+    crc ^= ((uint16_t) (data[j])) << 8;
+    
+    for(i=0; i<8; i++){
+      if( (crc & 0x8000) ){
+        crc <<= 1;
+        crc ^= 0x8005;
+      } else
+        crc <<= 1;
+    }
+  }
+
+  return (crc);
+}
+
+static int honeywell_callback(bitbuffer_t *bitbuffer) {
+  if(bitbuffer->num_rows == 0)
+    return 0;
+  
+  int good = 0;
+  char time_str[LOCAL_TIME_BUFLEN];
+        local_time_str(0, time_str);
+
+  const uint8_t *bb;
+  if(bitbuffer->num_rows == 1 && bitbuffer->bits_per_row[0] == 64){
+    good = 1;
+    for(uint16_t i=0; i < 8; i++)
+      bitbuffer->bb[0][i] = ~bitbuffer->bb[0][i];
+
+    bb = bitbuffer->bb[0];
+  }
+
+  if(good){
+    char device_id[6]; 
+    char hex[2];
+    sprintf(hex, "%02x", bb[2]);
+    device_id[0] = hex[1];
+    sprintf(hex, "%02x", bb[3]);
+    device_id[1] = hex[0];
+    device_id[2] = hex[1];
+    sprintf(hex, "%02x", bb[4]);
+    device_id[3] = hex[0];
+    device_id[4] = hex[1];
+    device_id[5] = '\0';
+
+    // Raw Binary
+    char binary [65];
+    for (uint16_t bit = 0; bit < 64; ++bit) {
+        if (bb[bit/8] & (0x80 >> (bit % 8))) {
+                binary[bit] = '1';
+        } else {
+                binary[bit] = '0';
+        }
+    }
+    binary[64] = '\0';
+
+    int heartbeat = 0;
+    uint16_t crc_calculated = crc16_buypass(bb);
+    uint16_t crc = (((uint16_t) bb[6]) << 8) + ((uint16_t) bb[7]);
+
+    data_t *data = data_make("time",     "", DATA_STRING, time_str,
+                     "time_unix","", DATA_INT, time(NULL),
+                     "id",       "", DATA_STRING, device_id,
+                     "state",    "", DATA_STRING, ( (bb[5] & 0x80) == 0x00)? "closed":"open",
+                     "checksum", "", DATA_STRING, crc == crc_calculated? "ok":"bad",
+                     "heartbeat" , "", DATA_STRING, ( (bb[5] & 0x04) == 0x04)? "yes" : "no",
+                     "binary",   "", DATA_STRING, binary,
+                            NULL);
+
+    data_acquired_handler(data);
+    return 1;
+  }
+
+  return 0; // Unrecognized data
+}
+
+static char *output_fields[] = {
+        "time",
+        "time_unix",
+        "id",
+        "state",
+        "checksum",
+        "heartbeat",
+        "binary",
+        NULL
+};
+
+r_device honeywell = {
+        .name                   = "Honeywell Door/Window Sensor",
+        .modulation             = OOK_PULSE_MANCHESTER_ZEROBIT,
+        .short_limit    =   39 * 4,
+        .long_limit             = 0,
+        .reset_limit    = 73 * 4,
+        .json_callback  = &honeywell_callback,
+        .disabled               = 0,
+        .demod_arg              = 0,
+        .fields                 = output_fields,
+};
