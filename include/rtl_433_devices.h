cbd8749b5f1abddafe2c8970b5767d60074cf887
gismo2004 <reumuellermarkus@gmail.com>
2016-10-19 17:59:55 +0200

    derp

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -65,14 +65,14 @@
 		DECL(schraeder) \
 		DECL(lightwave_rf) \
 		DECL(elro_db286a) \
-		DECL(efergy_optical) \
-		DECL(hondaremote) \
+                DECL(efergy_optical) \
+                DECL(hondaremote) \
 		DECL(template) \
 		DECL(fineoffset_XC0400) \
 		DECL(radiohead_ask) \
 		DECL(kerui) \
 		DECL(fineoffset_wh1050) \
-		DECL(honeywell)\
+		DECL(honeywell) \
 		DECL(maverick_et73x)
 
 
