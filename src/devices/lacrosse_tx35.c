024985f73bd7bfb5364c926c3b0f5f26eef10b9c
unknown <U402471@B957313.inetpsa.com>
2017-01-30 16:32:59 +0100

    Finalization

--- src/devices/lacrosse_tx35.c
+++ src/devices/lacrosse_tx35.c
@@ -41,7 +41,8 @@ one byte as the preamble. I own 3 sensors TX29, and two of them send a long prea
 So this decoder synchronize on the 0xaa 0x2d 0xd4 preamble, so many 0xaa can occurs before.
 Also, I added 0x9 in the preamble (the weather data length), because this decoder only handle 
 this type of message.
-TX29 and TX35 sahre the same protocol, but pulse are different length.
+TX29 and TX35 sahre the same protocol, but pulse are different length, thus this decoder
+handle the two signal and we use two r_device struct (only differing by the pulse width)
 
 How to make a decoder : https://enavarro.me/ajouter-un-decodeur-ask-a-rtl_433.html
 */
@@ -54,22 +55,25 @@ How to make a decoder : https://enavarro.me/ajouter-un-decodeur-ask-a-rtl_433.ht
 #define LACROSSE_TX35_CRC_POLY       0x31
 #define LACROSSE_TX35_CRC_INIT       0x00
 
-
+/**
+ ** Generic decoder for LaCrosse "IT+" (instant transmission) protocol
+ ** Param device29or35 contain "29" or "35" depending of the device.
+ **/
 static int lacrosse_it(bitbuffer_t *bitbuffer, uint8_t device29or35) {
 	char time_str[LOCAL_TIME_BUFLEN];
 	uint8_t *bb;
-    uint16_t brow, row_nbytes;
+	uint16_t brow, row_nbytes;
 	uint8_t msg_type, r_crc, c_crc;
 	int valid = 0;
 	data_t *data;
 	int events = 0;	
 	
 	static const uint8_t preamble[] = {
-      0xaa, // preamble
-      0x2d, // brand identifer
-      0xd4, // brand identifier
+	  0xaa, // preamble
+	  0x2d, // brand identifer
+	  0xd4, // brand identifier
 	  0x90, // data length (this decoder work only with data length of 9, so we hardcode it on the preamble)
-    };
+	};
 	
 	uint8_t out[8] = {0}; // array of byte to decode
 	local_time_str(0, time_str);
@@ -81,7 +85,7 @@ static int lacrosse_it(bitbuffer_t *bitbuffer, uint8_t device29or35) {
 		if(start_pos == bitbuffer->bits_per_row[brow])
 			continue; // no preamble detected, move to the next row
 		if (debug_output >= 1)
-			fprintf(stderr, "LaCrosse TX29 detected, buffer is %d bits length\n", bitbuffer->bits_per_row[brow]);
+			fprintf(stderr, "LaCrosse TX29/35 detected, buffer is %d bits length, device is TX%d\n", bitbuffer->bits_per_row[brow], device29or35);
 		// remove preamble and keep only 64 bits
 		bitbuffer_extract_bytes(bitbuffer, brow, start_pos, out, 64);
 
@@ -96,16 +100,16 @@ static int lacrosse_it(bitbuffer_t *bitbuffer, uint8_t device29or35) {
 		if (r_crc != c_crc) {
 			// example debugging output
 			if (debug_output >= 1)
-				fprintf(stderr, "%s LaCrosseTX35 bad CRC: calculated %02x, received %02x\n",
+				fprintf(stderr, "%s LaCrosse TX29/35 bad CRC: calculated %02x, received %02x\n",
 					time_str, c_crc, r_crc);
 			// reject row
 			continue;
-		}		
+		}
+
 		/*
 		 * Now that message "envelope" has been validated,
 		 * start parsing data.
 		 */
-	
 		uint8_t sensor_id, newbatt, battery_low;
 		uint8_t humidity; // in %. If > 100 : no humidity sensor
 		float temp_c; // in °C
@@ -117,46 +121,54 @@ static int lacrosse_it(bitbuffer_t *bitbuffer, uint8_t device29or35) {
 		//if (humidity == LACROSSE_TX29_NOHUMIDSENSOR) 
 		//	humidity = -1; // The TX29-IT sensor do not have humidity. It is replaced by a special value
 		
-			data = data_make("time",          "",            DATA_STRING, time_str,
-							 "brand",         "",            DATA_STRING, "LaCrosse",
-							 "model",         "",            DATA_STRING, (device29or35 == 29 ? "TX29-IT" : "TX35DTH-IT"),
-							 "id",            "",            DATA_INT,    sensor_id,
-							 "battery",       "Battery",     DATA_STRING, battery_low ? "LOW" : "OK",
-							 "newbattery",    "NewBattery",  DATA_INT,	  newbatt,
+			data = data_make("time",		  "",			DATA_STRING, time_str,
+							 "brand",		 "",			DATA_STRING, "LaCrosse",
+							 "model",		 "",			DATA_STRING, (device29or35 == 29 ? "TX29-IT" : "TX35DTH-IT"),
+							 "id",			"",			DATA_INT,	sensor_id,
+							 "battery",	   "Battery",	 DATA_STRING, battery_low ? "LOW" : "OK",
+							 "newbattery",	"NewBattery",  DATA_INT,	  newbatt,
 							 "temperature_C", "Temperature", DATA_FORMAT, "%.1f C", DATA_DOUBLE, temp_c,
-							 "humidity",      "Humidity",    DATA_FORMAT, "%u %%", DATA_INT, humidity,
-							 "crc",           "CRC",         DATA_STRING, "OK",
+							 "humidity",	  "Humidity",	DATA_FORMAT, "%u %%", DATA_INT, humidity,
+							 "crc",		   "CRC",		 DATA_STRING, "OK",
 							 NULL);
-		
+
 		data_acquired_handler(data);
 		events++;
 	}
 	return events;
 }
 
+/**
+ ** Wrapper for the TX29 device
+ **/
 static int lacrossetx29_callback(bitbuffer_t *bitbuffer) {
 	return lacrosse_it(bitbuffer, 29);
 }
 
+/**
+ ** Wrapper for the TX35 device
+ **/
 static int lacrossetx35_callback(bitbuffer_t *bitbuffer) {
 	return lacrosse_it(bitbuffer, 35);
 }
 
 static char *output_fields[] = {
-    "time",
+	"time",
 	"brand",
-    "model",
-    "id",
+	"model",
+	"id",
 	"battery",
 	"newbattery",
 	"status",
-    "temperature_C",
-    "humidity",
+	"temperature_C",
+	"humidity",
 	"crc",
-    NULL
+	NULL
 };
+
+// Receiver for the TX29 device
 r_device lacrosse_tx29 = {
-    .name           = "LaCrosse TX29IT Temperature snsor",
+	.name           = "LaCrosse TX29IT Temperature snsor",
 	.modulation     = FSK_PULSE_PCM,
 	.short_limit    = 55,
 	.long_limit     = 55,
@@ -167,11 +179,10 @@ r_device lacrosse_tx29 = {
 	.fields         = output_fields,
 };
 
+// Receiver for the TX29 device
 r_device lacrosse_tx35 = {
-    .name           = "LaCrosse TX35DTH-IT Temperature sensor",
+	.name           = "LaCrosse TX35DTH-IT Temperature sensor",
 	.modulation     = FSK_PULSE_PCM,
-	//.short_limit    = 55,
-	//.long_limit     = 55,
 	.short_limit    = 105,
 	.long_limit     = 105,
 	.reset_limit    = 4000,
