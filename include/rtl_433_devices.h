a9a574fe4f93b63caef52d0fcecb23afa0a01fc2
psa-jforestier <psa-jforestier@users.noreply.github.com>
2017-02-01 09:38:40 +0100

    LaCrosse TX29-IT / TX35DTH-IT weather sensor (#479)
    * 1st experiment
    
    * First try
    
    * First working decoder of LaCrosse TX29/35
    
    * Merge TX29 and TX35
    
    * Be compatible with rules
    
    * Finalization
    
    * oups
    
    * Code improvement

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -76,8 +76,9 @@
 		DECL(maverick_et73x) \
 		DECL(rftech) \
 		DECL(lacrosse_TX141TH_Bv2) \
-		DECL(acurite_00275rm)
-
+		DECL(acurite_00275rm) \
+		DECL(lacrosse_tx35) \
+		DECL(lacrosse_tx29)
 
 typedef struct {
 	char name[256];
