60bf690d5b9273788383a05d6faf1e7c50ac514a
Tommy Vestermark <tov@vestermark.dk>
2017-03-11 13:12:21 +0100

    Fineoffset: WH25 add checksum, cleanup and enable per default

--- src/devices/fineoffset.c
+++ src/devices/fineoffset.c
@@ -1,7 +1,18 @@
 /* Fine Offset Electronics sensor protocol
  *
- * The protocol is for the wireless Temperature/Humidity sensor
- * Fine Offset Electronics WH2
+ * Copyright (C) 2017 Tommy Vestermark
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+#include "rtl_433.h"
+#include "data.h"
+#include "util.h"
+
+
+/*
+ * Fine Offset Electronics WH2 Temperature/Humidity sensor protocol
  * aka Agimex Rosenborg 66796 (sold in Denmark)
  * aka ClimeMET CM9088 (Sold in UK)
  * aka TFA Dostmann/Wertheim 30.3157 (Temperature only!) (sold in Germany)
@@ -20,17 +31,7 @@
  * 
  * Based on reverse engineering with gnu-radio and the nice article here:
  *  http://lucsmall.com/2012/04/29/weather-station-hacking-part-2/
- *
- * Copyright (C) 2015 Tommy Vestermark
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
  */
-#include "rtl_433.h"
-#include "data.h"
-#include "util.h"
-
 static int fineoffset_WH2_callback(bitbuffer_t *bitbuffer) {
     bitrow_t *bb = bitbuffer->bb;
     data_t *data;
@@ -114,29 +115,32 @@ static int fineoffset_WH2_callback(bitbuffer_t *bitbuffer) {
  * Reading: 22.6 C, 40 %, 1001.7 hPa
  *
  * Extracted data:
- *                   TT TT HH PP PP
- * aa aa aa 2d d4 e5 02 72 28 27 21 c9 bb aa aa aa
+ *          II TT TT HH PP PP CC ??
+ * aa 2d d4 e5 02 72 28 27 21 c9 bb aa
+ *
+ * II = Sensor ID (guess). Does not change at battery change.
+ * TT TT = Temperature (+40*10)
+ * HH = Humidity
+ * PP PP = Pressure (*10)
+ * CC = Checksum of previous 6 bytes (binary sum truncated to 8 bit)
+ * ?? = Some additional checksum?
+ *
  */
 static int fineoffset_WH25_callback(bitbuffer_t *bitbuffer) {
     data_t *data;
     char time_str[LOCAL_TIME_BUFLEN];
-
-    uint8_t buffer[16];
-    uint16_t id = 0;
-    float temperature = 0;
-    uint8_t humidity = 0;
-    float pressure = 0;
 
     // Validate package
     if (bitbuffer->bits_per_row[0] < 400 || bitbuffer->bits_per_row[0] > 510) {  // Nominal size is 500 bit periods
         return 0;
     }
 
-    /* Get time now */
+    // Get time now
     local_time_str(0, time_str);
 
-    // Find a data package
-    static const uint8_t HEADER[] = { 0xAA, 0xAA, 0xAA, 0x2D };
+    // Find a data package and extract data buffer
+    static const uint8_t HEADER[] = { 0xAA, 0x2D, 0xD4 };
+    uint8_t buffer[12];
     unsigned bit_offset = bitbuffer_search(bitbuffer, 0, 320, HEADER, sizeof(HEADER)*8);    // Normal index is 361, skip some bytes to find faster
     if (bit_offset + sizeof(buffer)*8 >= bitbuffer->bits_per_row[0]) {  // Did not find a big enough package
         if (debug_output) {
@@ -145,30 +149,42 @@ static int fineoffset_WH25_callback(bitbuffer_t *bitbuffer) {
         }
         return 0;
     }
-
-    // Extract relevant bytes
     bitbuffer_extract_bytes(bitbuffer, 0, bit_offset, buffer, sizeof(buffer)*8);
 
-    id = (uint16_t)buffer[4] << 8 | buffer[5];     // Somewhat guesswork... (Based on 1 sensor)
-    temperature = (float)((uint16_t)buffer[6] << 8 | buffer[7]) / 10 - 40.0;
-    humidity = buffer[8];
-    pressure = (float)((uint16_t)buffer[9] << 8 | buffer[10]) / 10;
+    // Verify checksum
+    uint8_t checksum = buffer[3] + buffer[4] + buffer[5] + buffer[6] + buffer[7] + buffer[8];
+    if (checksum != buffer[9]) {
+        if (debug_output) {
+            fprintf(stderr, "Fineoffset_WH25: checksum error: %02x\n", checksum);
+            bitbuffer_print(bitbuffer);
+        }
+        return 0;
+    }
 
-    char raw_str[128];
-    for (unsigned n=0; n<sizeof(buffer); n++) { sprintf(raw_str+n*3, "%02x ", buffer[n]); }
+    // Decode data
+    uint8_t id = buffer[3];     // Somewhat guesswork... (Based on 1 sensor)
+    float   temperature = (float)((uint16_t)buffer[4] << 8 | buffer[5]) / 10.0 - 40.0;
+    uint8_t humidity = buffer[6];
+    float   pressure = (float)((uint16_t)buffer[7] << 8 | buffer[8]) / 10.0;
 
+    // Include raw string (To be deleted when all data is figured out...)
+//    char raw_str[128];
+//    for (unsigned n=0; n<sizeof(buffer); n++) { sprintf(raw_str+n*3, "%02x ", buffer[n]); }
+
+    // Output data
     data = data_make("time",          "",            DATA_STRING, time_str,
                      "model",         "",            DATA_STRING, "Fine Offset Electronics, WH25",
                      "id",            "ID",          DATA_INT, id,
                      "temperature_C", "Temperature", DATA_FORMAT, "%.01f C", DATA_DOUBLE, temperature,
                      "humidity",      "Humidity",    DATA_FORMAT, "%u %%", DATA_INT, humidity,
                      "pressure",      "Pressure",    DATA_FORMAT, "%.01f hPa", DATA_DOUBLE, pressure,
-                     "raw",           "raw",         DATA_STRING, raw_str,
+//                     "raw",           "Raw",         DATA_STRING, raw_str,
                       NULL);
     data_acquired_handler(data);
 
     return 1;
 }
+
 
 static char *output_fields[] = {
     "time",
@@ -179,6 +195,7 @@ static char *output_fields[] = {
     NULL
 };
 
+
 static char *output_fields_WH25[] = {
     "time",
     "model",
@@ -186,12 +203,13 @@ static char *output_fields_WH25[] = {
     "temperature_C",
     "humidity",
     "pressure",
-    "raw",
+//    "raw",
     NULL
 };
 
+
 r_device fineoffset_WH2 = {
-    .name           = "Fine Offset Electronics, WH-2 Sensor",
+    .name           = "Fine Offset Electronics, WH2 Temperature/Humidity Sensor",
     .modulation     = OOK_PULSE_PWM_RAW,
     .short_limit    = 800,	// Short pulse 544µs, long pulse 1524µs, fixed gap 1036µs
     .long_limit     = 2800,	// Maximum pulse period (long pulse + fixed gap)
@@ -206,11 +224,11 @@ r_device fineoffset_WH2 = {
 r_device fineoffset_WH25 = {
     .name           = "Fine Offset Electronics, WH25 Temperature/Humidity/Pressure Sensor",
     .modulation     = FSK_PULSE_PCM,
-    .short_limit    = 56,	// Bit width = 56µs
-    .long_limit     = 56,	// NRZ encoding (bit width = pulse width)
+    .short_limit    = 58,	// Bit width = 58µs (measured across 580 samples / 40 bits / 250 kHz )
+    .long_limit     = 58,	// NRZ encoding (bit width = pulse width)
     .reset_limit    = 20000,	// Package starts with a huge gap of ~18900 us
     .json_callback  = &fineoffset_WH25_callback,
-    .disabled       = 1,
+    .disabled       = 0,
     .demod_arg      = 0,
     .fields         = output_fields_WH25
 };
