c092c1261bd2facfadb97f8f70dc5e56c644f9ca
Tommy Vestermark <tov@vestermark.dk>
2017-03-18 23:24:31 +0100

    pulse_demod: Make pwm_precise resilient to short spurious pulses
    Tested with test corpus without regression. Decodes one more file and
    works better in live testing in a noisy environment (random spikes).

--- src/pulse_demod.c
+++ src/pulse_demod.c
@@ -163,6 +163,9 @@ int pulse_demod_pwm_precise(const pulse_data_t *pulses, struct protocol_state *d
 		// Sync pulse
 		} else if (p->pulse_sync_width && (fabsf(pulses->pulse[n] - p->pulse_sync_width) < p->pulse_tolerance)) {
 			bitbuffer_add_row(&bits);
+		// Ignore spurious short pulses
+		} else if (pulses->pulse[n] < (device->short_limit - p->pulse_tolerance)) {
+			// Do nothing
 		} else {
 			return 0;	// Pulse outside specified timing
 		}
