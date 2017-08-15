985f5ebffef009896aad97df1fd3517668dfb41d
Yuriy Shestakov <yshestakov@gmail.com>
2017-08-15 13:48:53 +0300

    Fixed do_build.sh, .travis.yml trying to run 'make test'

--- do_build.sh
+++ do_build.sh
@@ -5,10 +5,11 @@ cd build
 cmake ../
 make
 # make install
-
+set -x
 git clone https://github.com/merbanan/rtl_433_tests.git
 cd rtl_433_tests
 export PATH=../build/src:$PATH
+test -f ../build/src/rtl_433
 
 # virtualenv --system-site-packages .venv
 # source .venv/bin/activate
