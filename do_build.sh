29f22e43c20189e317a909e562740bb3572a02fe
Yuriy Shestakov <yshestakov@gmail.com>
2017-08-15 13:41:18 +0300

    Added tests from rtl_433_tests.git repo

--- do_build.sh
+++ do_build.sh
@@ -5,3 +5,12 @@ cd build
 cmake ../
 make
 # make install
+
+git clone https://github.com/merbanan/rtl_433_tests.git
+cd rtl_433_tests
+export PATH=../build/src:$PATH
+
+# virtualenv --system-site-packages .venv
+# source .venv/bin/activate
+# pip install deepdiff
+make test
