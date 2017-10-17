962a7d6393ee64e1b2eb3d82a04ad31c8515d622
Tugzrida <Tugzrida@users.noreply.github.com>
2017-10-17 18:35:56 +1100

    Fix output formatting
    - Invert output of learning status - it was backwards
    - Tidy output format - each value now has its own line and it nicely named and indented
    - Change source from tabs to spaces

--- src/devices/efergy_e2_classic.c
+++ src/devices/efergy_e2_classic.c
@@ -27,96 +27,94 @@
 #include "data.h"
 
 static int efergy_e2_classic_callback(bitbuffer_t *bitbuffer) {
-	unsigned num_bits = bitbuffer->bits_per_row[0];
-	uint8_t *bytes = bitbuffer->bb[0];
-	data_t *data;
-	char time_str[LOCAL_TIME_BUFLEN];
+    unsigned num_bits = bitbuffer->bits_per_row[0];
+    uint8_t *bytes = bitbuffer->bb[0];
+    data_t *data;
+    char time_str[LOCAL_TIME_BUFLEN];
 
-	if (num_bits < 64 || num_bits > 80) {
-		return 0;
-	}
+    if (num_bits < 64 || num_bits > 80) {
+        return 0;
+    }
 
-	// The bit buffer isn't always aligned to the transmitted data, so
-	// search for data start and shift out the bits which aren't part
-	// of the data. The data always starts with 0000 (or 1111 if
-	// gaps/pulses are mixed up).
-	while ((bytes[0] & 0xf0) != 0xf0 && (bytes[0] & 0xf0) != 0x00) {
-		num_bits -= 1;
-		if (num_bits < 64) {
-			return 0;
-		}
+    // The bit buffer isn't always aligned to the transmitted data, so
+    // search for data start and shift out the bits which aren't part
+    // of the data. The data always starts with 0000 (or 1111 if
+    // gaps/pulses are mixed up).
+    while ((bytes[0] & 0xf0) != 0xf0 && (bytes[0] & 0xf0) != 0x00) {
+        num_bits -= 1;
+        if (num_bits < 64) {
+            return 0;
+        }
 
-		for (unsigned i = 0; i < (num_bits + 7) / 8; ++i) {
-			bytes[i] <<= 1;
-			bytes[i] |= (bytes[i + 1] & 0x80) >> 7;
-		}
-	}
+        for (unsigned i = 0; i < (num_bits + 7) / 8; ++i) {
+            bytes[i] <<= 1;
+            bytes[i] |= (bytes[i + 1] & 0x80) >> 7;
+        }
+    }
 
-	// Sometimes pulses and gaps are mixed up. If this happens, invert
-	// all bytes to get correct interpretation.
-	if (bytes[0] & 0xf0) {
-		for (unsigned i = 0; i < 8; ++i) {
-			bytes[i] = ~bytes[i];
-		}
-	}
+    // Sometimes pulses and gaps are mixed up. If this happens, invert
+    // all bytes to get correct interpretation.
+    if (bytes[0] & 0xf0) {
+        for (unsigned i = 0; i < 8; ++i) {
+            bytes[i] = ~bytes[i];
+        }
+    }
 
-	unsigned checksum = 0;
-	for (unsigned i = 0; i < 7; ++i) {
-		checksum += bytes[i];
-	}
-	if (checksum == 0) {
-		return 0; // reduce false positives
-	}
-	checksum &= 0xff;
-	if (checksum != bytes[7]) {
-		return 0;
-	}
+    unsigned checksum = 0;
+    for (unsigned i = 0; i < 7; ++i) {
+        checksum += bytes[i];
+    }
+    if (checksum == 0) {
+        return 0; // reduce false positives
+    }
+    checksum &= 0xff;
+    if (checksum != bytes[7]) {
+        return 0;
+    }
 
-	uint16_t address = bytes[2] << 8 | bytes[1];
-	uint8_t learn = (bytes[3] & 0x80) >> 7;
-	uint8_t interval = (((bytes[3] & 0x30) >> 4) + 1) * 6;
-	uint8_t battery = (bytes[3] & 0x40) >> 6;
-	uint8_t fact = (-(int8_t)bytes[6] + 15);
-	float current_adc = (float)((bytes[4] << 8 | bytes[5])) / (1 << fact);
+    uint16_t address = bytes[2] << 8 | bytes[1];
+    uint8_t learn = (bytes[3] & 0x80) >> 7;
+    uint8_t interval = (((bytes[3] & 0x30) >> 4) + 1) * 6;
+    uint8_t battery = (bytes[3] & 0x40) >> 6;
+    uint8_t fact = (-(int8_t)bytes[6] + 15);
+    float current_adc = (float)((bytes[4] << 8 | bytes[5])) / (1 << fact);
 
-	local_time_str(0, time_str);
+    local_time_str(0, time_str);
 
-	// Output data
-	data = data_make(
-		"time",			"Time",		DATA_STRING,	time_str,
-		"model",		"",			DATA_STRING,	"Efergy e2 Sensor",
-		"id",			"ID",		DATA_INT,		address,
-		"current",		"",			DATA_FORMAT,	"%.2f", 	DATA_DOUBLE, current_adc,
-		"interval", 	"",			DATA_INT,		interval,
-		"battery",      "Battery",  DATA_STRING, 	battery ? "OK" : "LOW",
-		"learn",		"",			DATA_STRING, 	learn ? "NO" : "YES",
-		NULL
-	);
+    // Output data
+    data = data_make("time",     "",               DATA_STRING, time_str,
+                     "model",    "",               DATA_STRING, "Efergy e2 CT",
+                     "id",       "Transmitter ID", DATA_INT, address,
+                     "current",  "Current",        DATA_FORMAT, "%.2f A", DATA_DOUBLE, current_adc,
+                     "interval", "Interval",       DATA_FORMAT, "%ds", DATA_INT, interval,
+                     "battery",  "Battery",        DATA_STRING, battery ? "OK" : "LOW",
+                     "learn",    "Learning",       DATA_STRING, learn ? "YES" : "NO",
+                     NULL);
 
-	data_acquired_handler(data);
+    data_acquired_handler(data);
 
-	return 1;
+    return 1;
 }
 
 static char *output_fields[] = {
-	"time",
-	"model",
-	"id",
-	"current",
-	"interval",
-	"battery",
-	"learn",
-	NULL
+    "time",
+    "model",
+    "id",
+    "current",
+    "interval",
+    "battery",
+    "learn",
+    NULL
 };
 
 r_device efergy_e2_classic = {
-	.name           = "Efergy e2 classic",
-	.modulation     = FSK_PULSE_PWM_RAW,
-	.short_limit    = 92,
-	.long_limit     = 400,
-	.reset_limit    = 400,
-	.json_callback  = &efergy_e2_classic_callback,
-	.disabled       = 0,
-	.demod_arg      = 0,
-	.fields         = output_fields
+    .name           = "Efergy e2 classic",
+    .modulation     = FSK_PULSE_PWM_RAW,
+    .short_limit    = 92,
+    .long_limit     = 400,
+    .reset_limit    = 400,
+    .json_callback  = &efergy_e2_classic_callback,
+    .disabled       = 0,
+    .demod_arg      = 0,
+    .fields         = output_fields
 };
