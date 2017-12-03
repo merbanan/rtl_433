eb1d1bbe2885ebaa0c73307cc3751e529580e06e
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2017-12-03 22:59:11 +0100

    merge formatting from PR #615

--- src/devices/fordremote.c
+++ src/devices/fordremote.c
@@ -1,4 +1,3 @@
-
 /* Ford Car Key
  *
  * Identifies event, but does not attempt to decrypt rolling code...
@@ -13,15 +12,14 @@
  * [00] {60} 74 28 c1 06 a4 1a 05 20
  * [00] {60} 74 28 c1 06 a4 1a 05 20
  * [00] {60} 74 28 c1 06 a4 1a 05 20
-
  */
+
 #include "rtl_433.h"
 #include "data.h"
 #include "util.h"
 #define FORD_BITLEN  40
 #define FORD_BYTELEN 5
 #define FORD_PACKETCOUNT 4
-
 
 static int fordremote_callback(bitbuffer_t *bitbuffer) {
 	bitrow_t *bb = bitbuffer->bb;
@@ -30,57 +28,44 @@ static int fordremote_callback(bitbuffer_t *bitbuffer) {
 	char time_str[LOCAL_TIME_BUFLEN];
     	local_time_str(0, time_str);
 	int i;
-	uint8_t id=0;
     	uint32_t device_id =0;
-
 	unsigned bits = bitbuffer->bits_per_row[0];
 
-
- for (int i = 0; i < bitbuffer->num_rows; i++)
-{
-	// Validate preamble
-	if ((bitbuffer->bits_per_row[i] == 6) && (bytes[00]==120))
-	{
-	 if (debug_output) {
-                 bitbuffer_print(bitbuffer);
-                }
-
-	}
-
-	// Validate package code goes here
-	else if((bitbuffer->bits_per_row[i] > 55) && ((bb[i][0] >> 4)==7))
-	{
-	if (debug_output) {
-		 bitbuffer_print(bitbuffer);
+	for (int i = 0; i < bitbuffer->num_rows; i++) {
+		// Validate preamble
+		if ((bitbuffer->bits_per_row[i] == 6) && (bytes[00]==120)) {
+			if (debug_output) {
+                		bitbuffer_print(bitbuffer);
+                	}
+		} else if ((bitbuffer->bits_per_row[i] > 55) && ((bb[i][0] >> 4)==7)) {
+			if (debug_output) {
+				bitbuffer_print(bitbuffer);
+			}
+			device_id = ((bb[i][0]<<16)| (bb[i][1]<<8)|(bb[i][2]));
+			uint16_t code = (bb[i][7]);
+			/* Get time now */
+			local_time_str(0, time_str);
+			data = data_make(
+        			"time",		"time",		DATA_STRING, time_str,
+				"model",	"model",	DATA_STRING, "Ford Car Remote",
+				"id",		"device-id",	DATA_INT, device_id,
+				"code", 	"data",		DATA_INT, code,
+				NULL);
+			data_acquired_handler(data);
+			return 1;
 		}
-	device_id = ((bb[i][0]<<16)| (bb[i][1]<<8)|(bb[i][2]));
-	uint16_t code = (bb[i][7]);
-
-	/* Get time now */
-	local_time_str(0, time_str);
-
-     data = data_make(
-                        "time", "time", DATA_STRING, time_str,
-                        "model", "model", DATA_STRING, "Ford Car Remote",
-			"device_id", "device-id", DATA_INT, device_id,
-                        "code", "data", DATA_INT, code,
-                      NULL);
-
-       data_acquired_handler(data);
-       return 1;
-        }
-return 0;
-}
-return 0;
+		return 0;
+	}
+	return 0;
 }
 
 static char *output_fields[] = {
-        "time",
-        "model",
-	"device_id",
-        "code",
-        NULL
-        };
+	"time",
+	"model",
+	"id",
+	"code",
+	NULL
+};
 
 r_device fordremote = {
 	.name		= "Ford Car Key",
@@ -89,8 +74,7 @@ r_device fordremote = {
 	.long_limit	= 625,	// Not used
 	.reset_limit	= 1500,
 	.json_callback	= &fordremote_callback,
-	.disabled		= 0,
-	.demod_arg		= 2,
-        .fields		= output_fields	
+	.disabled	= 0,
+	.demod_arg	= 2,
+        .fields		= output_fields
 };
-
