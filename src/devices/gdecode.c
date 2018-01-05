49a0461a85261c3454d916d911dd49b65012b283
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2018-01-05 10:42:22 +0100

    adding kwargs

--- src/devices/gdecode.c
+++ src/devices/gdecode.c
@@ -11,12 +11,14 @@
 #include "data.h"
 #include "rtl_433.h"
 #include "util.h"
+#include "optparse.h"
 
 struct gdecode {
     char *myname;
     int min_rows;
     int min_bits;
     int min_repeats;
+    int match_len;
 };
 
 static int gdecode_callback(bitbuffer_t *bitbuffer, struct gdecode *params)
@@ -41,10 +43,11 @@ static int gdecode_callback(bitbuffer_t *bitbuffer, struct gdecode *params)
     if (!min_bits_found)
         return 0;
 
-    // TODO: add some spec for repeats...
     int r = bitbuffer_find_repeated_row(bitbuffer, params->min_repeats, params->min_bits);
     if (r < 0)
         return 0;
+
+    // TODO: add some spec for matches...
 
     if (debug_output >= 1) {
         fprintf(stderr, "%s: ", params->myname);
@@ -82,15 +85,18 @@ static char *output_fields[] = {
     NULL
 };
 
-#define GDECODE_SLOTS 5
+#define GDECODE_SLOTS 8
 static struct gdecode *params_slot[GDECODE_SLOTS];
 static int cb_slot0(bitbuffer_t *bitbuffer) { return gdecode_callback(bitbuffer, params_slot[0]); }
 static int cb_slot1(bitbuffer_t *bitbuffer) { return gdecode_callback(bitbuffer, params_slot[1]); }
 static int cb_slot2(bitbuffer_t *bitbuffer) { return gdecode_callback(bitbuffer, params_slot[2]); }
 static int cb_slot3(bitbuffer_t *bitbuffer) { return gdecode_callback(bitbuffer, params_slot[3]); }
 static int cb_slot4(bitbuffer_t *bitbuffer) { return gdecode_callback(bitbuffer, params_slot[4]); }
+static int cb_slot5(bitbuffer_t *bitbuffer) { return gdecode_callback(bitbuffer, params_slot[5]); }
+static int cb_slot6(bitbuffer_t *bitbuffer) { return gdecode_callback(bitbuffer, params_slot[6]); }
+static int cb_slot7(bitbuffer_t *bitbuffer) { return gdecode_callback(bitbuffer, params_slot[7]); }
 static unsigned next_slot = 0;
-int (*callback_slot[])(bitbuffer_t *bitbuffer) = {cb_slot0, cb_slot1, cb_slot2, cb_slot3, cb_slot4};
+int (*callback_slot[])(bitbuffer_t *bitbuffer) = {cb_slot0, cb_slot1, cb_slot2, cb_slot3, cb_slot4, cb_slot5, cb_slot6, cb_slot7};
 
 r_device *gdecode_create_device(char *spec)
 {
@@ -99,9 +105,9 @@ r_device *gdecode_create_device(char *spec)
         exit(1);
     }
 
-    struct gdecode *params = (struct gdecode *)malloc(sizeof(struct gdecode));
+    struct gdecode *params = (struct gdecode *)calloc(1, sizeof(struct gdecode));
     params_slot[next_slot] = params;
-    r_device *dev = (r_device *)malloc(sizeof(r_device));
+    r_device *dev = (r_device *)calloc(1, sizeof(r_device));
     char *c;
 
     spec = strdup(spec);
@@ -167,24 +173,38 @@ r_device *gdecode_create_device(char *spec)
     }
     dev->reset_limit = atoi(c);
 
-    c = strtok(NULL, ":");
-    if (c != NULL) {
-        dev->demod_arg = atoi(c);
-    }
-
     dev->json_callback = callback_slot[next_slot];
+    dev->demod_arg = 0;
     dev->disabled = 0;
     dev->fields = output_fields;
 
-    c = strtok(NULL, ":");
-    if (c != NULL) {
-        params->min_bits = atoi(c);
-    } else {
-        params->min_bits = 1;
-    }
-
     params->min_rows = 1;
+    params->min_bits = 1;
     params->min_repeats = 1;
+
+    getkwargs(&c, NULL, NULL); // skip the initial fixed part
+    char *key, *val;
+    while (getkwargs(&c, &key, &val)) {
+        if (!strcasecmp(key, "demod"))
+            dev->demod_arg = atoi(val);
+
+        else if (!strcasecmp(key, "minbits"))
+            params->min_bits = atoi(val);
+
+        else if (!strcasecmp(key, "minrows"))
+            params->min_rows = atoi(val);
+
+        else if (!strcasecmp(key, "minrepeats"))
+            params->min_repeats = atoi(val);
+
+        else if (!strcasecmp(key, "match"))
+            params->match_len = atoi(val); // TODO:
+
+        else {
+            fprintf(stderr, "Bad gdecoder spec, unknown keyword (%s)!\n", key);
+            exit(1);
+        }
+    }
 
     free(spec);
     next_slot++;
