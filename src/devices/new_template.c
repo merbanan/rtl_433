970f78c80d5dc5fae00dde960cfe50036524413c
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2018-01-25 21:15:05 +0100

    freshen up template with more examples

--- src/devices/new_template.c
+++ src/devices/new_template.c
@@ -1,11 +1,28 @@
-/* Template decoder
+/* Template decoder for DEVICE, tested with BRAND, BRAND.
  *
- * Copyright © 2016 Benjamin Larsson
+ * Copyright (C) 2016 Benjamin Larsson
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
+ *
+ * (describe the modulation, timing, and transmission, e.g.)
+ * The device uses PPM encoding,
+ * 0 is encoded as 40 us pulse and 132 us gap,
+ * 1 is encoded as 40 us pulse and 224 us gap.
+ * The device sends a transmission every 63 seconds.
+ * A transmission starts with a preamble of 0xAA,
+ * there a 5 repeated packets, each with a 1200 us gap.
+ *
+ * (describe the data and payload, e.g.)
+ * Packet nibbles:  FF PP PP II II II TT TT CC
+ * F = flags, (0x40 is battery_low)
+ * P = Pressure, 16-bit little-endian
+ * I = id, 24-bit little-endian
+ * T = Unknown, likely Temperature, 16-bit little-endian
+ * C = Checksum, CRC-8 truncated poly 0x07 init 0x00
+ *
  */
 
 /* Use this as a starting point for a new decoder. */
@@ -14,31 +31,33 @@
 #include "pulse_demod.h"
 #include "util.h"
 
-
 /*
  * Hypothetical template device
  *
  * Message is 68 bits long
  * Messages start with 0xAA
+ * The message is repeated as 5 packets,
+ * require at least 3 repeated packets.
  *
  */
-#define MYDEVICE_BITLEN        68
-#define MYDEVICE_STARTBYTE    0xAA
+#define MYDEVICE_BITLEN      68
+#define MYDEVICE_STARTBYTE   0xAA
+#define MYDEVICE_MINREPEATS  3
 #define MYDEVICE_MSG_TYPE    0x10
-#define MYDEVICE_CRC_POLY    0x80
+#define MYDEVICE_CRC_POLY    0x07
 #define MYDEVICE_CRC_INIT    0x00
 
-
-static int template_callback(bitbuffer_t *bitbuffer) {
+static int template_callback(bitbuffer_t *bitbuffer)
+{
     char time_str[LOCAL_TIME_BUFLEN];
-    uint8_t *bb;
-    uint16_t brow, row_nbytes;
-    uint16_t sensor_id = 0;
-    uint8_t msg_type, r_crc, c_crc;
-    int16_t value;
     data_t *data;
-    int valid = 0;
-
+    uint16_t r; // a row index
+    uint8_t *b; // bits of a row
+    int parity;
+    uint8_t r_crc, c_crc;
+    uint16_t sensor_id;
+    uint8_t msg_type;
+    int16_t value;
 
     /*
      * Early debugging aid to see demodulated bits in buffer and
@@ -53,10 +72,15 @@ static int template_callback(bitbuffer_t *bitbuffer) {
     //        bitbuffer_print(bitbuffer);
     //    }
 
-    local_time_str(0, time_str);
+    /*
+     * If you expect the bits flipped with respect to the demod
+     * invert the whole bit buffer.
+     */
+
+    bitbuffer_invert(bitbuffer);
 
     /*
-     * bit buffer will contain multiple rows, many of them empty.
+     * The bit buffer will contain multiple rows.
      * Typically a complete message will be contained in a single
      * row if long and reset limits are set correctly.
      * May contain multiple message repeats.
@@ -64,64 +88,112 @@ static int template_callback(bitbuffer_t *bitbuffer) {
      * start/preamble periods of different lengths.
      */
 
-    for (brow = 0; brow < bitbuffer->num_rows; ++brow) {
-    bb = bitbuffer->bb[brow];
-
     /*
-     * Validate message and reject invalid messages as
-     * early as possible before attempting to parse data..
-     *
-     * Check "message envelope"
-     * - valid message length
-     * - valid preamble/device type/fixed bits if any
-     * - Data integrity checks (CRC/Checksum/Parity)
+     * Either, if you expect just a single packet
+     * loop over all rows and collect or output data:
      */
 
-    if (bitbuffer->bits_per_row[brow] != 68)
-        continue;
+    for (r = 0; r < bitbuffer->num_rows; ++r) {
+        b = bitbuffer->bb[r];
+
+        /*
+         * Validate message and reject invalid messages as
+         * early as possible before attempting to parse data.
+         *
+         * Check "message envelope"
+         * - valid message length (use a minimum length to account
+         *   for stray bits appended or prepended by the demod)
+         * - valid preamble/device type/fixed bits if any
+         * - Data integrity checks (CRC/Checksum/Parity)
+         */
+
+        if (bitbuffer->bits_per_row[r] < MYDEVICE_BITLEN) {
+            continue;
+        }
+
+        /*
+         * ... see below and replace `return 0;` with `continue;`
+         */
+    }
 
     /*
-     * number of bytes in row.
-     *
-     * Number of decoded bits may not be a multiple of 8.
-     * bitbuffer row will have enough bytes to contain
-     * all bytes, so round up.
+     * Or, if you expect repeated packets
+     * find a suitable row:
      */
-    row_nbytes = (bitbuffer->bits_per_row[brow] + 7)/8;
 
+    r = bitbuffer_find_repeated_row(bitbuffer, MYDEVICE_MINREPEATS, MYDEVICE_BITLEN);
+    if (r < 0 || bitbuffer->bits_per_row[r] > MYDEVICE_BITLEN + 16) {
+        return 0;
+    }
+
+    b = bitbuffer->bb[r];
 
     /*
-     * Reject rows that don't start with the correct start byte
+     * Either reject rows that don't start with the correct start byte:
      * Example message should start with 0xAA
      */
-    if (bb[0] != MYDEVICE_STARTBYTE)
-        continue;
+    if (b[0] != MYDEVICE_STARTBYTE) {
+        return 0;
+    }
 
     /*
-     * Check message integrity (CRC/Checksum/parity)
+     * Or (prefered) search for the message preamble:
+     * See bitbuffer_search()
+     */
+
+    /*
+     * Check message integrity (Parity example)
+     */
+    // parity check: odd parity on bits [0 .. 67]
+    // i.e. 8 bytes and a nibble.
+    parity = b[0] ^ b[1] ^ b[2] ^ b[3] ^ b[4] ^ b[5] ^ b[6] ^ b[7]; // parity as byte
+    parity = (parity >> 4) ^ (parity & 0xF); // fold to nibble
+    parity ^= b[8] >> 4; // add remaining nibble
+    parity = (parity >> 2) ^ (parity & 0x3); // fold to 2 bits
+    parity = (parity >> 1) ^ (parity & 0x1); // fold to 1 bit
+
+    if (!parity) {
+        if (debug_output) {
+            fprintf(stderr, "new_template parity check failed\n");
+        }
+        return 0;
+    }
+
+    /*
+     * Check message integrity (Checksum example)
+     */
+    if (((b[0] + b[1] + b[2] + b[3] - b[4]) & 0xFF) != 0) {
+        if (debug_output) {
+            fprintf(stderr, "new_template checksum error\n");
+        }
+        return 0;
+    }
+
+    /*
+     * Check message integrity (CRC example)
      *
      * Example device uses CRC-8
      */
-    r_crc = bb[row_nbytes - 1];
-    c_crc = crc8(bb, row_nbytes - 1, MYDEVICE_CRC_POLY, MYDEVICE_CRC_INIT);
+    r_crc = b[7];
+    c_crc = crc8(b, MYDEVICE_BITLEN / 8, MYDEVICE_CRC_POLY, MYDEVICE_CRC_INIT);
     if (r_crc != c_crc) {
         // example debugging output
-        if (debug_output >= 1)
-        fprintf(stderr, "%s new_tamplate bad CRC: calculated %02x, received %02x\n",
-            time_str, c_crc, r_crc);
+        if (debug_output) {
+            fprintf(stderr, "new_template bad CRC: calculated %02x, received %02x\n",
+                    c_crc, r_crc);
+        }
 
         // reject row
-        continue;
+        return 0;
     }
 
     /*
      * Now that message "envelope" has been validated,
      * start parsing data.
      */
-
-    msg_type = bb[1];
-    sensor_id = bb[2] << 8 | bb[3];
-    value = bb[4] << 8 | bb[5];
+    msg_type = b[1];
+    sensor_id = b[2] << 8 | b[3];
+    value = b[4] << 8 | b[5];
 
     if (msg_type != MYDEVICE_MSG_TYPE) {
         /*
@@ -129,39 +201,38 @@ static int template_callback(bitbuffer_t *bitbuffer) {
          * could be a bad message or a new message not
          * previously seen.  Optionally log debug putput.
          */
-        continue;
+        return 0;
     }
 
-    data = data_make("time", "", DATA_STRING, time_str,
-        "model", "", DATA_STRING, "New Template",
-        "id", "", DATA_INT, sensor_id,
-        "data","", DATA_INT, value,
-        NULL);
+    local_time_str(0, time_str);
+
+    data = data_make(
+            "time",  "", DATA_STRING, time_str,
+            "model", "", DATA_STRING, "New Template",
+            "id",    "", DATA_INT,    sensor_id,
+            "data",  "", DATA_INT,    value,
+            "mic",   "", DATA_STRING, "CHECKSUM", // CRC, CHECKSUM, or PARITY
+            NULL);
 
     data_acquired_handler(data);
 
-    valid++;
-    }
-
     // Return 1 if message successfully decoded
-    if (valid)
     return 1;
-
-    return 0;
 }
 
 /*
- * List of fields to output when using CSV
+ * List of fields that may appear in the output
  *
  * Used to determine what fields will be output in what
- * order for this devince when using -F csv.
+ * order for this device when using -F csv.
  *
  */
-static char *csv_output_fields[] = {
+static char *output_fields[] = {
     "time",
     "model",
     "id",
     "data",
+    "mic", // remove if not applicable
     NULL
 };
 
@@ -170,16 +241,11 @@ static char *csv_output_fields[] = {
  *
  * Timings:
  *
- * short, long, nad reset - specify pulse/period timings
- *     based on number of samples at 250 Khz samples/second.
+ * short, long, and reset - specify pulse/period timings in [us].
  *     These timings will determine if the received pulses
  *     match, so your callback will fire after demodulation.
  *
- * for readabiliy, specify timings based on 1 Mhz samples
- *     but a divide by 4 in the definition.
- *
- *
- * Demodular:
+ * Modulation:
  *
  * The function used to turn the received signal into bits.
  * See:
@@ -188,15 +254,14 @@ static char *csv_output_fields[] = {
  *
  * This device is disabled by default. Enable it with -R 61 on the commandline
  */
-
 r_device template = {
     .name          = "Template decoder",
     .modulation    = OOK_PULSE_PPM_RAW,
-    .short_limit   = ((56+33)/2)*4,
-    .long_limit    = (56+33)*4,
-    .reset_limit   = (56+33)*2*4,
+    .short_limit   = (224 + 132) / 2, // short gap is 132 us, long gap is 224 us
+    .long_limit    = 224 + 132,
+    .reset_limit   = (224 + 132) * 2,
     .json_callback = &template_callback,
     .disabled      = 1,
     .demod_arg     = 0,
-    .fields        = csv_output_fields,
+    .fields        = output_fields,
 };
