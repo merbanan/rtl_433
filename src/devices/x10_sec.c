fcb5f3c5ac0f191b7eb34d6cdcf6d829ab6c417d
Anthony Kava <github@forensic.coffee>
2018-01-26 10:42:13 -0600

    Fixes spacing in comments

--- src/devices/x10_sec.c
+++ src/devices/x10_sec.c
@@ -28,13 +28,13 @@
 static int x10_sec_callback(bitbuffer_t *bitbuffer) {
     bitrow_t *bb = bitbuffer->bb;
     data_t *data;
-    uint16_t r;                            /* a row index                */
-    uint8_t *b;                            /* bits of a row            */
-    uint8_t test_a;                        /* test 1/2 for message        */
-    uint8_t test_b;                        /* test 2/2 for message        */
-    char *event_str = "UNKNOWN";        /* human-readable event        */
-    char x10_id_str[5] = "\0";            /* string showing hex value    */
-    char x10_code_str[5] = "\0";        /* string showing hex value    */
+    uint16_t r;                          /* a row index              */
+    uint8_t *b;                          /* bits of a row            */
+    uint8_t test_a;                      /* test 1/2 for message     */
+    uint8_t test_b;                      /* test 2/2 for message     */
+    char *event_str = "UNKNOWN";         /* human-readable event     */
+    char x10_id_str[5] = "\0";           /* string showing hex value */
+    char x10_code_str[5] = "\0";         /* string showing hex value */
     char time_str[LOCAL_TIME_BUFLEN];
 
     for (r=0; r < bitbuffer->num_rows; ++r) {
