2a5604279657c322d222a8294b16ac62bd0de2ef
Christian W. Zuckschwerdt <zany@triq.net>
2018-05-19 11:59:59 +0200

    Add Pacific PMV-C210 TMPS support (#717)

--- src/devices/tpms_toyota.c
+++ src/devices/tpms_toyota.c
@@ -1,4 +1,5 @@
-/* FSK 9-byte Manchester encoded TPMS data with CRC-7.
+/* FSK 9-byte Differential Manchester encoded TPMS data with CRC-8.
+ * Pacific Industries Co.Ltd. PMV-C210
  * Seen on a Toyota Auris(Corolla). The manufacturers of the Toyota TPMS are
  * Pacific Industrial Corp and sometimes TRW Automotive and might also be used
  * in other car brands. Contact me with your observations!
@@ -10,24 +11,22 @@
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * There are 15 bits of sync followed by 72 bits manchester encoded data.
- * Flipping the bits gives a CRC init of 0, which seems more likely.
- * E.g. 10101 0101 1000 01 100110011010[...60 manchester bits]01011001101000
- * The first data bit is always one, last always 0. Dropping the first data
- * bit (which could reasonably be part of the sync) we get 72 bits: 8 bytes
- * payload and 1 byte CRC (7-bit CRC in the MSBs with the LSB set to 0).
+ * There are 14 bits sync followed by 72 bits manchester encoded data and
+ * 3 bits trailer.
+ * E.g. 01010101001111 00110011 [...64 manchester bits] 00101010111
  *
- * Particularly interesting is the use of a CRC-7 with 0x7d as truncated poly.
- *
- * The first 4 bytes are the ID. Followed by likely two 8-bit values,
- * a 16-bit value and then the CRC. The values seem to be signed.
+ * The first 4 bytes are the ID. Followed by 1-bit state,
+ * 8-bit values of pressure, temperature, 7-bit state, 8-bit inverted pressure
+ * and then the a CRC-8 with 0x07 truncated poly and init 0x80.
+ * The temperature is offset by 40 deg C.
+ * The pressure seems to be 1/4 PSI offset by -7 PSI (i.e. 28 raw = 0 PSI).
  */
 
 #include "rtl_433.h"
 #include "util.h"
 
-// full preamble is 1 0101 0101 1000 01 = 55 8
-static const unsigned char preamble_pattern[2] = { 0x55, 0x80 }; // 12 bits
+// full preamble is 0101 0101 0011 11 = 55 3c
+static const unsigned char preamble_pattern[2] = {0x54, 0xf0}; // 12 bits
 
 static int tpms_toyota_decode(bitbuffer_t *bitbuffer, unsigned row, unsigned bitpos) {
     char time_str[LOCAL_TIME_BUFLEN];
@@ -37,36 +36,45 @@ static int tpms_toyota_decode(bitbuffer_t *bitbuffer, unsigned row, unsigned bit
     uint8_t *b;
     unsigned id;
     char id_str[9];
-    unsigned code;
-    char code_str[21];
+    unsigned status, pressure1, pressure2, temp;
     int crc;
 
     // skip the first 1 bit, i.e. raw "01" to get 72 bits
-    start_pos = bitbuffer_manchester_decode(bitbuffer, row, bitpos + 2, &packet_bits, 80);
-    if (start_pos-bitpos < 144) {
+    start_pos = bitbuffer_differential_manchester_decode(bitbuffer, row, bitpos, &packet_bits, 80);
+    if (start_pos - bitpos < 144) {
         return 0;
     }
     b = packet_bits.bb[0];
 
-    crc = b[8] >> 1;
-    if (crc7(b, 8, 0x7d, 0x00) != crc) {
+    crc = b[8];
+    if (crc8(b, 8, 0x07, 0x80) != crc) {
         return 0;
     }
 
-    id = b[0]<<24 | b[1]<<16 | b[2]<<8 | b[3];
-    sprintf(id_str, "%08x", id);
+    id = b[0] << 24 | b[1] << 16 | b[2] << 8 | b[3];
+    status = (b[4] & 0x80) | (b[6] & 0x7f); // status bit and 0 filler
+    pressure1 = (b[4] & 0x7f) << 1 | b[5] >> 7;
+    temp = (b[5] & 0x7f) << 1 | b[6] >> 7;
+    pressure2 = b[7] ^ 0xff;
 
-    code = b[4]<<24 | b[5]<<16 | b[6]<<8 | b[7];
-    sprintf(code_str, "%08x", code);
+    if (pressure1 != pressure2) {
+        if (debug_output)
+            fprintf(stderr, "Toyota TPMS pressure check error: %02x vs %02x\n", pressure1, pressure2);
+        return 0;
+    }
+
+    sprintf(id_str, "%08x", id);
 
     local_time_str(0, time_str);
     data = data_make(
-        "time",         "",     DATA_STRING, time_str,
-        "model",        "",     DATA_STRING, "Toyota",
-        "type",         "",     DATA_STRING, "TPMS",
-        "id",           "",     DATA_STRING, id_str,
-        "code",         "",     DATA_STRING, code_str,
-        "mic",          "",     DATA_STRING, "CRC",
+        "time",             "",     DATA_STRING,    time_str,
+        "model",            "",     DATA_STRING,    "Toyota",
+        "type",             "",     DATA_STRING,    "TPMS",
+        "id",               "",     DATA_STRING,    id_str,
+        "status",           "",     DATA_INT,       status,
+        "pressure_PSI",     "",     DATA_DOUBLE,    pressure1*0.25-7.0,
+        "temperature_C",    "",     DATA_DOUBLE,    temp-40.0,
+        "mic",              "",     DATA_STRING,    "CRC",
         NULL);
 
     data_acquired_handler(data);
@@ -76,8 +84,6 @@ static int tpms_toyota_decode(bitbuffer_t *bitbuffer, unsigned row, unsigned bit
 static int tpms_toyota_callback(bitbuffer_t *bitbuffer) {
     unsigned bitpos = 0;
     int events = 0;
-
-    bitbuffer_invert(bitbuffer);
 
     // Find a preamble with enough bits after it that it could be a complete packet
     while ((bitpos = bitbuffer_search(bitbuffer, 0, bitpos, (const uint8_t *)&preamble_pattern, 12)) + 158 <=
@@ -94,7 +100,9 @@ static char *output_fields[] = {
     "model",
     "type",
     "id",
-    "code",
+    "status",
+    "pressure_PSI",
+    "temperature_C",
     "mic",
     NULL
 };
