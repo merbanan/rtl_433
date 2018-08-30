964211dba77cd60bf3faa02fa96e28745671d4d0
Petr Konecny <pekon@google.com>
2018-08-30 13:19:49 -0700

    Add comment about lack of checksum.

--- src/devices/simplisafe.c
+++ src/devices/simplisafe.c
@@ -146,6 +146,8 @@ ss_keypad_commands (bitbuffer_t *bitbuffer, int row)
 static int
 ss_sensor_callback (bitbuffer_t *bitbuffer)
 {
+        // Simplisafe transmits data twice. Treat second copy as a checkum
+        // and reject transmissions that lack it.
         int row = bitbuffer_find_repeated_row(bitbuffer, 2, 90);
         if (row < 0) return 0;
 	bitbuffer_invert(bitbuffer); // Invert the Bits
