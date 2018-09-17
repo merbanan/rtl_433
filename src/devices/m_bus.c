25ab033b43e1cb604ecdddb2364f6cbaa994b640
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2018-09-17 07:07:07 +0000

    Fix OpenWrt by not forcing inline (#811)

--- src/devices/m_bus.c
+++ src/devices/m_bus.c
@@ -14,7 +14,7 @@
 #include "util.h"
 
 // Convert two BCD encoded nibbles to an integer
-inline unsigned bcd2int(uint8_t bcd) {
+static unsigned bcd2int(uint8_t bcd) {
     return 10*(bcd>>4) + (bcd & 0xF);
 }
 
