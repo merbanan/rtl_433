31c22e6c5f43131283f6d74d38bc5955f9855ec4
NorthernMan54 <buyminivan-northern@yahoo.ca>
2018-09-27 19:34:32 -0400

    Working
    # Conflicts:
    #	src/devices/skylink_ha-434tl.c

--- src/devices/skylink_ha-434tl.c
+++ src/devices/skylink_ha-434tl.c
@@ -2,9 +2,9 @@
  * Skylink HA-434TL
  *
 
- This motion sensor is pretty primitive, but the price is good.  It only sends messages when it see's motion, and no motion clear message.  It also does not appear to send battery levels or low battery.  It also does not appear to send regular heartbeats as well.
+   This motion sensor is pretty primitive, but the price is good.  It only sends messages when it see's motion, and no motion clear message.  It also does not appear to send battery levels or low battery.  It also does not appear to send regular heartbeats as well.
 
- The message that is received, appears to only be a device serial, and does not appear to change.
+   The message that is received, appears to only be a device serial, and does not appear to change.
 
    2018-09-26 21:25:07 :	Skylink motion sensor   :	00000
    pulse_demod_ppm(): Skylink HA-434TL motion sensor
@@ -31,6 +31,39 @@
    [ 16] Pulse:  135, Gap:  125, Period:  260   0
    [ 17] Pulse:  135, Gap:  383, Period:  518   1
    [ 18] Pulse:  134, Gap: 5071, Period: 5205   End of message
+
+   This is transmitted roughly every 2 hours
+
+   {"time" : "2018-09-27 06:55:50", "model" : "Skylink motion sensor", "code" : "5e3e8"}
+   pulse_demod_ppm(): Skylink HA-434TL motion sensor
+   bitbuffer:: Number of rows: 2
+   [00] { 3} e0        : 111
+   [01] {17} 5e 3e 80  : 01011110 00111110 1
+   Pulse data: 22 pulses
+   [  0] Pulse:   12, Gap:  269, Period:  281
+   [  1] Pulse:   13, Gap:  269, Period:  282
+   [  2] Pulse:   10, Gap:  235, Period:  245
+   [  3] Pulse:  506, Gap:  499, Period: 1005  Header
+   [  4] Pulse:  132, Gap:  127, Period:  259  0
+   [  5] Pulse:  132, Gap:  385, Period:  517  1
+   [  6] Pulse:  132, Gap:  127, Period:  259
+   [  7] Pulse:  133, Gap:  384, Period:  517
+   [  8] Pulse:  132, Gap:  385, Period:  517
+   [  9] Pulse:  132, Gap:  385, Period:  517
+   [ 10] Pulse:  132, Gap:  384, Period:  516
+   [ 11] Pulse:  132, Gap:  128, Period:  260
+   [ 12] Pulse:  132, Gap:  128, Period:  260
+   [ 13] Pulse:  132, Gap:  127, Period:  259
+   [ 14] Pulse:  132, Gap:  384, Period:  516
+   [ 15] Pulse:  133, Gap:  384, Period:  517
+   [ 16] Pulse:  133, Gap:  383, Period:  516
+   [ 17] Pulse:  133, Gap:  384, Period:  517
+   [ 18] Pulse:  133, Gap:  383, Period:  516
+   [ 19] Pulse:  133, Gap:  128, Period:  261
+   [ 20] Pulse:  132, Gap:  384, Period:  516
+   [ 21] Pulse:  133, Gap: 5061, Period: 5194
+   new_tmplate callback:
+
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -46,7 +79,10 @@ static int skylink_motion_callback(bitbuffer_t *bitbuffer) {
         data_t *data;
         uint8_t *b;
         int code;
+        int motion;
+        int raw;
         char code_str[6];
+        char raw_str[6];
 
         if (debug_output > 1) {
                 printf("new_tmplate callback:\n");
@@ -55,19 +91,30 @@ static int skylink_motion_callback(bitbuffer_t *bitbuffer) {
 
         for (int i = 0; i < bitbuffer->num_rows; ++i) {
                 b = bitbuffer->bb[i];
-                if (bitbuffer->bits_per_row[i] < 10)
+                if (bitbuffer->bits_per_row[i] != 17)
                         continue;
+
                 {
 
-                        code = (b[0] << 12) | (b[1] << 4) | (b[2] >> 4);
+// [01] {17} 5e 3e 80  : 01011110 00111110 1  -- No motion
+// [01] {17} be 3e 80  : 10111110 00111110 1  -- Motion
+
+                        motion = (b[0] >> 5);
+                        raw = code = ((b[0]) << 12) | (b[1] << 4) | (b[2] >> 4);
+                        code = ((b[0] & 0x1F) << 12) | (b[1] << 4) | (b[2] >> 4);
+
                         sprintf(code_str, "%05x", code);
+                        sprintf(raw_str, "%05x", raw);
+                        motion = ( motion == 5 );
 
                         /* Get time now */
                         local_time_str(0, time_str);
                         data = data_make(
                                 "time",     "",  DATA_STRING, time_str,
-                                "model",    "",  DATA_STRING, "Skylink motion sensor",
-                                "code",     "",  DATA_STRING, code_str,
+                                "model",    "",  DATA_STRING, "Skylink HA-434TL motion sensor",
+                                "motion",    "",  DATA_STRING, motion ? "true" : "false",
+                                "id",     "",  DATA_STRING, code_str,
+                                "raw",     "",  DATA_STRING, raw_str,
                                 NULL);
 
                         data_acquired_handler(data);
