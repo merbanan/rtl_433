d915014cc1b0a16f6400e45a82a817e08c22f715
Erik <erik@montnemery.com>
2018-10-17 19:41:41 +0200

    Use built in crc8 function

--- src/devices/rubicson.c
+++ src/devices/rubicson.c
@@ -13,27 +13,13 @@
  * unk1 is always 0 probably unused
  * temp is 12 bit signed scaled by 10
  * F is always 0xf
- * crc1 and crc2 forms a 8-bit crc
+ * crc1 and crc2 forms a 8-bit crc, polynomial 0x31, initial value 0x6c, final value 0x0
  *
  * The sensor can be bought at Kjell&Co
  */
 
 
-/* Bitwise implementation of CRC-8 with parameters:
- * width=8  poly=0x31  init=0x6c  refin=false  refout=false  xorout=0x00
- */
-uint8_t crc8_rubicson(uint8_t const *data, size_t len)
-{
-    unsigned crc = 0x6c;
-    while (len--) {
-        crc ^= *data++;
-        for (unsigned k = 0; k < 8; k++)
-            crc = crc & 0x80 ? (crc << 1) ^ 0x31 : crc << 1;
-    }
-    return crc;
-}
-
-static int rubicson_crc_check(bitrow_t *bb) {
+int rubicson_crc_check(bitrow_t *bb) {
     uint8_t tmp[5];
     tmp[0] = bb[1][0];            // Byte 0 is nibble 0 and 1
     tmp[1] = bb[1][1];            // Byte 1 is nibble 2 and 3
@@ -41,7 +27,7 @@ static int rubicson_crc_check(bitrow_t *bb) {
     tmp[3] = bb[1][3]&0xf0;       // Byte 3 is nibble 6 and 0-padding
     tmp[4] = (bb[1][3]&0x0f)<<4 | // CRC is nibble 7 and 8
              (bb[1][4]&0xf0)>>4;
-    return crc8_rubicson(tmp, 5) == 0;
+    return crc8(tmp, 5, 0x31, 0x6c) == 0;
 }
 
 static int rubicson_callback(bitbuffer_t *bitbuffer) {
