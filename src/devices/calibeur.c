de41a39336aa91c1c2efd9f7dcbce2e5c2566306
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2018-11-20 13:38:18 +0100

    Add some missing csv field declarations

--- src/devices/calibeur.c
+++ src/devices/calibeur.c
@@ -111,5 +111,6 @@ r_device calibeur_RF104 = {
 	.tolerance      = 0,	// raw mode
 	.json_callback  = &calibeur_rf104_callback,
 	.disabled       = 0,
-	.demod_arg      = 0		// not used
+	.demod_arg      = 0,	// not used
+	.fields         = output_fields,
 };
