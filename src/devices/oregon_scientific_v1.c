f77ef5f0196b1d3ce6b622c473c31a602f8cb175
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2018-12-12 14:18:54 +0100

    Clean up OSv1

--- src/devices/oregon_scientific_v1.c
+++ src/devices/oregon_scientific_v1.c
@@ -14,18 +14,6 @@
 
 #define	OSV1_BITS	32
 
-static int rev_nibble(int nib)
-{
-	int revnib = 0;
-
-	revnib += (nib >> 3) & 0x1;
-	revnib += (nib >> 1) & 0x2;
-	revnib += (nib << 1) & 0x4;
-	revnib += (nib << 3) & 0x8;
-
-	return(revnib);
-}
-
 static int oregon_scientific_v1_callback(r_device *decoder, bitbuffer_t *bitbuffer) {
 	int ret = 0;
 	int row;
@@ -37,42 +25,46 @@ static int oregon_scientific_v1_callback(r_device *decoder, bitbuffer_t *bitbuff
 	int battery, uk2, sign, uk3, checksum;
 	data_t *data;
 
+	for (row = 0; row < bitbuffer->num_rows; row++) {
+		if (bitbuffer->bits_per_row[row] != OSV1_BITS)
+			continue;
 
-	for(row = 0; row < bitbuffer->num_rows; row++) {
-		if(bitbuffer->bits_per_row[row] == OSV1_BITS) {
-			cs = 0;
-			for(i = 0; i < OSV1_BITS / 8; i++) {
-				nibble[i * 2    ] = rev_nibble((bitbuffer->bb[row][i] >> 4));
-				nibble[i * 2 + 1] = rev_nibble((bitbuffer->bb[row][i] & 0x0f));
-				if(i < ((OSV1_BITS / 8) - 1))
-					cs += nibble[i * 2] + 16 * nibble[i * 2 + 1];
-			}
-			cs = (cs & 0xFF) + (cs >> 8);
-			checksum = nibble[6] + (nibble[7] << 4);
-			if(checksum == cs) {
-				sid      = nibble[0];
-				channel  = ((nibble[1] >> 2) & 0x03) + 1;
-				uk1      = (nibble[1] >> 0) & 0x03;	/* unknown.  Seen change every 60 minutes */
-				tempC    =  nibble[2] / 10. + nibble[3] + nibble[4] * 10.;
-				battery  = (nibble[5] >> 3) & 0x01;
-				uk2      = (nibble[5] >> 2) & 0x01;	/* unknown.  Always zero? */
-				sign     = (nibble[5] >> 1) & 0x01;
-				uk3      = (nibble[5] >> 0) & 0x01;	/* unknown.  Always zero? */
-
-				if(sign) tempC = -tempC;
-
-				data = data_make(
-					"brand",		"",				DATA_STRING,	"OS",
-					"model",		"",				DATA_STRING,	"OSv1 Temperature Sensor",
-					"sid",			"SID",			DATA_INT,		sid,
-					"channel",		"Channel",		DATA_INT,		channel,
-					"battery",		"Battery",		DATA_STRING,	battery ? "LOW" : "OK",
-					"temperature_C","Temperature",	DATA_FORMAT,	"%.01f C",				DATA_DOUBLE,	tempC,
-					NULL);
-				decoder_output_data(decoder, data);
-				ret++;
-			}
+		cs = 0;
+		for (i = 0; i < OSV1_BITS / 8; i++) {
+			uint8_t byte = reverse8(bitbuffer->bb[row][i]);
+			nibble[i * 2    ] = byte & 0x0f;
+			nibble[i * 2 + 1] = byte >> 4;
+			if (i < ((OSV1_BITS / 8) - 1))
+				cs += nibble[i * 2] + 16 * nibble[i * 2 + 1];
 		}
+
+		cs = (cs & 0xFF) + (cs >> 8);
+		checksum = nibble[6] + (nibble[7] << 4);
+		if (checksum != cs)
+			continue;
+
+		sid      = nibble[0];
+		channel  = ((nibble[1] >> 2) & 0x03) + 1;
+		uk1      = (nibble[1] >> 0) & 0x03;	/* unknown.  Seen change every 60 minutes */
+		tempC    =  nibble[2] * 0.1 + nibble[3] + nibble[4] * 10.;
+		battery  = (nibble[5] >> 3) & 0x01;
+		uk2      = (nibble[5] >> 2) & 0x01;	/* unknown.  Always zero? */
+		sign     = (nibble[5] >> 1) & 0x01;
+		uk3      = (nibble[5] >> 0) & 0x01;	/* unknown.  Always zero? */
+
+		if (sign)
+			tempC = -tempC;
+
+		data = data_make(
+				"brand",		"",				DATA_STRING,	"OS",
+				"model",		"",				DATA_STRING,	"OSv1 Temperature Sensor",
+				"sid",			"SID",			DATA_INT,		sid,
+				"channel",		"Channel",		DATA_INT,		channel,
+				"battery",		"Battery",		DATA_STRING,	battery ? "LOW" : "OK",
+				"temperature_C","Temperature",	DATA_FORMAT,	"%.01f C",				DATA_DOUBLE,	tempC,
+				NULL);
+		decoder_output_data(decoder, data);
+		ret++;
 	}
 	return ret;
 }
