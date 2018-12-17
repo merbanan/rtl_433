d935ee7203c68f04865862bcbceeac970f76dc65
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2018-12-17 09:00:48 +0100

    Change homedir conf load path

--- src/compat_paths.c
+++ src/compat_paths.c
@@ -19,7 +19,7 @@ char **compat_get_default_conf_paths()
     static char buf[256] = "";
     if (!paths[0]) {
         paths[0] = "rtl_433.conf";
-        snprintf(buf, sizeof(buf), "%s%s", getenv("HOME"), "/.rtl_433.conf");
+        snprintf(buf, sizeof(buf), "%s%s", getenv("HOME"), "/.rtl_433/rtl_433.conf");
         paths[1] = buf;
         paths[2] = "/usr/local/etc/rtl_433/rtl_433.conf";
         paths[3] = "/etc/rtl_433/rtl_433.conf";
