8f4c9e082a0e5c9f072e11eba8c928c33e45a73a
Simon Lundell <s.lundell@gmail.com>
2019-01-15 16:51:24 +0100

    Fail early. Return information even if the packet could not be decrypted (but is otherwise valid). Will be useful for troubleshooting purposes. Moved debug outputs to where they may last be needed.
    Made a few changes to

--- src/devices/ikea_sparsnas.c
+++ src/devices/ikea_sparsnas.c
@@ -71,55 +71,56 @@ detail. Many thanks to kodarn!
 #include "decoder.h"
 #define IKEA_SPARSNAS_MESSAGE_BITLEN 160    // 20 bytes incl 8 bit length, 8 bit address, 128 bits data, and 16 bits of CRC. Excluding preamble and sync word
 #define IKEA_SPARSNAS_MESSAGE_BYTELEN    ((IKEA_SPARSNAS_MESSAGE_BITLEN + 7) / 8)
+#define IKEA_SPARSNAS_MESSAGE_BITLEN_MAX 260 // Just for early sanity checks
 
 #define IKEA_SPARSNAS_PREAMBLE_BITLEN 32
 static const uint8_t preamble_pattern[4] = {0xAA, 0xAA, 0xD2, 0x01};
 
+#define IKEA_SPARSNAS_CRC_INIT 0xffff
+#define IKEA_SPARSNAS_CRC_POLY 0x8005
 
 static int ikea_sparsnas_callback(r_device *decoder, bitbuffer_t *bitbuffer)
 {
 
-    if (decoder->verbose > 1)
-        decoder_output_bitbufferf(decoder, bitbuffer, "IKEA Sparsnäs:");
-  
+    if ((bitbuffer->bits_per_row[0] < IKEA_SPARSNAS_MESSAGE_BITLEN) || (bitbuffer->bits_per_row[0] > IKEA_SPARSNAS_MESSAGE_BITLEN_MAX)) {
+        if (decoder->verbose > 1) {
+            decoder_output_bitbufferf(decoder, bitbuffer, "IKEA Sparsnäs");
+            fprintf(stderr, "IKEA Sparsnäs: Too short packet received. Expected %d, received %d", IKEA_SPARSNAS_MESSAGE_BITLEN, bitbuffer->bits_per_row[0]);
+        }
+        return 0;
+    }
 
-    uint8_t buffer[IKEA_SPARSNAS_MESSAGE_BYTELEN];
-    
     // Look for preamble
     uint16_t bitpos = bitbuffer_search(bitbuffer, 0, 0, (const uint8_t *)&preamble_pattern, IKEA_SPARSNAS_PREAMBLE_BITLEN);
 
-    if (!bitpos || (bitpos + IKEA_SPARSNAS_MESSAGE_BITLEN > bitbuffer->bits_per_row[0])) {
-        if (decoder->verbose > 1)
+    if ((bitbuffer->bits_per_row[0] == bitpos) || (bitpos + IKEA_SPARSNAS_MESSAGE_BITLEN > bitbuffer->bits_per_row[0])) {
+        if (decoder->verbose > 1){
+            decoder_output_bitbufferf(decoder, bitbuffer, "IKEA Sparsnäs");
             fprintf(stderr, "IKEA Sparsnäs: malformed package, preamble not found. (Expected 0xAAAAD201)\n");
+        }
         return 0;
     }
-
-    if (decoder->verbose > 1)
-        fprintf(stderr, "IKEA Sparsnäs: Found a message at bitpos %3d\n", bitpos);
     
     // extract message, discarding preamble
+    uint8_t buffer[IKEA_SPARSNAS_MESSAGE_BYTELEN];
     bitbuffer_extract_bytes(bitbuffer, 0, bitpos + IKEA_SPARSNAS_PREAMBLE_BITLEN, buffer, IKEA_SPARSNAS_MESSAGE_BITLEN);
     
-    if (decoder->verbose > 1)
+    if (decoder->verbose > 1) {
+        decoder_output_bitbufferf(decoder, bitbuffer, "IKEA Sparsnäs");
         decoder_output_bitrowf(decoder, buffer, IKEA_SPARSNAS_MESSAGE_BITLEN, "Encrypted message");
-
-
+    }
     // CRC check
-    uint16_t crc_calculated = crc16(buffer, IKEA_SPARSNAS_MESSAGE_BYTELEN - 2, 0x8005 ,0xffff);
+    uint16_t crc_calculated = crc16(buffer, IKEA_SPARSNAS_MESSAGE_BYTELEN - 2, IKEA_SPARSNAS_CRC_POLY, IKEA_SPARSNAS_CRC_INIT);
     uint16_t crc_received = buffer[18] << 8 | buffer[19];
 
     if (crc_received != crc_calculated) {
-        if (decoder->verbose > 1)
-            fprintf(stderr, "IKEA Sparsnäs: CRC check failed (0x%X != ox%X)\n", crc_calculated, crc_received);
+        if (decoder->verbose > 1) {
+            fprintf(stderr, "IKEA Sparsnäs: CRC check failed (0x%X != 0x%X)\n", crc_calculated, crc_received);
+        }
         return 0;
     }
 
-    if (decoder->verbose > 1)
-        fprintf(stderr, "IKEA Sparsnäs: CRC OK (%X == %X)\n", crc_calculated, crc_received);
-
-    
     //Decryption
-
     uint8_t decrypted[18];
 
     uint8_t key[5];
@@ -131,8 +132,6 @@ static int ikea_sparsnas_callback(r_device *decoder, bitbuffer_t *bitbuffer)
     key[3] = 0x47;
     key[4] = (uint8_t)(sensor_id_sub >> 16);
     
-    if (decoder->verbose > 1)
-        fprintf(stderr, "IKEA Sparsnäs: Encryption key: 0x%X%X%X%X%X\n", key[0], key[1], key[2], key[3], key[4]);
     
     for (size_t i = 0; i < 5; i++)
         decrypted[i] = buffer[i];
@@ -140,83 +139,91 @@ static int ikea_sparsnas_callback(r_device *decoder, bitbuffer_t *bitbuffer)
     for(size_t i = 0; i < 13; i++)
         decrypted[5 + i] = buffer[5 + i] ^ key[i % 5];
 
-    if (decoder->verbose > 1)
-        decoder_output_bitrowf(decoder, decrypted, 18 * 8, "Decrypted");
-
     // Additional integrity checks
-
     int rcv_sensor_id = decrypted[5] << 24 | decrypted[6] << 16 | decrypted[7] << 8 | decrypted[8];
 
+    if (decoder->verbose > 1){
+        fprintf(stderr, "IKEA Sparsnäs: CRC OK (%X == %X)\n", crc_calculated, crc_received);
+        fprintf(stderr, "IKEA Sparsnäs: Encryption key: 0x%X%X%X%X%X\n", key[0], key[1], key[2], key[3], key[4]);
+        decoder_output_bitrowf(decoder, decrypted, 18 * 8, "Decrypted");
+        fprintf(stderr, "IKEA Sparsnäs: Received sensor id: %d\n", rcv_sensor_id);
+    }
     if (rcv_sensor_id != IKEA_SPARSNAS_SENSOR_ID) {
-        if (decoder->verbose > 1)
+        if (decoder->verbose > 1){
             fprintf(stderr, "IKEA Sparsnäs: Received sensor id (%d) not the same as sender (%d)\n", rcv_sensor_id, IKEA_SPARSNAS_SENSOR_ID);
-        return 0;
+        }
+
+        data_t *data;
+        data = data_make(
+            "model",         "Model",               DATA_STRING, "IKEA Sparsnäs Energy Meter Monitor [Encrypted]",
+            "id",            "Sensor ID",           DATA_INT, IKEA_SPARSNAS_SENSOR_ID,
+            "mic",           "Integrity",           DATA_STRING,    "CRC",
+            NULL
+        );
+        decoder_output_data(decoder, data);
+        return 1;
     }
 
     if (decrypted[0] != 0x11){
         decoder_output_bitrowf(decoder, decrypted + 5, 13 * 8,  "Message malformed");
-        if (decoder->verbose > 1)
+        if (decoder->verbose > 1){
             fprintf(stderr, "IKEA Sparsnäs: Message malformed (byte0=%X expected %X)\n", decrypted[0], 0x11);
+        }
         return 0;
     }
-    
     if (decrypted[3] != 0x07){
         decoder_output_bitrowf(decoder, decrypted + 5, 13 * 8,  "Message malformed");
-        if (decoder->verbose > 1)
+        if (decoder->verbose > 1){
             fprintf(stderr, "IKEA Sparsnäs: Message malformed (byte3=%X expected %X)\n", decrypted[0], 0x07);
+        }
         return 0;
     }
 
-    if (decoder->verbose > 1)
-        fprintf(stderr, "IKEA Sparsnäs: Received sensor id: %d\n", rcv_sensor_id);
-    
     //Value extraction and interpretation
-
     uint16_t sequence_number = (decrypted[9] << 8 | decrypted[10]);
-    uint16_t effect = (decrypted[11] << 8 | decrypted[12]);
+    uint16_t effect = (decrypted[11] <<  8 | decrypted[12]);
     uint32_t pulses = (decrypted[13] << 24 | decrypted[14] << 16 | decrypted[15] << 8 | decrypted[16]);
-    uint8_t battery = decrypted[17];
-    double watt = effect * 24;
+    uint8_t battery =  decrypted[17];
+    double watt = effect * 24.;
     uint8_t mode = decrypted[4]^0x0f;
 
-    //Note that mode cycles between 0-3 when you first put in the batteries in
-    if(mode == 1){
-      watt = (double)((3600000 / IKEA_SPARSNAS_PULSES_PER_KWH) * 1024) / (effect);
+    if(mode == 1){     //Note that mode cycles between 0-3 when you first put in the batteries in
+      watt = ((3600000.0 / IKEA_SPARSNAS_PULSES_PER_KWH) * 1024.0) / effect;
     } else if (mode == 0 ) { // special mode for low power usage
       watt = effect * 0.24 / IKEA_SPARSNAS_PULSES_PER_KWH;
     }
-    
     double cumulative_kWh = ((double)pulses) / ((double)IKEA_SPARSNAS_PULSES_PER_KWH);
     
-
     data_t *data;
     data = data_make(
         "model",         "Model",               DATA_STRING, "IKEA Sparsnäs Energy Meter Monitor",
         "id",            "Sensor ID",           DATA_INT, rcv_sensor_id,
+        "pulses_per_kwh", "Pulses per kWh",     DATA_INT, IKEA_SPARSNAS_PULSES_PER_KWH,
         "sequence",      "Sequence Number",     DATA_INT, sequence_number,
         "battery",       "Battery",             DATA_FORMAT, "%d%%", DATA_INT, battery,
         "cumulative_kWh", "Cumulative kWh",     DATA_FORMAT, "%7.3fkWh", DATA_DOUBLE,  cumulative_kWh,
         "effect",        "Effect",              DATA_FORMAT, "%dW", DATA_INT,  effect,
         "pulses",        "Pulses",              DATA_INT,  pulses,
         "mode",          "Mode",                DATA_INT, mode,
+        "mic",           "Integrity",           DATA_STRING,    "CRC",
         NULL
     );
 
     decoder_output_data(decoder, data);
-    
     return 1;
-
 }
 
 static char *output_fields[] = {
     "model",
     "id",
+    "pulses_per_kwh",
     "sequence",
     "battery",
-    "pulses",
     "cumulative_kWh",
     "effect",
+    "pulses",
     "mode",
+    "mic",
     NULL
 };
 
