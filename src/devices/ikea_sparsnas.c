2f73d55556acdcd15577c68476e01447072714c9
Simon Lundell <s.lundell@gmail.com>
2019-01-17 09:03:11 +0100

    Added brute force cracking of the IKEA Sparsnäs encryption and some minor improvements.

--- src/devices/ikea_sparsnas.c
+++ src/devices/ikea_sparsnas.c
@@ -13,7 +13,7 @@ unit, but usual values are 500, 1000 and 2000. This is usually indicated like
 
 1000 imp/kWh
 
-on the front of the meter. This value goes into IKEA_SPARSNAS_PULSES_PER_KWH 
+on the front of the meter. This value goes into ikea_sparsnas_pulses_per_kwh 
 in this file. The sender also has a unique ID which is used in the encryption
 key, hence it is needed here to decrypt the data. The sender ID is on a sticker
 in the battery compartment. There are three groups of three digits there. The
@@ -43,6 +43,9 @@ Packet structure according to: https://github.com/strigeus/sparsnas_decoder
 17: uint8_t battery;       // Battery level, 0-100%
 18: uint16_t CRC;          // 16 bit CRC of bytes 0-17
 
+Example packet: 0x11a15f070ea2dfefe6d5fdd20547e6340ae7be61
+
+
 The packet's integrety can be checked with the 16b CRC at the end of the packet. 
 There are also several other ways to check the integrety of the package. 
  - (preamble)
@@ -61,13 +64,6 @@ detail. Many thanks to kodarn!
 */
 
 
-
-#define IKEA_SPARSNAS_SENSOR_ID 617633
-#define IKEA_SPARSNAS_PULSES_PER_KWH 1000
-
-// No settings required below this point
-
-
 #include "decoder.h"
 #define IKEA_SPARSNAS_MESSAGE_BITLEN 160    // 20 bytes incl 8 bit length, 8 bit address, 128 bits data, and 16 bits of CRC. Excluding preamble and sync word
 #define IKEA_SPARSNAS_MESSAGE_BYTELEN    ((IKEA_SPARSNAS_MESSAGE_BITLEN + 7) / 8)
@@ -78,6 +74,57 @@ static const uint8_t preamble_pattern[4] = {0xAA, 0xAA, 0xD2, 0x01};
 
 #define IKEA_SPARSNAS_CRC_INIT 0xffff
 #define IKEA_SPARSNAS_CRC_POLY 0x8005
+
+#define IKEA_SPARSNAS_ID_KEY_SUB 0x5D38E8CB
+
+static uint16_t ikea_sparsnas_pulses_per_kwh = 1000;
+static uint32_t ikea_sparsnas_sensor_id = 0;
+
+static uint32_t ikea_sparsnas_brute_force_encryption(uint8_t buffer[18]){
+    
+    const uint8_t b5 = buffer[5 + 0];
+    const uint8_t b6 = buffer[5 + 1];
+    const uint8_t b7 = buffer[5 + 2];
+    const uint8_t b8 = buffer[5 + 3];
+    const uint8_t battery_enc = buffer[17];
+    
+    uint8_t d0, d1, d2;
+    const uint8_t d3 = b8 ^ 0x47;
+
+    uint32_t dec_sensor_id, key_sensor_id;
+    uint8_t battery_dec, k0, k1, k2, k4;
+
+    for (k0=0;k0<0xFF;++k0){
+        d0 = b5 ^ k0;
+        if (d0 > 0x0F) { //will result in sensor_id > 999999
+            continue;
+        }
+        for (k1=0;k1<0xFF;++k1){
+            d1 = b6 ^ k1;
+
+            for (k2=0;k2<0xFF;++k2){
+                
+                d2 = b7 ^ k2;
+                battery_dec = battery_enc ^ k2;
+                dec_sensor_id = d0 << 24 | d1 << 16 | d2 << 8 | d3;
+                
+                if (dec_sensor_id > 999999){
+                    continue; //sensor id is at most 6 digits
+                }
+
+                for (k4=0;k4<0xFF;++k4){
+                    key_sensor_id  = (k0 << 24 | k4 << 16 | k2 << 8 | k1) + IKEA_SPARSNAS_ID_KEY_SUB;
+
+                    if ((dec_sensor_id == key_sensor_id) && (battery_dec <= 100)) {
+                        return dec_sensor_id;
+                    } 
+                }
+            }   
+        }        
+    }
+   return 0;
+}
+
 
 static int ikea_sparsnas_callback(r_device *decoder, bitbuffer_t *bitbuffer)
 {
@@ -121,10 +168,24 @@ static int ikea_sparsnas_callback(r_device *decoder, bitbuffer_t *bitbuffer)
     }
 
     //Decryption
+    if (!ikea_sparsnas_sensor_id){
+        if (decoder->verbose > 1){
+            fprintf(stderr, "IKEA Sparsnäs: No sensor ID configured. Brute forcing encryption.\n");
+        }
+        ikea_sparsnas_sensor_id = ikea_sparsnas_brute_force_encryption(buffer);
+        if (decoder->verbose > 1){
+            if (ikea_sparsnas_sensor_id){
+                fprintf(stderr, "IKEA Sparsnäs: Found valid sensor ID %06d. If reported values does not make sense, this might be incorrect.\n", ikea_sparsnas_sensor_id);
+            } else {
+                fprintf(stderr, "IKEA Sparsnäs: No valid sensor ID found.\n");
+            }
+        }
+    }
+    
     uint8_t decrypted[18];
 
     uint8_t key[5];
-    const uint32_t sensor_id_sub = IKEA_SPARSNAS_SENSOR_ID - 0x5D38E8CB;
+    const uint32_t sensor_id_sub = ikea_sparsnas_sensor_id - IKEA_SPARSNAS_ID_KEY_SUB;
 
     key[0] = (uint8_t)(sensor_id_sub >> 24);
     key[1] = (uint8_t)(sensor_id_sub);
@@ -140,7 +201,7 @@ static int ikea_sparsnas_callback(r_device *decoder, bitbuffer_t *bitbuffer)
         decrypted[5 + i] = buffer[5 + i] ^ key[i % 5];
 
     // Additional integrity checks
-    int rcv_sensor_id = decrypted[5] << 24 | decrypted[6] << 16 | decrypted[7] << 8 | decrypted[8];
+    uint32_t rcv_sensor_id = decrypted[5] << 24 | decrypted[6] << 16 | decrypted[7] << 8 | decrypted[8];
 
     if (decoder->verbose > 1){
         fprintf(stderr, "IKEA Sparsnäs: CRC OK (%X == %X)\n", crc_calculated, crc_received);
@@ -148,15 +209,19 @@ static int ikea_sparsnas_callback(r_device *decoder, bitbuffer_t *bitbuffer)
         decoder_output_bitrowf(decoder, decrypted, 18 * 8, "Decrypted");
         fprintf(stderr, "IKEA Sparsnäs: Received sensor id: %d\n", rcv_sensor_id);
     }
-    if (rcv_sensor_id != IKEA_SPARSNAS_SENSOR_ID) {
+    
+    if (rcv_sensor_id != ikea_sparsnas_sensor_id) {
         if (decoder->verbose > 1){
-            fprintf(stderr, "IKEA Sparsnäs: Received sensor id (%d) not the same as sender (%d)\n", rcv_sensor_id, IKEA_SPARSNAS_SENSOR_ID);
+            fprintf(stderr, "IKEA Sparsnäs: Malformed package, or wrong sensor id. Received sensor id (%d) not the same as sender (%d)\n", rcv_sensor_id, ikea_sparsnas_sensor_id);
         }
+    }
 
+    if ((!ikea_sparsnas_sensor_id) || (rcv_sensor_id != ikea_sparsnas_sensor_id)){
+        
         data_t *data;
         data = data_make(
             "model",         "Model",               DATA_STRING, "IKEA Sparsnäs Energy Meter Monitor [Encrypted]",
-            "id",            "Sensor ID",           DATA_INT, IKEA_SPARSNAS_SENSOR_ID,
+            "id",            "Sensor ID",           DATA_INT, ikea_sparsnas_sensor_id,
             "mic",           "Integrity",           DATA_STRING,    "CRC",
             NULL
         );
@@ -188,17 +253,17 @@ static int ikea_sparsnas_callback(r_device *decoder, bitbuffer_t *bitbuffer)
     uint8_t mode = decrypted[4]^0x0f;
 
     if(mode == 1){     //Note that mode cycles between 0-3 when you first put in the batteries in
-      watt = ((3600000.0 / IKEA_SPARSNAS_PULSES_PER_KWH) * 1024.0) / effect;
+      watt = ((3600000.0 / ikea_sparsnas_pulses_per_kwh) * 1024.0) / effect;
     } else if (mode == 0 ) { // special mode for low power usage
-      watt = effect * 0.24 / IKEA_SPARSNAS_PULSES_PER_KWH;
+      watt = effect * 0.24 / ikea_sparsnas_pulses_per_kwh;
     }
-    double cumulative_kWh = ((double)pulses) / ((double)IKEA_SPARSNAS_PULSES_PER_KWH);
+    double cumulative_kWh = ((double)pulses) / ((double)ikea_sparsnas_pulses_per_kwh);
     
     data_t *data;
     data = data_make(
         "model",         "Model",               DATA_STRING, "IKEA Sparsnäs Energy Meter Monitor",
         "id",            "Sensor ID",           DATA_INT, rcv_sensor_id,
-        "pulses_per_kwh", "Pulses per kWh",     DATA_INT, IKEA_SPARSNAS_PULSES_PER_KWH,
+        "pulses_per_kwh", "Pulses per kWh",     DATA_INT, ikea_sparsnas_pulses_per_kwh,
         "sequence",      "Sequence Number",     DATA_INT, sequence_number,
         "battery",       "Battery",             DATA_FORMAT, "%d%%", DATA_INT, battery,
         "cumulative_kWh", "Cumulative kWh",     DATA_FORMAT, "%7.3fkWh", DATA_DOUBLE,  cumulative_kWh,
