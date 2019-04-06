e365d4426cc5cfb6ac388af28678e7ce13d0cb02
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2019-04-06 18:04:14 +0200

    Refactor functions to API

--- include/r_api.h
+++ include/r_api.h
@@ -15,6 +15,10 @@
 #include <stdint.h>
 
 struct r_cfg;
+struct r_device;
+struct data;
+struct pulse_data;
+struct list;
 
 /* general */
 
@@ -25,5 +29,69 @@ struct r_cfg *r_create_cfg(void);
 void r_init_cfg(struct r_cfg *cfg);
 
 void r_free_cfg(struct r_cfg *cfg);
+
+/* device decoder protocols */
+
+void update_protocol(struct r_cfg *cfg, struct r_device *r_dev);
+
+void register_protocol(struct r_cfg *cfg, struct r_device *r_dev, char *arg);
+
+void free_protocol(struct r_device *r_dev);
+
+void unregister_protocol(struct r_cfg *cfg, struct r_device *r_dev);
+
+void register_all_protocols(struct r_cfg *cfg, unsigned disabled);
+
+void update_protocols(struct r_cfg *cfg);
+
+/* output helper */
+
+void calc_rssi_snr(struct r_cfg *cfg, struct pulse_data *pulse_data);
+
+char *time_pos_str(struct r_cfg *cfg, unsigned samples_ago, char *buf);
+
+char const **well_known_output_fields(struct r_cfg *cfg);
+
+char const **determine_csv_fields(struct r_cfg *cfg, char const **well_known, int *num_fields);
+
+int run_ook_demods(struct list *r_devs, struct pulse_data *pulse_data);
+
+int run_fsk_demods(struct list *r_devs, struct pulse_data *fsk_pulse_data);
+
+/* handlers */
+
+void event_occured_handler(struct r_cfg *cfg, struct data *data);
+
+void data_acquired_handler(struct r_device *r_dev, struct data *data);
+
+struct data *create_report_data(struct r_cfg *cfg, int level);
+
+void flush_report_data(struct r_cfg *cfg);
+
+/* setup */
+
+void add_json_output(struct r_cfg *cfg, char *param);
+
+void add_csv_output(struct r_cfg *cfg, char *param);
+
+void add_kv_output(struct r_cfg *cfg, char *param);
+
+void add_mqtt_output(struct r_cfg *cfg, char *param);
+
+void add_syslog_output(struct r_cfg *cfg, char *param);
+
+void add_null_output(struct r_cfg *cfg, char *param);
+
+void start_outputs(struct r_cfg *cfg, char const **well_known);
+
+void add_dumper(struct r_cfg *cfg, char const *spec, int overwrite);
+
+void add_infile(struct r_cfg *cfg, char *in_file);
+
+/* runtime */
+
+//void set_center_freq(struct r_cfg *cfg, uint32_t center_frequency);
+
+//void set_sample_rate(struct r_cfg *cfg, uint32_t samp_rate);
 
 #endif /* INCLUDE_R_API_H_ */
