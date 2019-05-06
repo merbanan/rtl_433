55df049e9ca575b998e400522ae6ba38bd50f497
klohner <klohner@thespill.com>
2019-05-06 16:38:22 -0400

    Add Companion WTR001 decoder

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -133,6 +133,7 @@
     DECL(tpms_jansite) \
     DECL(lacrosse_ws7000) \
     DECL(ts_ft002) \
+    DECL(companion_wtr001) \
     /* Add new decoders here. */
 
 #define DECL(name) extern r_device name;
