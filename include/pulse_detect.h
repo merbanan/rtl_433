7ecaa101539044edbc517c642d42e4ecb26762f6
Simon Lundell <s.lundell@gmail.com>
2019-06-16 10:07:23 +0200

    Fixed accidential removal of copyright comment

--- include/pulse_detect.h
+++ include/pulse_detect.h
@@ -1,3 +1,11 @@
+/** @file
+    Pulse detection functions.
+     Copyright (C) 2015 Tommy Vestermark
+     This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+*/
 
 #ifndef INCLUDE_PULSE_DETECT_H_
 #define INCLUDE_PULSE_DETECT_H_
