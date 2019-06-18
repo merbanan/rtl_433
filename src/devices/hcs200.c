01759b7b61538d12afcd5438c9296df9d58c26cb
667bdrm <416680+667bdrm@users.noreply.github.com>
2019-06-19 02:11:09 +0300

    Formatting fix

--- src/devices/hcs200.c
+++ src/devices/hcs200.c
@@ -15,20 +15,20 @@ static int hcs200_callback(r_device *decoder, bitbuffer_t *bitbuffer)
     uint8_t *b = bitbuffer->bb[0];
     char encrypted[9];
     int i;
-	uint32_t serial;
-	char serial_str[9];
+    uint32_t serial;
+    char serial_str[9];
 
     /* Reject codes of wrong length */
     if (78 != bitbuffer->bits_per_row[0])
       return 0;
 	
-	for (i = 1 ; i < 10; i++) {
-	   b[i] = ( b[i] << 4 ) | ((b[i+1] & 0xf0) >> 4);
+    for (i = 1 ; i < 10; i++) {
+      b[i] = ( b[i] << 4 ) | ((b[i+1] & 0xf0) >> 4);
     }
 	
-	serial = (((b[5] << 24) | (b[6] << 16) | (b[7] << 8) | (b[8] & 0x0F)) >> 4) & 0x0fffffff;
+    serial = (((b[5] << 24) | (b[6] << 16) | (b[7] << 8) | (b[8] & 0x0F)) >> 4) & 0x0fffffff;
 	
-	sprintf(encrypted, "%04X", (b[4] | (b[3] << 8) | (b[2] << 16) | (b[1] << 24)));
+    sprintf(encrypted, "%04X", (b[4] | (b[3] << 8) | (b[2] << 16) | (b[1] << 24)));
     sprintf(serial_str, "%04X", serial);
 
     data = data_make(
@@ -36,10 +36,10 @@ static int hcs200_callback(r_device *decoder, bitbuffer_t *bitbuffer)
         "serial",    "",     DATA_STRING,    serial_str,
         "encrypted", "",     DATA_STRING,    encrypted,
         "button1",    "",    DATA_STRING,     ((b[8] & 0x04) == 0x04) ? "ON" : "OFF",
-		"button2",    "",    DATA_STRING,     ((b[8] & 0x02) == 0x02) ? "ON" : "OFF",
-		"button3",    "",    DATA_STRING,     ((b[8] & 0x09) == 0x09) ? "ON" : "OFF",
-		"button4",    "",    DATA_STRING,     ((b[8] & 0x06) == 0x06) ? "ON" : "OFF",
-		"misc",       "",    DATA_STRING,     (b[8] == 0x0F) ? "ALL_PRESSED" : "",
+        "button2",    "",    DATA_STRING,     ((b[8] & 0x02) == 0x02) ? "ON" : "OFF",
+        "button3",    "",    DATA_STRING,     ((b[8] & 0x09) == 0x09) ? "ON" : "OFF",
+        "button4",    "",    DATA_STRING,     ((b[8] & 0x06) == 0x06) ? "ON" : "OFF",
+        "misc",       "",    DATA_STRING,     (b[8] == 0x0F) ? "ALL_PRESSED" : "",
         "battery",   "",     DATA_STRING,    (((b[9] >> 4) & 0x08) == 0x08) ? "LOW" : "OK",
         NULL);
     decoder_output_data(decoder, data);
@@ -54,7 +54,7 @@ static char *output_fields[] = {
     "button1",
     "button2",
     "button3",
-	"button4",
+    "button4",
     "misc",
     "battery",
     NULL
