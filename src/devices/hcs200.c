fa7cb8739080c57c3416dee07a6ac2abe35e58cb
667bdrm <416680+667bdrm@users.noreply.github.com>
2019-06-23 20:32:28 +0300

    Partial suggestions implementation

--- src/devices/hcs200.c
+++ src/devices/hcs200.c
@@ -20,56 +20,56 @@ static int hcs200_callback(r_device *decoder, bitbuffer_t *bitbuffer)
 
     /* Reject codes of wrong length */
     if (78 != bitbuffer->bits_per_row[0])
-      return 0;
+        return 0;
 	
     for (i = 1 ; i < 10; i++) {
-      b[i] = ( b[i] << 4 ) | ((b[i+1] & 0xf0) >> 4);
+        b[i] = ( b[i] << 4 ) | ((b[i+1] & 0xf0) >> 4);
     }
 	
     serial = (((b[5] << 24) | (b[6] << 16) | (b[7] << 8) | (b[8] & 0x0F)) >> 4) & 0x0fffffff;
 	
-    sprintf(encrypted, "%04X", (b[4] | (b[3] << 8) | (b[2] << 16) | (b[1] << 24)));
-    sprintf(serial_str, "%04X", serial);
+    sprintf(encrypted, "%08X", (b[4] | (b[3] << 8) | (b[2] << 16) | ((unsigned)b[1] << 24)));
+    sprintf(serial_str, "%08X", serial);
 
     data = data_make(
-        "model",     "",     DATA_STRING,    _X("HCS200","Microchip HCS200"),
-        "serial",    "",     DATA_STRING,    serial_str,
-        "encrypted", "",     DATA_STRING,    encrypted,
-        "button1",    "",    DATA_STRING,     ((b[8] & 0x04) == 0x04) ? "ON" : "OFF",
-        "button2",    "",    DATA_STRING,     ((b[8] & 0x02) == 0x02) ? "ON" : "OFF",
-        "button3",    "",    DATA_STRING,     ((b[8] & 0x09) == 0x09) ? "ON" : "OFF",
-        "button4",    "",    DATA_STRING,     ((b[8] & 0x06) == 0x06) ? "ON" : "OFF",
-        "misc",       "",    DATA_STRING,     (b[8] == 0x0F) ? "ALL_PRESSED" : "",
-        "battery",   "",     DATA_STRING,    (((b[9] >> 4) & 0x08) == 0x08) ? "LOW" : "OK",
-        NULL);
+            "model",     "",     DATA_STRING,    _X("HCS200","Microchip HCS200"),
+            "id",        "",     DATA_STRING,    serial_str,
+            "encrypted", "",     DATA_STRING,    encrypted,
+            "button1",    "",    DATA_STRING,     ((b[8] & 0x04) == 0x04) ? "ON" : "OFF",
+            "button2",    "",    DATA_STRING,     ((b[8] & 0x02) == 0x02) ? "ON" : "OFF",
+            "button3",    "",    DATA_STRING,     ((b[8] & 0x09) == 0x09) ? "ON" : "OFF",
+            "button4",    "",    DATA_STRING,     ((b[8] & 0x06) == 0x06) ? "ON" : "OFF",
+            "misc",       "",    DATA_STRING,     (b[8] == 0x0F) ? "ALL_PRESSED" : "",
+            "battery_ok", "",    DATA_INT,        (((b[9] >> 4) & 0x08) == 0x08) ? 0 : 1,
+            NULL);
     decoder_output_data(decoder, data);
 
     return 1;
 }
 
 static char *output_fields[] = {
-    "model",
-    "serial",
-    "encrypted",
-    "button1",
-    "button2",
-    "button3",
-    "button4",
-    "misc",
-    "battery",
-    NULL
+        "model",
+        "id",
+        "encrypted",
+        "button1",
+        "button2",
+        "button3",
+        "button4",
+        "misc",
+        "battery_ok",
+        NULL,
 };
 
 r_device hcs200 = {
-    .name           = "Microchip HCS200",
-    .modulation     = OOK_PULSE_PWM,
-    .short_width    = 370,
-    .long_width     = 772,
-    .gap_limit      = 4000,
-    .reset_limit    = 14000,
-    .sync_width     = 0,    // No sync bit used
-    .tolerance      = 152,  // us
-    .decode_fn      = &hcs200_callback,
-    .disabled       = 0,
-    .fields         = output_fields
+        .name           = "Microchip HCS200",
+        .modulation     = OOK_PULSE_PWM,
+        .short_width    = 370,
+        .long_width     = 772,
+        .gap_limit      = 4000,
+        .reset_limit    = 14000,
+        .sync_width     = 0,    // No sync bit used
+        .tolerance      = 152,  // us
+        .decode_fn      = &hcs200_callback,
+        .disabled       = 0,
+        .fields         = output_fields,
 };
