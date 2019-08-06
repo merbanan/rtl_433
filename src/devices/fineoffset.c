e9452cfcdf3f2472b39b6dd41c839e0ad3243699
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2019-08-06 11:52:59 +0200

    Change pm10_ug_m3 to pm10_0_ug_m3

--- src/devices/fineoffset.c
+++ src/devices/fineoffset.c
@@ -332,7 +332,7 @@ Data layout:
 - ?: 2 bits ?
 - P: 14 bit PM2.5 reading in ug/m3
 - ?: 2 bits ?
-- A: 14 bit PM10 reading in ug/m3
+- A: 14 bit PM10.0 reading in ug/m3
 - ?: 8 bits ?
 - C: 8 bit CRC checksum of the previous 6 bytes
 - B: 8 bit Bitsum (sum without carry, XOR) of the previous 7 bytes
@@ -377,7 +377,7 @@ static int fineoffset_WH0290_callback(r_device *decoder, bitbuffer_t *bitbuffer)
             "model",            "",             DATA_STRING, _X("Fineoffset-WH0290","Fine Offset Electronics, WH0290"),
             "id",               "ID",           DATA_INT,    id,
             "pm2_5_ug_m3",      "2.5um Fine Particulate Matter",  DATA_FORMAT, "%i ug/m3", DATA_INT, pm25/10,
-            "pm10_ug_m3",       "10um Coarse Particulate Matter",  DATA_FORMAT, "%i ug/m3", DATA_INT, pm100/10,
+            "pm10_0_ug_m3",     "10um Coarse Particulate Matter",  DATA_FORMAT, "%i ug/m3", DATA_INT, pm100/10,
             "mic",              "Integrity",    DATA_STRING, "CHECKSUM",
             NULL);
     /* clang-format on */
@@ -825,7 +825,7 @@ static char *output_fields_WH25[] = {
     "light_lux",
     //WH0290
     "pm2_5_ug_m3",
-    "pm10_ug_m3",
+    "pm10_0_ug_m3",
     "battery",
     "mic",
     NULL,
