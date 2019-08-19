c45896f612f049eb64f78f1cd52ee12341ba904a
winterrace <hauptadmin@gmail.com>
2019-08-19 21:44:20 +0200

    Remove stray tabs

--- src/devices/oil_watchman.c
+++ src/devices/oil_watchman.c
@@ -19,100 +19,100 @@ static const unsigned char preamble_pattern = 0xe0;
 static const unsigned char postamble_pattern[2] = { 0x00, 0xc0 };
 
 static int oil_watchman_callback(r_device *decoder, bitbuffer_t *bitbuffer) {
-	uint8_t *b;
-	uint32_t unit_id;
-	uint16_t depth = 0;
-	uint16_t binding_countdown = 0;
-	uint8_t flags;
-	uint8_t maybetemp;
-	double temperature;
-	data_t *data;
-	unsigned bitpos = 0;
-	bitbuffer_t databits = {0};
-	int events = 0;
+    uint8_t *b;
+    uint32_t unit_id;
+    uint16_t depth = 0;
+    uint16_t binding_countdown = 0;
+    uint8_t flags;
+    uint8_t maybetemp;
+    double temperature;
+    data_t *data;
+    unsigned bitpos = 0;
+    bitbuffer_t databits = {0};
+    int events = 0;
 
-	// Find a preamble with enough bits after it that it could be a complete packet
-	while ((bitpos = bitbuffer_search(bitbuffer, 0, bitpos, &preamble_pattern, 6)) + 136 <=
-			bitbuffer->bits_per_row[0]) {
+    // Find a preamble with enough bits after it that it could be a complete packet
+    while ((bitpos = bitbuffer_search(bitbuffer, 0, bitpos, &preamble_pattern, 6)) + 136 <=
+            bitbuffer->bits_per_row[0]) {
 
-		// Skip the matched preamble bits to point to the data
-		bitpos += 6;
+        // Skip the matched preamble bits to point to the data
+        bitpos += 6;
 
-		bitpos = bitbuffer_manchester_decode(bitbuffer, 0, bitpos, &databits, 64);
-		if (databits.bits_per_row[0] != 64)
-			continue;
+        bitpos = bitbuffer_manchester_decode(bitbuffer, 0, bitpos, &databits, 64);
+        if (databits.bits_per_row[0] != 64)
+            continue;
 
-		b = databits.bb[0];
+        b = databits.bb[0];
 
-		// Check for postamble, depending on last data bit
-		if (bitbuffer_search(bitbuffer, 0, bitpos, &postamble_pattern[b[7] & 1], 2) != bitpos)
-			continue;
+        // Check for postamble, depending on last data bit
+        if (bitbuffer_search(bitbuffer, 0, bitpos, &postamble_pattern[b[7] & 1], 2) != bitpos)
+            continue;
 
-		if (b[7] != crc8le(b, 7, 0x31, 0))
-			continue;
+        if (b[7] != crc8le(b, 7, 0x31, 0))
+            continue;
 
-		// The unit ID changes when you rebind by holding a magnet to the
-		// sensor for long enough; it seems to be time-based.
-		unit_id = (b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3];
+        // The unit ID changes when you rebind by holding a magnet to the
+        // sensor for long enough; it seems to be time-based.
+        unit_id = (b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3];
 
-		// 0x01: Rebinding (magnet held to sensor)
-		// 0x08: Leak/theft alarm
-		// top three bits seem also to vary with temperature (independently of maybetemp)
-		flags = b[4];
+        // 0x01: Rebinding (magnet held to sensor)
+        // 0x08: Leak/theft alarm
+        // top three bits seem also to vary with temperature (independently of maybetemp)
+        flags = b[4];
 
-		// Not entirely sure what this is but it might be inversely
-		// proportional to temperature.
-		maybetemp = b[5] >> 2;
-		temperature = (double)(145.0 - 5.0 * maybetemp) / 3.0;
-		if (flags & 1)
-			// When binding, the countdown counts up from 0x51 to 0x5a
-			// (as long as you hold the magnet to it for long enough)
-			// before the device ID changes. The receiver unit needs
-			// to receive this *strongly* in order to change its
-			// allegiance.
-			binding_countdown = b[6];
-		else
-			// A depth reading of zero indicates no reading. Even with
-			// the sensor flat down on a table, it still reads about 13.
-			depth = ((b[5] & 3) << 8) | b[6];
+        // Not entirely sure what this is but it might be inversely
+        // proportional to temperature.
+        maybetemp = b[5] >> 2;
+        temperature = (double)(145.0 - 5.0 * maybetemp) / 3.0;
+        if (flags & 1)
+            // When binding, the countdown counts up from 0x51 to 0x5a
+            // (as long as you hold the magnet to it for long enough)
+            // before the device ID changes. The receiver unit needs
+            // to receive this *strongly* in order to change its
+            // allegiance.
+            binding_countdown = b[6];
+        else
+            // A depth reading of zero indicates no reading. Even with
+            // the sensor flat down on a table, it still reads about 13.
+            depth = ((b[5] & 3) << 8) | b[6];
 
-		/* clang-format off */
-		data = data_make(
-				"model", "", DATA_STRING, _X("Oil-SonicSmart","Oil Watchman"),
-				"id", "", DATA_FORMAT, "%06x", DATA_INT, unit_id,
-				"flags", "", DATA_FORMAT, "%02x", DATA_INT, flags,
-				"maybetemp", "", DATA_INT, maybetemp,
-				"temperature_C", "", DATA_DOUBLE, temperature,
-				"binding_countdown", "", DATA_INT, binding_countdown,
-				_X("depth_cm","depth"), "", DATA_INT, depth,
-				NULL);
-		/* clang-format on */
+        /* clang-format off */
+        data = data_make(
+                "model", "", DATA_STRING, _X("Oil-SonicSmart","Oil Watchman"),
+                "id", "", DATA_FORMAT, "%06x", DATA_INT, unit_id,
+                "flags", "", DATA_FORMAT, "%02x", DATA_INT, flags,
+                "maybetemp", "", DATA_INT, maybetemp,
+                "temperature_C", "", DATA_DOUBLE, temperature,
+                "binding_countdown", "", DATA_INT, binding_countdown,
+                _X("depth_cm","depth"), "", DATA_INT, depth,
+                NULL);
+        /* clang-format on */
 
-		decoder_output_data(decoder, data);
-		events++;
-	}
-	return events;
+        decoder_output_data(decoder, data);
+        events++;
+    }
+    return events;
 }
 
 static char *output_fields[] = {
-		"model",
-		"id",
-		"flags",
-		"maybetemp",
-		"temperature_C",
-		"binding_countdown",
-		"depth", // TODO: remove this
-		"depth_cm",
-		NULL,
+        "model",
+        "id",
+        "flags",
+        "maybetemp",
+        "temperature_C",
+        "binding_countdown",
+        "depth", // TODO: remove this
+        "depth_cm",
+        NULL,
 };
 
 r_device oil_watchman = {
-		.name			= "Watchman Sonic / Apollo Ultrasonic / Beckett Rocket oil tank monitor",
-		.modulation		= FSK_PULSE_PCM,
-		.short_width	= 1000,
-		.long_width		= 1000, // NRZ
-		.reset_limit	= 4000,
-		.decode_fn		= &oil_watchman_callback,
-		.disabled		= 0,
-		.fields			= output_fields,
+        .name           = "Watchman Sonic / Apollo Ultrasonic / Beckett Rocket oil tank monitor",
+        .modulation     = FSK_PULSE_PCM,
+        .short_width    = 1000,
+        .long_width     = 1000, // NRZ
+        .reset_limit    = 4000,
+        .decode_fn      = &oil_watchman_callback,
+        .disabled       = 0,
+        .fields         = output_fields,
 };
--- src/devices/oregon_scientific_v1.c
+++ src/devices/oregon_scientific_v1.c
@@ -12,82 +12,82 @@
 
 #include "decoder.h"
 
-#define	OSV1_BITS	32
+#define OSV1_BITS   32
 
 static int oregon_scientific_v1_callback(r_device *decoder, bitbuffer_t *bitbuffer) {
-	int ret = 0;
-	int row;
-	int cs;
-	int i;
-	int nibble[OSV1_BITS/4];
-	int sid, channel, uk1;
-	float tempC;
-	int battery, uk2, sign, uk3, checksum;
-	data_t *data;
+    int ret = 0;
+    int row;
+    int cs;
+    int i;
+    int nibble[OSV1_BITS/4];
+    int sid, channel, uk1;
+    float tempC;
+    int battery, uk2, sign, uk3, checksum;
+    data_t *data;
 
-	for (row = 0; row < bitbuffer->num_rows; row++) {
-		if (bitbuffer->bits_per_row[row] != OSV1_BITS)
-			continue;
+    for (row = 0; row < bitbuffer->num_rows; row++) {
+        if (bitbuffer->bits_per_row[row] != OSV1_BITS)
+            continue;
 
-		cs = 0;
-		for (i = 0; i < OSV1_BITS / 8; i++) {
-			uint8_t byte = reverse8(bitbuffer->bb[row][i]);
-			nibble[i * 2    ] = byte & 0x0f;
-			nibble[i * 2 + 1] = byte >> 4;
-			if (i < ((OSV1_BITS / 8) - 1))
-				cs += nibble[i * 2] + 16 * nibble[i * 2 + 1];
-		}
+        cs = 0;
+        for (i = 0; i < OSV1_BITS / 8; i++) {
+            uint8_t byte = reverse8(bitbuffer->bb[row][i]);
+            nibble[i * 2    ] = byte & 0x0f;
+            nibble[i * 2 + 1] = byte >> 4;
+            if (i < ((OSV1_BITS / 8) - 1))
+                cs += nibble[i * 2] + 16 * nibble[i * 2 + 1];
+        }
 
-		cs = (cs & 0xFF) + (cs >> 8);
-		checksum = nibble[6] + (nibble[7] << 4);
-		if (checksum != cs)
-			continue;
+        cs = (cs & 0xFF) + (cs >> 8);
+        checksum = nibble[6] + (nibble[7] << 4);
+        if (checksum != cs)
+            continue;
 
-		sid      = nibble[0];
-		channel  = ((nibble[1] >> 2) & 0x03) + 1;
-		uk1      = (nibble[1] >> 0) & 0x03;	/* unknown.  Seen change every 60 minutes */
-		tempC    =  nibble[2] * 0.1 + nibble[3] + nibble[4] * 10.;
-		battery  = (nibble[5] >> 3) & 0x01;
-		uk2      = (nibble[5] >> 2) & 0x01;	/* unknown.  Always zero? */
-		sign     = (nibble[5] >> 1) & 0x01;
-		uk3      = (nibble[5] >> 0) & 0x01;	/* unknown.  Always zero? */
+        sid      = nibble[0];
+        channel  = ((nibble[1] >> 2) & 0x03) + 1;
+        uk1      = (nibble[1] >> 0) & 0x03; /* unknown.  Seen change every 60 minutes */
+        tempC    =  nibble[2] * 0.1 + nibble[3] + nibble[4] * 10.;
+        battery  = (nibble[5] >> 3) & 0x01;
+        uk2      = (nibble[5] >> 2) & 0x01; /* unknown.  Always zero? */
+        sign     = (nibble[5] >> 1) & 0x01;
+        uk3      = (nibble[5] >> 0) & 0x01; /* unknown.  Always zero? */
 
-		if (sign)
-			tempC = -tempC;
+        if (sign)
+            tempC = -tempC;
 
-		data = data_make(
-				"brand",		"",				DATA_STRING,	"OS",
-				"model",		"",				DATA_STRING,	_X("Oregon-v1","OSv1 Temperature Sensor"),
-				_X("id","sid"),			"SID",			DATA_INT,		sid,
-				"channel",		"Channel",		DATA_INT,		channel,
-				"battery",		"Battery",		DATA_STRING,	battery ? "LOW" : "OK",
-				"temperature_C","Temperature",	DATA_FORMAT,	"%.01f C",				DATA_DOUBLE,	tempC,
-				NULL);
-		decoder_output_data(decoder, data);
-		ret++;
-	}
-	return ret;
+        data = data_make(
+                "brand",        "",             DATA_STRING,    "OS",
+                "model",        "",             DATA_STRING,    _X("Oregon-v1","OSv1 Temperature Sensor"),
+                _X("id","sid"),         "SID",          DATA_INT,       sid,
+                "channel",      "Channel",      DATA_INT,       channel,
+                "battery",      "Battery",      DATA_STRING,    battery ? "LOW" : "OK",
+                "temperature_C","Temperature",  DATA_FORMAT,    "%.01f C",              DATA_DOUBLE,    tempC,
+                NULL);
+        decoder_output_data(decoder, data);
+        ret++;
+    }
+    return ret;
 }
 
 static char *output_fields[] = {
-	"brand",
-	"model",
-	"sid", // TODO: delete this
-	"id",
-	"channel",
-	"battery",
-	"temperature_C",
-	NULL
+    "brand",
+    "model",
+    "sid", // TODO: delete this
+    "id",
+    "channel",
+    "battery",
+    "temperature_C",
+    NULL
 };
 
 r_device oregon_scientific_v1 = {
-	.name           = "OSv1 Temperature Sensor",
-	.modulation     = OOK_PULSE_PWM_OSV1,
-	.short_width    = 1465, // nominal half-bit width
-	.sync_width     = 5780,
-	.gap_limit      = 3500,
-	.reset_limit    = 14000,
-	.decode_fn      = &oregon_scientific_v1_callback,
-	.disabled       = 0,
-	.fields         = output_fields
+    .name           = "OSv1 Temperature Sensor",
+    .modulation     = OOK_PULSE_PWM_OSV1,
+    .short_width    = 1465, // nominal half-bit width
+    .sync_width     = 5780,
+    .gap_limit      = 3500,
+    .reset_limit    = 14000,
+    .decode_fn      = &oregon_scientific_v1_callback,
+    .disabled       = 0,
+    .fields         = output_fields
 };
