3a762731c1d8e09d6b16a8f414df9a8676299877
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2019-08-27 11:58:48 +0000

    Add VuePress for docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  title: 'rtl_433',
+  description: 'generic data receiver for ISM/SRD bands.',
+  base: "/rtl_433/",
+  markdown: {
+    linkify: true
+  },
+  themeConfig: {
+    repo: 'merbanan/rtl_433',
+    displayAllHeaders: true,
+    sidebar: [
+      ['/', 'Overview'],
+      'BUILDING',
+      'CHANGELOG',
+      'DATA_FORMAT',
+      'EXAMPLES',
+      'TESTS',
+      'VS15'
+    ]
+  }
+};
---
+++ docs/build-docs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# abort on errors
+set -e
+
+# navigate to the docs directory
+cd ${0%/*}
+
+# copy other docs
+sed 's/docs\///' ../README.md >README.md
+cp ../CHANGELOG.md .
+cp ../examples/README.md EXAMPLES.md
+cp ../rtl_433_tests/README.md TESTS.md
+cp ../vs15/README.md VS15.md
+
+# build
+yarn install
+yarn docs:build
