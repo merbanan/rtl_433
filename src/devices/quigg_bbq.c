9fac98e57ef0fe0f9f56b4692cf46acb8bcf20f8
Olaf Glage <git@dieglages.de>
2019-09-15 16:12:18 +0200

    Added support for QUIGG GT-TMBBQ-05

---
+++ src/devices/quigg_bbq.c
@@ -0,0 +1,100 @@
+/* Quigg BBQ GT-TMBBQ-05
+
+BBQ thermometer sold at Aldi (germany)
+Simple device, no possibility to select channel. Single temperature measurement.
+
+The temperature is transmitted in Fahrenheit with an addon of 90. Accurycity is 10 bit. No decimals.  
+One data row contains 33 bits and is repeated 8 times. Each followed by a 0-row. So we have 16 rows in total.  
+First 9 bits seem to be a static header, I assume for sync purposes (001001001).
+Next 8 bits contain the lower 8 bits of the temperature.
+Next 8 bits are static. Purpose unknown. Maybe hard wired channel inside device? 
+Next 2 bits contain the upper 2 bits of the temperature
+Next 6 bits vary, but purpose is unknown. I assume a kind of CRC as they are the same for same temperatures.
+
+Here's the data I used to reverse engineer, more sampes in rtl_test
+001001001100010000111100110010110  HI
+001001001010101010111100110010000  507
+001001001010011010111100110010111  499
+001001001110101110111100101010110  381
+001001001110000000111100101011110  358
+001001001001011010111100101010001  211
+001001001001000000111100101000011  198
+001001001111010110111100100000110  145
+001001001101100010111100100001001  89
+001001001101011010111100100010101  83
+001001001101011010111100100010101  83
+001001001101010110111100100010011  81
+001001001101010010111100100000000  79
+001001001101010000111100100010000  78
+001001001101001110111100100011111  77
+001001001101001100111100100001101  76
+001001001101001010111100100001100  75
+001001001101001010111100100001100  75
+001001001101000110111100100001010  73
+001001001100010100111100100010000  48
+001001001011011110111100100000010  21
+001001001011001110111100100011011  13
+001001001010010010111100100011011  LO
+
+
+Frame structure:
+    Byte:             1        2        3        
+    Type:   001001001 tttttttt ssssssss TTcccccc 
+
+tt = temperature+90 F lower 8 bits
+TT = temperature+90 F upper 2 bits
+ss = unknown, static -> maybe channel?
+cc = unknown, changes -> maybe CRC?
+ */
+
+#include "decoder.h"
+
+
+static int quigg_bbq_callback(r_device *decoder, bitbuffer_t *bitbuffer) {
+    bitrow_t *bb = bitbuffer->bb;
+    data_t *data;
+
+    if (decoder->verbose > 1) {
+        fprintf(stderr,"Possible Quigg BBQ: ");
+        bitbuffer_print(bitbuffer);
+    }
+    
+    // 33 bit, repeated multiple times (technically it is repeated 8 times, look for 5 identical versions)
+    int r = bitbuffer_find_repeated_row(bitbuffer, 5, 33);
+
+    // we're looking for exactly 33 bits
+    if (r < 0 || bitbuffer->bits_per_row[r] != 33)
+        return 0;
+
+    // remove the 9 leading bits and extract the 3 bytes carrying the data
+    uint8_t data_bytes[3];
+    bitbuffer_extract_bytes(bitbuffer, r, 9, data_bytes, 24);
+
+    // concat the upper bits to the lower bits and substract the fixed offset 90 
+    uint16_t tempf = ((data_bytes[2]>>6) | data_bytes[0]) -90;
+
+    data = data_make(
+                "model",         "",            DATA_STRING, _X("QUIGG","GT-TMBBQ-05"),
+                "temperature_F", "Temperature", DATA_FORMAT, "%.02f F", DATA_DOUBLE, (double)tempf,
+                NULL);
+    decoder_output_data(decoder, data);
+    return 1;
+}
+
+static char *output_fields[] = {
+    "model",
+    "temperature_F",
+    NULL
+};
+
+r_device quigg_bbq = {
+    .name           = "QUIGG BBQ",
+    .modulation     = OOK_PULSE_PPM,
+    .short_width    = 2000,
+    .long_width     = 4000,
+    .gap_limit      = 4100,
+    .reset_limit    = 9000,
+    .decode_fn      = &quigg_bbq_callback,
+    .disabled       = 0,
+    .fields         = output_fields
+};
