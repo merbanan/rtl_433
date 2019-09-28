7e30b4c24316ccf8544b63c7f6e6b1b0cfa9cc92
Kumar Vivek <kv2000in@mac.kv>
2019-09-28 18:14:11 -0400

    somewhat working. able to print out ID via printf

--- src/devices/tpms_ford.c
+++ src/devices/tpms_ford.c
@@ -51,8 +51,10 @@
 
 #include "decoder.h"
 
-/*preamble = 111000101010101 0x71 0x55, inverted = 000111010101010 0x0e 0xaa */
-static const uint8_t preamble_pattern[2] = { 0x0e, 0xaa }; // 15 bits
+/*preamble = 111000101010101 0x71 0x55, inverted = 000 111010101010 0x0e 0xaa */
+//Full preamble mx sensor = 11011010111000101010101
+// inverted 00100101000111010101010 12 8E aa
+static const uint8_t preamble_pattern[2] = {0x71,0x55}; // 31 bits
 
 static int tpms_ford_decode(r_device *decoder, bitbuffer_t *bitbuffer, unsigned row, unsigned bitpos)
 {
@@ -67,33 +69,50 @@ static int tpms_ford_decode(r_device *decoder, bitbuffer_t *bitbuffer, unsigned 
 	unsigned status, pressure1, pressure2, temp, battery_low, counter, failed;
 	float pressure_kpa, temperature_c;
 	int crc;
+
 	/*unsigned bitbuffer_manchester_decode(bitbuffer_t *inbuf, unsigned row, unsigned start,
 	 bitbuffer_t *outbuf, unsigned max)*/
 	
-    start_pos = bitbuffer_manchester_decode(bitbuffer, row, bitpos, &packet_bits, 160);
+    start_pos = bitbuffer_manchester_decode(bitbuffer, row, bitpos, &packet_bits, 64);
     // require 64 data bits
-    if (start_pos-bitpos < 128) {
-        return 0;
-    }
-    b = packet_bits.bb[0];
-
+	
+	printf("manchester decode called");
+	printf("start_pos=%d",start_pos);
+	if (start_pos-bitpos < 128) {
+		return 0;
+	}
+	unsigned z=start_pos-bitpos;
+	printf("size=%d",z);
+	b = packet_bits.bb[0];
+	
+	printf("byte 0=%d",b[0]);
+	printf("byte 1=%d",b[1]);
+	printf("byte 2=%d",b[2]);
+	printf("byte 3=%d",b[3]);
+	printf("byte 4=%d",b[4]);
+	printf("byte 5=%d",b[5]);
+	printf("byte 6=%d",b[6]);
+	printf("byte 7=%d",b[7]);
+	
+	
    /* if (((b[0]+b[1]+b[2]+b[3]+b[4]+b[5]+b[6]) & 0xff) != b[7]) {
         return 0;
     }*/
 	//uint8_t crc8(uint8_t const message[], unsigned nBytes, uint8_t polynomial, uint8_t init)
-	crc = b[7];
-	if (crc8(b, 7, 0x07, 0x00) != crc) {
-		return 0;
-	}
+	//crc = b[7];
+	//if (crc8(b, 7, 0x07, 0x00) != crc) {
+	//	return 0;
+	//}
+	
+	//printf("CRC passed");
     id = b[2]<<24 | b[3]<<16 | b[4]<<8 | b[5];
-    sprintf(id_str, "%08x", id);
+    //sprintf(id_str, "%08x", id);
+	printf("%08x", id);
 
     data = data_make(
         "model",        "",     DATA_STRING, "Elantra",
         "type",         "",     DATA_STRING, "TPMS",
         "id",           "",     DATA_STRING, id_str,
-        "code",         "",     DATA_STRING, code_str,
-        "mic",          "",     DATA_STRING, "CHECKSUM",
         NULL);
 
     decoder_output_data(decoder, data);
@@ -104,19 +123,25 @@ static int tpms_ford_callback(r_device *decoder, bitbuffer_t *bitbuffer) {
     int row;
     unsigned bitpos;
     int events = 0;
-
-    bitbuffer_invert(bitbuffer);
-
+	printf("main function called");
+	//bitbuffer_invert(bitbuffer);
+	//printf("inverted");
+	
     for (row = 0; row < bitbuffer->num_rows; ++row) {
         bitpos = 0;
+		bitbuffer_print(bitbuffer);
 		/*unsigned bitbuffer_search(bitbuffer_t *bitbuffer, unsigned row, unsigned start,
 		 const uint8_t *pattern, unsigned pattern_bits_len)*/
 		// Find a preamble with enough bits after it that it could be a complete packet
         while ((bitpos = bitbuffer_search(bitbuffer, row, bitpos,
-                (const uint8_t *)&preamble_pattern, 15)) + 128 <=
+                (const uint8_t *)&preamble_pattern, 16)) + 128 <=
                 bitbuffer->bits_per_row[row]) {
-            events += tpms_ford_decode(decoder, bitbuffer, row, bitpos + 15);
-            bitpos += 14;
+			printf("bit_pos=%d",bitpos);
+			//bitrow_t tmp;
+			//bitbuffer_extract_bytes(bitbuffer, row, bitpos, tmp, len);
+			bitbuffer_print(bitbuffer);
+			events += tpms_ford_decode(decoder, bitbuffer, row, bitpos + 16);
+            bitpos += 15;
         }
     }
 
@@ -127,14 +152,12 @@ static char *output_fields[] = {
     "model",
     "type",
     "id",
-    "code",
-    "mic",
     NULL
 };
 
 r_device tpms_ford = {
     .name           = "Elantra TPMS",
-    .modulation     = FSK_PCM,
+    .modulation     = FSK_PULSE_PCM,
     .short_width    = 49, // 12-13 samples @250k
     .long_width     = 49, // FSK
     .reset_limit    = 50000, // Maximum gap size before End Of Message [us].
