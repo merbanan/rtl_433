2e59af9bc1c6d60267fcbdb1b7e64b427bb265d9
Christian W. Zuckschwerdt <zany@triq.net>
2019-10-14 08:22:55 +0200

    Enhance Honeywell for 2Gig, RE208 (#747)

--- src/devices/honeywell.c
+++ src/devices/honeywell.c
@@ -1,83 +1,136 @@
-/*
-* *** Honeywell (Ademco) Door/Window Sensors (345.0Mhz) ***
-*
-* Tested with the Honeywell 5811 Wireless Door/Window transmitters
-*
-* 64 bit packets, repeated multiple times per open/close event
-*
-* Protocol whitepaper: "DEFCON 22: Home Insecurity" by Logan Lamb
-*
-* PP PP C IIIII EE SS SS
-* P: 16bit Preamble and sync bit (always ff fe)
-* C: 4bit Channel
-* I: 20bit Device serial number / or counter value
-* E: 8bit Event, where 0x80 = Open/Close, 0x04 = Heartbeat / or id
-* S: 16bit CRC
-*
+/** @file
+    Honeywell (Ademco) Door/Window Sensors (345Mhz).
+
+    Copyright (C) 2016 adam1010
+    Copyright (C) 2019 Christian W. Zuckschwerdt <zany@triq.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+*/
+/**
+Honeywell (Ademco) Door/Window Sensors (345.0Mhz).
+
+Tested with the Honeywell 5811 Wireless Door/Window transmitters.
+
+Also: 2Gig DW10 door sensors,
+and Resolution Products RE208 (wire to air repeater).
+
+Maybe: 5890PI?
+
+64 bit packets, repeated multiple times per open/close event.
+
+Protocol whitepaper: "DEFCON 22: Home Insecurity" by Logan Lamb.
+
+Data layout:
+
+    PP PP C IIIII EE SS SS
+
+- P: 16bit Preamble and sync bit (always ff fe)
+- C: 4bit Channel
+- I: 20bit Device serial number / or counter value
+- E: 8bit Event, where 0x80 = Open/Close, 0x04 = Heartbeat / or id
+- S: 16bit CRC
+
 */
 
 #include "decoder.h"
 
-static int honeywell_callback(r_device *decoder, bitbuffer_t *bitbuffer) {
-    const uint8_t *bb;
+static int honeywell_decode(r_device *decoder, bitbuffer_t *bitbuffer)
+{
+    // full preamble is 0xFFFE
+    uint8_t const preamble_pattern[2] = {0xff, 0xe0}; // 12 bits
+
+    data_t *data;
+    int row;
+    int pos;
+    uint8_t b[6] = {0};
     int channel;
     int device_id;
     int event;
-    int state;
-    int heartbeat;
     uint16_t crc_calculated;
     uint16_t crc;
+    int state;
+    int heartbeat;
+    int alarm;
+    int tamper;
+    int battery_low;
 
-    if(bitbuffer->num_rows != 1 || bitbuffer->bits_per_row[0] != 64)
-        return 0; // Unrecognized data
+    row = 0; // we expect a single row only. reduce collisions
+    if (bitbuffer->num_rows != 1 || bitbuffer->bits_per_row[row] < 60)
+        return DECODE_ABORT_LENGTH;
 
-    for(uint16_t i=0; i < 8; i++)
-        bitbuffer->bb[0][i] = ~bitbuffer->bb[0][i];
+    bitbuffer_invert(bitbuffer);
 
-    bb = bitbuffer->bb[0];
+    pos = bitbuffer_search(bitbuffer, row, 0, preamble_pattern, 12) + 12;
+    if (pos >= bitbuffer->bits_per_row[row])
+        return DECODE_ABORT_LENGTH;
+    bitbuffer_extract_bytes(bitbuffer, row, pos, b, 48);
 
-    crc_calculated = crc16(bb, 6, 0x8005, 0xfffe);
-    crc = (((uint16_t) bb[6]) << 8) + ((uint16_t) bb[7]);
-    if(crc != crc_calculated)
-        return 0; // Not a valid packet
+    channel   = b[0] >> 4;
+    device_id = ((b[0] & 0xf) << 16) | (b[1] << 8) | b[2];
+    crc       = (b[4] << 8) | b[5];
 
-    channel = bb[2] >> 4;
-    device_id = ((bb[2] & 0xf) << 16) | (bb[3] << 8)| bb[4];
-    event = bb[5];
-    state = (event & 0x80) >> 7;
-    heartbeat = (event & 0x04) >> 2;
+    if (device_id == 0 && crc == 0)
+        return DECODE_ABORT_EARLY; // Reduce collisions
 
+    if (channel == 0x2 || channel == 0xA) {
+        // 2GIG brand
+        crc_calculated = crc16(b, 4, 0x8050, 0);
+    } else { // channel == 0x8
+        crc_calculated = crc16(b, 4, 0x8005, 0);
+    }
+    if (crc != crc_calculated)
+        return DECODE_FAIL_MIC; // Not a valid packet
 
-    data_t *data = data_make(
-          "model", "", DATA_STRING, _X("Honeywell-Security","Honeywell Door/Window Sensor"),
-          "id",       "", DATA_FORMAT, "%05x", DATA_INT, device_id,
-          "channel","", DATA_INT, channel,
-          "event","", DATA_FORMAT, "%02x", DATA_INT, event,
-          "state",    "", DATA_STRING, state ? "open" : "closed",
-          "heartbeat" , "", DATA_STRING, heartbeat ? "yes" : "no",
-          NULL);
+    event = b[3];
+    // decoded event bits: AATABHUU
+    // NOTE: not sure if these apply to all device types
+    state       = (event & 0x80) >> 7;
+    alarm       = (event & 0xb0) >> 4;
+    tamper      = (event & 0x40) >> 6;
+    battery_low = (event & 0x08) >> 3;
+    heartbeat   = (event & 0x04) >> 2;
+
+    /* clang-format off */
+    data = data_make(
+            "model",        "", DATA_STRING, _X("Honeywell-Security","Honeywell Door/Window Sensor"),
+            "id",           "", DATA_FORMAT, "%05x", DATA_INT, device_id,
+            "channel",      "", DATA_INT,    channel,
+            "event",        "", DATA_FORMAT, "%02x", DATA_INT, event,
+            "state",        "", DATA_STRING, state ? "open" : "closed",
+            "alarm",        "", DATA_INT,    alarm,
+            "tamper",       "", DATA_INT,    tamper,
+            "battery_ok",   "", DATA_INT,    !battery_low,
+            "heartbeat",    "", DATA_INT,    heartbeat,
+            NULL);
+    /* clang-format on */
 
     decoder_output_data(decoder, data);
     return 1;
 }
 
 static char *output_fields[] = {
-    "model",
-    "id",
-    "channel",
-    "event",
-    "state",
-    "heartbeat",
-    NULL
+        "model",
+        "id",
+        "channel",
+        "event",
+        "state",
+        "alarm",
+        "tamper",
+        "battery_ok",
+        "heartbeat",
+        NULL,
 };
 
 r_device honeywell = {
-    .name                   = "Honeywell Door/Window Sensor",
-    .modulation             = OOK_PULSE_MANCHESTER_ZEROBIT,
-    .short_width    =   39 * 4,
-    .long_width             = 0,
-    .reset_limit    = 73 * 4,
-    .decode_fn      = &honeywell_callback,
-    .disabled               = 0,
-    .fields                 = output_fields,
+        .name        = "Honeywell Door/Window Sensor",
+        .modulation  = OOK_PULSE_MANCHESTER_ZEROBIT,
+        .short_width = 156,
+        .long_width  = 0,
+        .reset_limit = 292,
+        .decode_fn   = &honeywell_decode,
+        .disabled    = 0,
+        .fields      = output_fields,
 };
