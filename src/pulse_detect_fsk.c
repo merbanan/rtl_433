a664d0baac333132f00cda3925e8422fe1b6950d
Benjamin Larsson <banan@ludd.ltu.se>
2019-11-02 00:37:09 +0100

    fsk: add min/max fsk pulse detector

--- src/pulse_detect_fsk.c
+++ src/pulse_detect_fsk.c
@@ -164,3 +164,65 @@ void pulse_FSK_wrap_up(pulse_data_t *fsk_pulses, pulse_FSK_state_t *s)
         fsk_pulses->num_pulses++;
     }
 }
+
+
+/// Demodulate Frequency Shift Keying (FSK) sample by sample
+///
+/// Function is stateful between calls
+/// @param fm_n: One single sample of FM data
+/// @param *fsk_pulses: Will return a pulse_data_t structure for FSK demodulated data
+/// @param *s: Internal state
+void pulse_FSK_detect_mm(int16_t fm_n, pulse_data_t *fsk_pulses, pulse_FSK_state_t *s){
+
+    int16_t mid = 0;
+
+    /* Skip a few samples in the beginning, need for framing
+     * otherwise the min/max trackers wont converge properly
+     */
+    if (!s->skip_samples) {
+        s->var_test_max = MAX(fm_n, s->var_test_max);
+        s->var_test_min = MIN(fm_n, s->var_test_min);
+        mid = (s->var_test_max + s->var_test_min) / 2;
+        if (fm_n > mid)
+            s->var_test_max -= 10;
+        if (fm_n < mid)
+            s->var_test_min += 10;
+
+        s->fsk_pulse_length++;
+        switch(s->fsk_state) {
+            case PD_FSK_STATE_INIT:
+                if (fm_n > mid)
+                    s->fsk_state = PD_FSK_STATE_FH;
+                if (fm_n <= mid)
+                    s->fsk_state = PD_FSK_STATE_FL;
+                break;
+            case PD_FSK_STATE_FH:
+                if (fm_n < mid) {
+                    s->fsk_state = PD_FSK_STATE_FL;
+                    fsk_pulses->pulse[fsk_pulses->num_pulses] = s->fsk_pulse_length;
+                    s->fsk_pulse_length = 0;
+                }
+                break;
+            case PD_FSK_STATE_FL:
+                if (fm_n > mid) {
+                    s->fsk_state = PD_FSK_STATE_FH;
+                    fsk_pulses->gap[fsk_pulses->num_pulses] = s->fsk_pulse_length;
+                    fsk_pulses->num_pulses++;
+                    s->fsk_pulse_length = 0;
+                    // When pulse buffer is full go to error state
+                    if (fsk_pulses->num_pulses >= PD_MAX_PULSES) {
+                        fprintf(stderr, "pulse_FSK_detect(): Maximum number of pulses reached!\n");
+                        s->fsk_state = PD_FSK_STATE_ERROR;
+                    }
+                }
+                break;
+            case PD_FSK_STATE_ERROR:        // Stay here until cleared
+                break;
+            default:
+                fprintf(stderr, "pulse_FSK_detect(): Unknown FSK state!!\n");
+                s->fsk_state = PD_FSK_STATE_ERROR;
+                break;
+        }
+    }
+    if (s->skip_samples > 0) s->skip_samples--;
+}
