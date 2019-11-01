620ca8c0fd8b7778c0ed349c62ea285534dbb441
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2019-11-01 08:32:28 +0100

    Cleanup minor

--- include/pulse_detect_fsk.h
+++ include/pulse_detect_fsk.h
@@ -11,5 +11,10 @@
     (at your option) any later version.
 */
 
+#ifndef INCLUDE_PULSE_DETECT_FSK_H_
+#define INCLUDE_PULSE_DETECT_FSK_H_
+
 void pulse_FSK_detect(int16_t fm_n, pulse_data_t *fsk_pulses, pulse_FSK_state_t *s);
 void pulse_FSK_wrap_up(pulse_data_t *fsk_pulses, pulse_FSK_state_t *s);
+
+#endif /* INCLUDE_PULSE_DETECT_FSK_H_ */
