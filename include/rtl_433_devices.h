84fb94bcb63c36f4b5f7b20188ace702f8079b0d
Benjamin Larsson <banan@ludd.ltu.se>
2020-02-11 00:42:36 +0100

    Add ERT SCM protocol decoder

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -155,7 +155,8 @@
     DECL(ws2032) \
     DECL(auriol_afw2a1) \
     DECL(tfa_drop_303233) \
-    DECL(dsc_security_ws4945)
+    DECL(dsc_security_ws4945) \
+    DECL(ert_amr)
 
     /* Add new decoders here. */
 
