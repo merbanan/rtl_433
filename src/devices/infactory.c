6e6b21b3861953a9a795b3d4bff8c36bb87f72e7
Hagen Patzke <hpatzke@gmx.net>
2020-02-21 15:24:14 +0100

    add CRC check, battery, power-on and datagram field

--- src/devices/infactory.c
+++ src/devices/infactory.c
@@ -1,8 +1,10 @@
 /** @file
     inFactory outdoor temperature and humidity sensor.
+    Other brand names: nor-tec, GreenBlue
 
     Copyright (C) 2017 Sirius Weiß <siriuz@gmx.net>
     Copyright (C) 2017 Christian W. Zuckschwerdt <zany@triq.net>
+    Copyright (C) 2020 Hagen Patzke <hpatzke@gmx.net>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -10,20 +12,21 @@
     (at your option) any later version.
 */
 /**
-inFactory Outdoor sensor transmits data temperature, humidity.
+inFactory Outdoor sensor transmits data temperature, humidity,
+          NC-3982-913 from Pearl (for FWS-686 station)
 
-Also NC-3982-913 from Pearl (for FWS-686 station).
-
-Transmissions also includes an id. The sensor transmits
-every 60 seconds 6 packets.
+Transmissions also includes an id. Every 60 seconds
+the sensor transmits 6 packets:
 
     0000 1111 | 0011 0000 | 0101 1100 | 1110 0111 | 0110 0001
-    xxxx xxxx | cccc cccc | tttt tttt | tttt hhhh | hhhh ??nn
+    iiii iiii | cccc pw?? | tttt tttt | tttt hhhh | hhhh ??nn
 
-- x: ID // changes on battery switch
-- c: Unknown Checksum (changes on every transmit if the other values are different)
-- h: Humidity // BCD-encoded, each nibble is one digit
-- t: Temperature   // in °F as binary number with one decimal place + 90 °F offset
+- i: identification // changes on battery switch
+- c: CRC-4 // CCITT checksum, see below for computation specifics
+- p: power-on // flag to indicate power-on (e.g. battery change)
+- w: weak-battery // flag to indicate low battery voltage
+- h: Humidity // BCD-encoded, each nibble is one digit, 'A0' means 100%rH
+- t: Temperature // in °F as binary number with one decimal place + 90 °F offset
 - n: Channel // Channel number 1 - 3
 
 Usage:
@@ -35,18 +38,55 @@ Payload looks like this:
     [00] { 4} 00             : 0000
     [01] {40} 0f 30 5c e7 61 : 00001111 00110000 01011100 11100111 01100001
 
-First row is actually the preamble part. This is added to make the signal more unique.
+First row is actually the preamble part. This makes the signal more unique
+but also trains the receiver AGC (automatic gain control).
 */
 
 #include "decoder.h"
+
+static void infactory_raw_msg(char *msg, uint8_t *b) {
+    int i;
+    char c;
+    char *p = msg;
+    for (i = 0; i < 10; i++) {
+        c = ((b[i >> 1]) >> (4 * (1 - (i & 1)))) & 0x0F;
+        c = (c > 9) ? c + 55 : c + 48;
+        *p++ = c;
+    }
+    *p = 0;
+}
+
+static char* infactory_crc_check(uint8_t *b) {
+    int i;
+    uint8_t msg_crc, crc;
+    uint8_t msg[5];
+    memcpy(msg, b, 5);
+    msg_crc = msg[1] >> 4;
+    // for CRC computation, channel bits are at CRC position(!)
+    msg[1] = (msg[1] & 0x0F) | (msg[4] & 0x0F) << 4;
+    crc = msg[0] >> 4;
+    for ( i = 4; i < 36; i++) {
+        crc <<= 1;
+        if (msg[i>>3] & (0x80 >> (i&7))) {
+            crc |= 1; 
+        }
+        if (crc & 0x10) {
+            crc ^= 0x13; // Koopmann 0x9, CCITT-4; FP-4; ITU-T G.704
+        }
+    }
+
+    return (crc == msg_crc) ? "CRC_OK" : "CRC_ERROR";
+}
 
 static int infactory_callback(r_device *decoder, bitbuffer_t *bitbuffer)
 {
     bitrow_t *bb;
     data_t *data;
     uint8_t *b;
-    int id, humidity, temp_raw, channel;
+    int id, humidity, temp_raw, channel, pwron, weakbatt;
     float temp_f;
+    char *mic = "ERROR";
+    char raw_msg[11] = ".........."; // 10 hex chars plus 0
 
     if (bitbuffer->bits_per_row[0] != 4) {
         return DECODE_ABORT_LENGTH;
@@ -56,29 +96,38 @@ static int infactory_callback(r_device *decoder, bitbuffer_t *bitbuffer)
     }
     bb = bitbuffer->bb;
 
-    /* Check that 4 bits of preamble is 0 */
+    /* Check that 4 preamble bits are 0 */
     b = bb[0];
     if (b[0]&0x0F)
         return DECODE_ABORT_EARLY;
 
-    /* Check that last 4 bits of message is 0x1 */
+    /* Check that the last 4 bits of message are not 0 (channel number) */
     b = bb[1];
     if (!(b[4]&0x0F))
         return DECODE_ABORT_EARLY;
 
     id       = b[0];
-    humidity = (b[3] & 0x0F) * 10 + (b[4] >> 4); // BCD
+    pwron    = (b[1] >> 3) & 1;
+    weakbatt = (b[1] >> 2) & 1;
+    humidity = (b[3] & 0x0F) * 10 + (b[4] >> 4); // BCD, 'A0'=100%rH
     temp_raw = (b[2] << 4) | (b[3] >> 4);
     temp_f   = (float)temp_raw * 0.1 - 90;
     channel  = b[4] & 0x03;
+
+    mic = infactory_crc_check(b);
+    infactory_raw_msg(raw_msg, b);
 
     /* clang-format off */
     data = data_make(
             "model",            "",             DATA_STRING, _X("inFactory-TH","inFactory sensor"),
             "id",               "ID",           DATA_INT, id,
             "channel",          "Channel",      DATA_INT, channel,
+            "weakbatt",         "Battery",      DATA_INT, weakbatt,
             "temperature_F",    "Temperature",  DATA_FORMAT, "%.02f °F", DATA_DOUBLE, temp_f,
             "humidity",         "Humidity",     DATA_FORMAT, "%u %%", DATA_INT, humidity,
+            "mic",              "Integrity",    DATA_STRING, mic,
+            "poweron",          "Power-On",     DATA_INT, pwron,
+            "rawmsg",           "Datagram",     DATA_STRING, raw_msg,
             NULL);
     /* clang-format on */
 
@@ -92,11 +141,15 @@ static char *output_fields[] = {
         "channel",
         "temperature_F",
         "humidity",
+        "mic",
+        "poweron",
+        "weakbatt",
+        "rawmsg",
         NULL,
 };
 
 r_device infactory = {
-        .name        = "inFactory, FreeTec NC-3982-913 temperature humidity sensor",
+        .name        = "inFactory, nor-tec, FreeTec NC-3982-913 temperature humidity sensor",
         .modulation  = OOK_PULSE_PPM,
         .short_width = 1850,
         .long_width  = 4050,
