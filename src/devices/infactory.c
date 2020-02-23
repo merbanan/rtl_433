83039f85991ab49c6813ac1dd38c6cd683f0e83a
Hagen Patzke <hpatzke@gmx.net>
2020-02-23 18:28:29 +0100

    clean-up, new timings

--- src/devices/infactory.c
+++ src/devices/infactory.c
@@ -1,6 +1,6 @@
 /** @file
     inFactory outdoor temperature and humidity sensor.
-    Other brand names: nor-tec, GreenBlue
+    Other brand names: nor-tec, GreenBlue, DAY
 
     Copyright (C) 2017 Sirius Weiß <siriuz@gmx.net>
     Copyright (C) 2017 Christian W. Zuckschwerdt <zany@triq.net>
@@ -12,19 +12,22 @@
     (at your option) any later version.
 */
 /**
-inFactory Outdoor sensor transmits data temperature, humidity,
-          NC-3982-913 from Pearl (for FWS-686 station)
+Outdoor sensor, transmits temperature and humidity data
+- inFactory NC-3982-913/NX-5817-902, Pearl (for FWS-686 station)
+- nor-tec 73383 (weather station + sensor), Schou Company AS, Denmark
+- DAY 73365 (weather station + sensor), Schou Company AS, Denmark
+
 
 Transmissions also includes an id. Every 60 seconds
 the sensor transmits 6 packets:
 
     0000 1111 | 0011 0000 | 0101 1100 | 1110 0111 | 0110 0001
-    iiii iiii | cccc pw?? | tttt tttt | tttt hhhh | hhhh ??nn
+    iiii iiii | cccc ub?? | tttt tttt | tttt hhhh | hhhh ??nn
 
 - i: identification // changes on battery switch
 - c: CRC-4 // CCITT checksum, see below for computation specifics
-- p: power-on // flag to indicate power-on (e.g. battery change)
-- w: weak-battery // flag to indicate low battery voltage
+- u: unknown // (sometimes set at power-on, but not always)
+- b: battery low // flag to indicate low battery voltage
 - h: Humidity // BCD-encoded, each nibble is one digit, 'A0' means 100%rH
 - t: Temperature // in °F as binary number with one decimal place + 90 °F offset
 - n: Channel // Channel number 1 - 3
@@ -32,14 +35,14 @@ the sensor transmits 6 packets:
 Usage:
 
     # rtl_433 -f 434052000 -R 91 -F json:log.json
+    # rtl_433 -R 91 -F json:log.json
+    # rtl_433 -C si
 
 Payload looks like this:
 
-    [00] { 4} 00             : 0000
-    [01] {40} 0f 30 5c e7 61 : 00001111 00110000 01011100 11100111 01100001
+    [00] {40} 0f 30 5c e7 61 : 00001111 00110000 01011100 11100111 01100001
 
-First row is actually the preamble part. This makes the signal more unique
-but also trains the receiver AGC (automatic gain control).
+(See below for more information about the signal timing.)
 */
 
 #include "decoder.h"
@@ -62,7 +65,7 @@ static char* infactory_crc_check(uint8_t *b) {
     uint8_t msg[5];
     memcpy(msg, b, 5);
     msg_crc = msg[1] >> 4;
-    // for CRC computation, channel bits are at CRC position(!)
+    // for CRC computation, channel bits are at the CRC position(!)
     msg[1] = (msg[1] & 0x0F) | (msg[4] & 0x0F) << 4;
     crc = msg[0] >> 4;
     for ( i = 4; i < 36; i++) {
@@ -75,7 +78,7 @@ static char* infactory_crc_check(uint8_t *b) {
         }
     }
 
-    return (crc == msg_crc) ? "CRC_OK" : "CRC_ERROR";
+    return (crc == msg_crc);
 }
 
 static int infactory_callback(r_device *decoder, bitbuffer_t *bitbuffer)
@@ -83,50 +86,41 @@ static int infactory_callback(r_device *decoder, bitbuffer_t *bitbuffer)
     bitrow_t *bb;
     data_t *data;
     uint8_t *b;
-    int id, humidity, temp_raw, channel, pwron, weakbatt;
+    int id, battery, temp_raw, humidity, channel;
     float temp_f;
     char *mic = "ERROR";
     char raw_msg[11] = ".........."; // 10 hex chars plus 0
 
-    if (bitbuffer->bits_per_row[0] != 4) {
-        return DECODE_ABORT_LENGTH;
-    }
-    if (bitbuffer->bits_per_row[1] != 40) {
+    if (bitbuffer->bits_per_row[0] != 40) {
         return DECODE_ABORT_LENGTH;
     }
     bb = bitbuffer->bb;
 
-    /* Check that 4 preamble bits are 0 */
+    /* Check that the last 4 bits of message are not 0 (channel number 1 - 3) */
     b = bb[0];
-    if (b[0]&0x0F)
-        return DECODE_ABORT_EARLY;
-
-    /* Check that the last 4 bits of message are not 0 (channel number) */
-    b = bb[1];
     if (!(b[4]&0x0F))
         return DECODE_ABORT_EARLY;
 
     id       = b[0];
-    pwron    = (b[1] >> 3) & 1;
-    weakbatt = (b[1] >> 2) & 1;
-    humidity = (b[3] & 0x0F) * 10 + (b[4] >> 4); // BCD, 'A0'=100%rH
+    battery  = (b[1] >> 2) & 1;
     temp_raw = (b[2] << 4) | (b[3] >> 4);
-    temp_f   = (float)temp_raw * 0.1 - 90;
+    humidity = (b[3] & 0x0F) * 10 + (b[4] >> 4); // BCD, 'A0'=100%rH
     channel  = b[4] & 0x03;
 
-    mic = infactory_crc_check(b);
-    infactory_raw_msg(raw_msg, b);
+    temp_f   = (float)temp_raw * 0.1 - 90;
+
+    mic = infactory_crc_check(b) ? "CRC" : "CRC_ERROR"; // we could also return DECODE_FAIL_MIC;
+    infactory_raw_msg(raw_msg, b);                      // DEBUG: print raw message bits
 
     /* clang-format off */
     data = data_make(
             "model",            "",             DATA_STRING, _X("inFactory-TH","inFactory sensor"),
             "id",               "ID",           DATA_INT, id,
             "channel",          "Channel",      DATA_INT, channel,
-            "weakbatt",         "Battery",      DATA_INT, weakbatt,
+            "battery",          "Battery",      DATA_INT, battery,
             "temperature_F",    "Temperature",  DATA_FORMAT, "%.02f °F", DATA_DOUBLE, temp_f,
-            "humidity",         "Humidity",     DATA_FORMAT, "%u %%", DATA_INT, humidity,
+            "humidity",         "Humidity",     DATA_FORMAT, "%u %%rH", DATA_INT, humidity,
             "mic",              "Integrity",    DATA_STRING, mic,
-            "poweron",          "Power-On",     DATA_INT, pwron,
             "rawmsg",           "Datagram",     DATA_STRING, raw_msg,
             NULL);
     /* clang-format on */
@@ -142,20 +136,42 @@ static char *output_fields[] = {
         "temperature_F",
         "humidity",
         "mic",
-        "poweron",
-        "weakbatt",
+        "battery",
+#ifdef DEBUG        
         "rawmsg",
+#endif // DEBUG        
         NULL,
 };
+
+/*
+Analysis using Genuino (see http://gitlab.com/hp-uno, e.g. uno_log_433):
+
+Observed On-Off-Key (OOK) data pattern:
+
+    preamble            syncPrefix        data...(40 bit)                        syncPostfix
+    HHLL HHLL HHLL HHLL HLLLLLLLLLLLLLLLL (HLLLL HLLLLLLLL HLLLL HLLLLLLLL ....) HLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL
+
+Breakdown:
+
+- four preamble pairs '1'/'0' each with a length of ca. 1000uS
+- syncPre, syncPost, data0, data1 have a '1' start pulse of ca. 500uS
+- syncPre pulse before dataPtr has a '0' pulse length of ca. 8000uS
+- data0 (0-bits) have then a '0' pulse length of ca. 2000uS
+- data1 (1-bits) have then a '0' pulse length of ca. 4000uS
+- syncPost after dataPtr has a '0' pulse length of ca. 16000uS (we declare it received at 12ms)
+
+This analysis is the reason for the new r_device definitions below.
+NB: pulse_demod_ppm does not use .gap_limit if .tolerance is set.
+*/
 
 r_device infactory = {
         .name        = "inFactory, nor-tec, FreeTec NC-3982-913 temperature humidity sensor",
         .modulation  = OOK_PULSE_PPM,
-        .short_width = 1850,
-        .long_width  = 4050,
-        .gap_limit   = 4000, // Maximum gap size before new row of bits [us]
-        .reset_limit = 8500, // Maximum gap size before End Of Message [us].
-        .tolerance   = 1000,
+        .sync_width  = 500,   // Sync pulse width (not used)
+        .short_width = 1980,  // Width of a '0' gap
+        .long_width  = 3980,  // Width of a '1' gap
+        .reset_limit = 5000,  // Maximum gap size before End Of Message [us].
+        .tolerance   = 350,
         .decode_fn   = &infactory_callback,
         .disabled    = 0,
         .fields      = output_fields,
