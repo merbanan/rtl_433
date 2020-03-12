0b34b2f522b68d8b669d308ae8b76b36e0f47d61
David E. Tiller <sonocto@davidtiller.com>
2020-03-12 18:32:00 -0400

    More initial changes.

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -157,7 +157,8 @@
     DECL(tfa_drop_303233) \
     DECL(dsc_security_ws4945) \
     DECL(ert_amr) \
-    DECL(klimalogg)
+    DECL(klimalogg) \
+    DECL(rolling_code)
 
     /* Add new decoders here. */
 
