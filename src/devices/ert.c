e32facde62d1d1a92127c0dbcb57160ef70fe35d
Paul Harrington <phrrngtn@gmail.com>
2020-05-14 16:58:58 -0400

    fix ERT Endpoint Type extraction. Noticed via comparison of rtlamr output.

--- src/devices/ert.c
+++ src/devices/ert.c
@@ -65,7 +65,9 @@ static int ert_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 
     /* Extract parameters */
     physical_tamper = (b[3]&0xC0) >> 6;
-    ert_type = (b[3]&0x60) >> 2;
+    /* endpoint type is 4 bits and starts at position 26.
+     * xref https://github.com/bemasher/rtlamr/wiki/Protocol */
+    ert_type = (b[3]&0x36) >> 2;
     encoder_tamper = b[3]&0x03;
     consumption_data = (b[4]<<16) | (b[5]<<8) | b[6];
     ert_id = ((b[2]&0x06)<<23) | (b[7]<<16) | (b[8]<<8) | b[9];
