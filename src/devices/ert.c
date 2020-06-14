b515e19417e63aebca91541e17441c5e50c2f7a0
Benjamin Larsson <banan@ludd.ltu.se>
2020-06-14 23:10:30 +0200

    Update ert.c

--- src/devices/ert.c
+++ src/devices/ert.c
@@ -64,6 +64,7 @@ static int ert_decode(r_device *decoder, bitbuffer_t *bitbuffer)
      * CRC and extract the parameters from the back */
 
     /* Extract parameters */
+    physical_tamper = (b[3]&0xC0) >> 6;
     ert_type = (b[3]>>2) & 0x0F;
     encoder_tamper = b[3]&0x03;
     consumption_data = (b[4]<<16) | (b[5]<<8) | b[6];
