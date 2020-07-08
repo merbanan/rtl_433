f20c1553fbdf1ac67e4fc1428e2700f1c410de7c
Fakstate <odessaclaude@yahoo.com>
2020-07-08 22:19:48 +0200

    Adding copyright header.

--- src/devices/tfa_30_3221.c
+++ src/devices/tfa_30_3221.c
@@ -1,3 +1,11 @@
+/** @file
+    TFA 30.3221.02 decoder
+    Copyright (C) 2020 Odessa Claude
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+*/
 /*
     Temperature/Humidity outdoor sensor TFA 30.3221.02 
     -------------------------------------------------------------------------------------------------
