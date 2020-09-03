57b015b569199bbf66d414081bca470c0058af30
Odessa <odessaclaude@yahoo.com>
2020-09-03 12:33:51 +0200

    Add support for TFA 30.3221.02 Temperature/Humidity sensor (#1426)

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -172,6 +172,7 @@
     DECL(thermopro_tx2) \
     DECL(acurite_590tx) \
     DECL(secplus_v2) \
+    DECL(tfa_30_3221) \
     /* Add new decoders here. */
 
 #define DECL(name) extern r_device name;
