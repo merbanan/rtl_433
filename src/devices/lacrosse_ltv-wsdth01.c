b690d2a81af1ade2a6d89db6e9b78b8a6ceee31c
Mike <70450969+mbruski@users.noreply.github.com>
2020-09-05 11:03:02 -0400

    Add files via upload

--- src/devices/lacrosse_ltv-wsdth01.c
+++ src/devices/lacrosse_ltv-wsdth01.c
@@ -32,105 +32,105 @@ An inspection of the 79400 console reveals that it employs a HopeRF
 CMT2219A ISM receiver chip.  An application note is available that
 provides further info into the capabilities of the CMT2119A and
 CMT2219A.
+
+(http://www.cmostek.com/download/CMT2119A_v0.95.pdf)
+(http://www.cmostek.com/download/CMT2219A.pdf)
 (http://www.cmostek.com/download/AN138%20CMT2219A%20Configuration%20Guideline.pdf)
+
 
 Protocol Specification:
  
-Data bits are NRZ encoded with logical 1 and 0 bits 106.841us in length.
+Data bits are NRZ encoded with logical 1 and 0 bits 106.842us in length.
 
-Packet length is 266 bits according to inspectrum broken down as follows:
+SYN:32h ID:24h ?:4 SEQ:3b ?:1b TEMP:12d HUM:12d WSPD:12d WDIR:12d CHK:8h END:32h
 
-sync:		 7 bytes (0x55)
-preamble         5 bytes (0x695516ea05)
-device ID:      19 bits  (no change after replacing rechargeable cell)
-x1:              1 bit   (unknown)
+Packet length is 264 bits according to inspectrum broken down as follows:
+
+preamble:	 7 bytes (when aligned with sync word these are 0xaa)
+sync word:       4 bytes (0xd2aa2dd4)
+device ID:       3 bytes (matches bar code underside of unit covering pgm port)
+x1:              4 bit   (unknown, bit 0?00 might be 'battery low')
+sequence:        3 bits  (0-7, one up per packet, then repeats)
 x2:              1 bit   (unknown)
-sequence:        3 bits  (0-7, one up per packet, rinse, lather, repeat)
-x3:              1 bit   (unknown)
 celsius:        12 bits  (base 400, scale 10, range: -29°C to 60°C)
 humidity:       12 bits  (10 to 99% relative humidity)
 wind speed:     12 bits  (0.0 to 178.0 kMh)
 wind direction: 12 bits  (0 to 359°)
-CRC:             8 bits  (haven't discovered details yet)
-postamble:       3 bytes (0x696969)
-reset:          56 bits  (all zeros)
+checksum:        8 bits  (CRC-8 poly 0x31 init 0x00 over 10 bytes after sync)
+end:            32 bytes (0xd2d2d200)
 
-Given one or more of the following conditions is satisfied since the time of
-the last packet transmission:
 
-	temp      changes +/- 0.5°C 
-	humidity  changes +/- 2%
-	speed     changes +/- 0.8kM
+The sensor generates a packet every 'n' seconds but only transmits if one or
+more of the following conditions are satified:
 
-an update packet will be sent.  To conserve battery power, the interval between
-packets is adjusted as temperature decreases.  At temperatures above 0°C, the
-interval between packets will be 31 seconds (assuming the above conditions are
-satisfied).  Between 0°C and -17°C, the packet interval is 1 minute.  Below 17°C,
-the time between packets increases to 6 minutes.
+	temp changes +/- 0.8 degrees C
+	humidity changes +/- 1%
+        wind speed changes +/- 0.5 kM/h
 
-I tried using CRC check for TX145wsdth but that failed so I'm not checking it
-for the time being.  If someone wants to crack it, please be my guest.
-
-- checksum CRC-8 poly 0x31 init 0x00 over preceeding 7 bytes - tried but failed
+Thus, if there is a gap in sequencing, it is due to bad packet[s] (too short,
+failed CRC) or packet[s] that didn't satisfy at least one of these three
+conditions. 'n' above varies with temperature.  At 0C and above, 'n' is 31.
+Between -17C and 0C, 'n' is 60.  Below -17C, 'n' is 360.  
 
 */
 
 #include "decoder.h"
 
-static const uint8_t preamble_pattern[] = { 0x69, 0x55, 0x16, 0xea, 0x05 };
+static const uint8_t sync_word[] = { 0xd2, 0xaa, 0x2d, 0xd4 };
 
 static int lacrosse_breezepro_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 {
     data_t *data;
-    uint8_t p[32], l, *c, x1, x2, x3, seq, offset, chk, mic;
+    uint8_t p[32], l, *c, x1, x2, seq, offset, chk;
     uint32_t id;
     uint16_t raw_temp, humidity, raw_speed, direction;
     float temp_c, speed_kmh;
 
-    if (decoder->verbose > 1) {
-          bitrow_debug(0, bitbuffer->bits_per_row[0]);
-    }
-
-    if (bitbuffer->bits_per_row[0] != 264) {
-        if (decoder->verbose > 1) {
-            fprintf(stderr, "%s: bits_per_row: %d\n", __func__, bitbuffer->bits_per_row[0]);
-        }
+    if (bitbuffer->bits_per_row[0] < 264) {
+        if (decoder->verbose) {
+            fprintf(stderr, "%s: Wrong packet length: %d\n", __func__, bitbuffer->bits_per_row[0]);
+	}
         return DECODE_ABORT_LENGTH;
     }
 
     offset = bitbuffer_search(bitbuffer, 0, 0,
-            preamble_pattern, sizeof (preamble_pattern) * 8);
+            sync_word, sizeof(sync_word) * 8);
 
     if (offset == bitbuffer->bits_per_row[0]) {
+        if (decoder->verbose) {
+            fprintf(stderr, "%s: Sync word not found\n", __func__);
+	}	   
         return DECODE_FAIL_SANITY;
     }
 
 
-//    c = bitbuffer->bb[0];
+    c = bitbuffer->bb[offset];
+ 
+    chk = crc8(c, 10, 0x31, 0x00);
+    if (chk) {
+        if (decoder->verbose) {
+           fprintf(stderr, "%s: CRC failed!\n", __func__);
+        }
+        return DECODE_FAIL_MIC;
+    }
 
-//    chk = crc8(c, 8, 0x31, 0x00);
-//    if (chk) {
-//        if (decoder->verbose > 1) {
-//            fprintf(stderr, "%s: CRC failed!\n", __func__);
-//        }
-//        return DECODE_FAIL_MIC;
-//    }
+    if (decoder->verbose) {
+        bitbuffer_print(bitbuffer);
+    }
 
-    /* align row - not the best align but I can work with it */
     l = bitbuffer->bits_per_row[0] - offset;
-    offset += sizeof(preamble_pattern) * 8;
+    offset += sizeof(sync_word) * 8;
     bitbuffer_extract_bytes(bitbuffer, 0, offset, p, l);
 
-    id          = ((p[2] & 0xe0) >> 5) | (p[1] << 3) | (p[0] << 11);
-    x1          = (p[2] & 0x10) >> 4;
-    x2          = (p[2] & 0x08) >> 3;
-    seq         = p[2] & 0x07;
-    x3          = (p[3] & 0x80) >> 7;
-    raw_temp    = ((p[4] & 0xf8) >> 3) | (p[3] & 0x7f) << 5;
-    humidity    = ((p[6] & 0x80) >> 7) | (p[5] << 1) | ((p[4] & 0x07) << 9);
-    raw_speed   = ((p[7] & 0xf8) >> 3) | (p[6] & 0x7f) << 5;
-    direction   = ((p[9] & 0x80) >> 7) | (p[8] << 1) | ((p[7] & 0x07) << 9);
-    mic         = ((p[10] & 0x80) >> 7) | (p[9] & 0x7f) << 1;    
+    id          = (p[0] << 16) | (p[1] << 8) | p[2];
+    x1          = (p[3] & 0xf0) >> 4;
+    seq         = (p[3] & 0x0e) >> 1;
+    x2          = p[3] & 0x01;
+    raw_temp    = p[4] << 4 | ((p[5] & 0xf0) >> 4);
+    humidity    = ((p[5] & 0x0f) << 8) | p[6];
+    raw_speed   = p[7] << 4 | ((p[8] & 0xf0) >> 4);
+    direction   = ((p[8] & 0x0f) << 8) | p[9];
+
     /* base and/or scale adjustments */
     temp_c = (float)raw_temp * 0.1 - 40.0;
     speed_kmh = (float)raw_speed * 0.1;
@@ -138,16 +138,15 @@ static int lacrosse_breezepro_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     /* clang-format off */
     data = data_make(
             "model",            "",                 DATA_STRING, "LaCrosse-LTV-WSDTH01",
-            "id",               "Sensor ID",        DATA_FORMAT, "%05x", DATA_INT, id,
-	    "x1",		"unknown",          DATA_INT,    x1,
-	    "x2",		"unknown",	    DATA_INT,    x2,
+            "id",               "Sensor ID",        DATA_FORMAT, "%06x", DATA_INT, id,
+	    "x1",		"unknown",          DATA_INT,     x1,
             "seq",              "Sequence",         DATA_FORMAT, "%01x", DATA_INT, seq,
-            "x3",               "unknown",          DATA_INT,    x3,
+            "x2",               "unknown",          DATA_INT,     x2,
             "temperature_C",    "Temperature",      DATA_FORMAT, "%.1f C", DATA_DOUBLE, temp_c,
             "humidity",         "Humidity",         DATA_FORMAT, "%u %%", DATA_INT, humidity,
             "wind_avg_km_h",    "Wind speed",       DATA_FORMAT, "%.1f km/h",  DATA_DOUBLE, speed_kmh,
             "wind_dir_deg",     "Wind direction",   DATA_INT,    direction,
-            "mic",              "Integrity",        DATA_STRING, "CRC",
+            "mic",              "Integrity",        DATA_STRING, "CRC8",
             NULL);
     /* clang-format on */
 
@@ -159,9 +158,8 @@ static char *output_fields[] = {
         "model",
         "id",
 	"x1",
-	"x2"
 	"seq",
-        "x3",
+        "x2",
         "temperature_C",
         "humidity",
         "wind_avg_km_h",
