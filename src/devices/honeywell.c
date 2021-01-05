725d75921afd32a82b8f11b445446cdf240115bd
Peter Shipley <peter.shipley@gmail.com>
2021-01-04 19:13:05 -0800

    Fix:  buffer len

--- src/devices/honeywell.c
+++ src/devices/honeywell.c
@@ -80,8 +80,9 @@ static int honeywell_decode(r_device *decoder, bitbuffer_t *bitbuffer)
         return DECODE_ABORT_EARLY; // Reduce collisions
 
     if (len > 50) { // DW11
+        // printf("honeywell len = %d %d\n", len, (len >80 ? 80 : len));
         if (decoder->verbose)
-            bitrow_printf(b, len, "%s: ", __func__);
+            bitrow_printf(b, (len >80 ? 80 : len) , "%s: ", __func__);
     }
 
     if (channel == 0x2 || channel == 0x4 || channel == 0xA) {
