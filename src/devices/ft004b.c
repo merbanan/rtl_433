a60892844607458c87f56f6314ab56a49e116ed7
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2021-02-10 09:11:07 +0100

    minor: punctuation in license texts

--- src/devices/ft004b.c
+++ src/devices/ft004b.c
@@ -3,9 +3,9 @@
 
     Copyright (C) 2017 George Hopkins <george-hopkins@null.net>
 
-    This program is free software: you can redistribute it and/or modify
+    This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
+    the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 */
 /**
--- src/devices/kedsum.c
+++ src/devices/kedsum.c
@@ -4,9 +4,9 @@
     Copyright (C) 2016 John Lifsey
     Enhanced (C) 2019 Christian W. Zuckschwerdt <zany@triq.net>
 
-    This program is free software: you can redistribute it and/or modify
+    This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 2 of the License, or
+    the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 */
 /**
