2439b1c73027c60a5e5d67a745abd6f9e6cbf85a
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2021-02-14 13:04:28 +0100

    minor: fix Cavius code style

--- README.md
+++ README.md
@@ -253,7 +253,7 @@ See [CONTRIBUTING.md](./docs/CONTRIBUTING.md).
     [176]  BlueLine Power Monitor
     [177]  Burnhard BBQ thermometer
     [178]  Security+ (Keyfob)
-    [179]  Cavius
+    [179]  Cavius smoke, heat and water detector
 
 * Disabled by default, use -R n or -G
 
--- src/devices/cavius.c
+++ src/devices/cavius.c
@@ -1,17 +1,16 @@
 /** @file
-    Cavius protocol.
+    Cavius smoke, heat and water detector.
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
-
 */
-/** @fn int cavius_callback(r_device *decoder, bitbuffer_t *bitbuffer)
-Cavius smoke, heat and water detector decoder
+/**
+Cavius smoke, heat and water detector decoder.
 
-The alarm units use HopeRF RF69 chips on 869.67 MHz. FSK modulation, 4800 bps
-They seem to use 'Cavi' as a sync word on the chips
+The alarm units use HopeRF RF69 chips on 869.67 MHz, FSK modulation, 4800 bps.
+They seem to use 'Cavi' as a sync word on the chips.
 Everything after the sync word is Manchester coded.
 The unpacked payload is 11 bytes long structured as follows:
 
@@ -38,55 +37,55 @@ Sometimes the receiver samplerate has to be at 250ksps to decode properly.
 
 #include "decoder.h"
 
-enum cavius_message {
-    cavius_pairing  = 0x80,
-    cavius_test     = 0x40,
-    cavius_alarm    = 0x20,
-    cavius_warning  = 0x10,
-    cavius_battlow  = 0x08,
-    cavius_mute     = 0x04,
-    cavius_unknown2 = 0x02,
-    cavius_unknown1 = 0x01,
-};
-
-static int cavius_callback(r_device *decoder, bitbuffer_t *bitbuffer)
+static int cavius_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 {
+    uint8_t const preamble[] = {0x43, 0x61, 0x76, 0x69};
+
+    enum cavius_message {
+        cavius_pairing  = 0x80,
+        cavius_test     = 0x40,
+        cavius_alarm    = 0x20,
+        cavius_warning  = 0x10,
+        cavius_battlow  = 0x08,
+        cavius_mute     = 0x04,
+        cavius_unknown2 = 0x02,
+        cavius_unknown1 = 0x01,
+    };
+
     data_t *data;
 
-    static const uint8_t preamble[]  = {0x43, 0x61, 0x76, 0x69};
-
     // Find the sync
-    unsigned bit_offset = bitbuffer_search(bitbuffer, 0, 0, preamble, sizeof(preamble)*8);
-    if (bit_offset + 22*8 >= bitbuffer->bits_per_row[0]) {  // Did not find a big enough package
-        return 0;
+    unsigned bit_offset = bitbuffer_search(bitbuffer, 0, 0, preamble, sizeof(preamble) * 8);
+    if (bit_offset + 22 * 8 >= bitbuffer->bits_per_row[0]) { // Did not find a big enough package
+        return DECODE_ABORT_EARLY;
     }
-    bit_offset += sizeof(preamble)*8;     // skip sync
+    bit_offset += sizeof(preamble) * 8; // skip sync
 
     bitbuffer_t databits = {0};
 
-    bitbuffer_manchester_decode(bitbuffer, 0, bit_offset, &databits, 11*8);
+    bitbuffer_manchester_decode(bitbuffer, 0, bit_offset, &databits, 11 * 8);
     bitbuffer_invert(&databits);
 
-    if (decoder->verbose) {
-        bitbuffer_print(&databits);
+    // we require 11 bytes
+    if (databits.bits_per_row[0] < 11 * 8) {
+        return DECODE_FAIL_SANITY; // manchester_decode fail
     }
 
     uint8_t *b = databits.bb[0];
 
     uint32_t net_id    = ((uint32_t)b[0] << 24) | (b[1] << 16) | (b[2] << 8) | (b[3]);
     uint32_t sender_id = ((uint32_t)b[7] << 24) | (b[8] << 16) | (b[9] << 8) | (b[10]);
-    uint8_t  message   = b[4];
+    int message        = (b[4]);
+    int batt_low       = (b[4] & cavius_battlow) != 0;
 
-    if (crc8le(databits.bb[0], 7, 0x31, 0x0) != 0) return DECODE_FAIL_MIC; // invalid CRC
+    int crc = crc8le(databits.bb[0], 7, 0x31, 0x0);
+    if (crc != 0)
+        return DECODE_FAIL_MIC; // invalid CRC
 
     char *text = "Unknown";
-
     switch (message) {
         case cavius_alarm:
             text = "Fire alarm";
-            break;
-        case cavius_battlow:
-            text = "Battery low";
             break;
         case cavius_mute:
             text = "Alarm muted";
@@ -100,19 +99,22 @@ static int cavius_callback(r_device *decoder, bitbuffer_t *bitbuffer)
         case cavius_warning:
             text = "Warning/Water detected";
             break;
+        case cavius_battlow:
+            text = "Battery low";
+            break;
         default:
             break;
     }
 
     /* clang-format off */
     data = data_make(
-            "id",            "Device ID",   DATA_INT,    sender_id,
-            "net_id",        "Net ID",      DATA_INT,    net_id,
-            "message",       "Message",     DATA_INT,    message,
-            "text",          "Description", DATA_STRING, text,
-            "battery_ok",    "Battery OK",  DATA_INT,    ((message & cavius_battlow) == cavius_battlow) ? 0 : 1,
-            "mic",           "Integrity",   DATA_STRING, "CRC",
-            "model",         "",            DATA_STRING, "Cavius-Security",
+            "model",        "",             DATA_STRING, "Cavius-Security",
+            "id",           "Device ID",    DATA_INT,    sender_id,
+            "battery_ok",   "Battery OK",   DATA_INT,    !batt_low,
+            "net_id",       "Net ID",       DATA_INT,    net_id,
+            "message",      "Message",      DATA_INT,    message,
+            "text",         "Description",  DATA_STRING, text,
+            "mic",          "Integrity",    DATA_STRING, "CRC",
             NULL);
     /* clang-format on */
 
@@ -121,13 +123,13 @@ static int cavius_callback(r_device *decoder, bitbuffer_t *bitbuffer)
 }
 
 static char *output_fields[] = {
+        "model",
         "id",
+        "battery_ok"
         "net_id",
         "message",
         "text",
-        "battery_ok"
         "mic",
-        "model",
         NULL,
 };
 
@@ -139,7 +141,6 @@ r_device cavius = {
         .sync_width  = 2700,
         .gap_limit   = 1000,
         .reset_limit = 1000,
-        .decode_fn   = &cavius_callback,
-        .disabled    = 0,
+        .decode_fn   = &cavius_decode,
         .fields      = output_fields,
 };
