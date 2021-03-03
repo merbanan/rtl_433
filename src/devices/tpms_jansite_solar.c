f22dfb68e8e467edf8191efb64c23fe79361e564
Benjamin Larsson <benjamin.larsson@iopsys.eu>
2021-03-03 19:00:12 +0100

    Add mic support

--- src/devices/tpms_jansite_solar.c
+++ src/devices/tpms_jansite_solar.c
@@ -35,33 +35,34 @@ static int tpms_jansite_solar_decode(r_device *decoder, bitbuffer_t *bitbuffer, 
     char id_str[7 + 1];
     int flags;
     int pressure;
-    int bar;
     int temperature;
     char code_str[9 * 2 + 1];
 
     bitbuffer_manchester_decode(bitbuffer, row, bitpos, &packet_bits, 88);
     bitbuffer_invert(&packet_bits);
-//    bitbuffer_debug(&packet_bits);
     
     if (packet_bits.bits_per_row[0] < 88) {
         return DECODE_FAIL_SANITY;
-        // fprintf(stderr, "%s packet_bits.bits_per_row = %d\n", __func__, packet_bits.bits_per_row[0]);
     }
     b = packet_bits.bb[0];
     
+    /* Check for sync */
     if ((b[0]<<8 | b[1]) != 0xdd33) {
-        fprintf(stdout, "0x%X\n",b[0]<<8 | b[1]);
         return DECODE_FAIL_SANITY;
     }
 
-    // TODO: validate checksum
+    /* Check crc (CRC-16/BUYPASS) */
+    uint16_t crc_calc = crc16(&b[2], 7, 0x8005, 0x0000);
+    if ( ((b[9]<<8) | b[10]) != crc_calc) {
+        fprintf(stdout, "%04x vs %02x %02x\n", crc_calc, b[9], b[10]);
+        return DECODE_FAIL_MIC;
+    }
+    //bitbuffer_debug(&packet_bits);
 
     id          = (unsigned)b[2] << 16 | b[3] << 8 | b[4];
     flags       = b[5];
     temperature = b[6];
     pressure    = b[7];
-    bar         = b[7];
-    //crc         = b[6];
     sprintf(id_str, "%06x", id);
     sprintf(code_str, "%02x%02x%02x%02x%02x%02x%02x%02x%02x", b[2], b[3], b[4], b[5], b[6], b[7], b[8], b[9], b[10]); // figure out the checksum
 
@@ -72,12 +73,9 @@ static int tpms_jansite_solar_decode(r_device *decoder, bitbuffer_t *bitbuffer, 
             "id",               "",             DATA_STRING, id_str,
             "flags",            "",             DATA_INT, flags,
             "pressure_kPa",     "Pressure",     DATA_FORMAT, "%.0f kPa", DATA_DOUBLE, (float)pressure * 1.6,
-            "bar_int",          "",             DATA_INT, bar,
-            "bar",              "",             DATA_FORMAT, "%.3f", DATA_DOUBLE, (float)bar*0.01565,
-            "bar_round",              "",       DATA_FORMAT, "%.1f", DATA_DOUBLE, (float)bar*0.016,
             "temperature_C",    "Temperature",  DATA_FORMAT, "%.0f C", DATA_DOUBLE, (float)temperature - 55.0,
             "code",             "",             DATA_STRING, code_str,
-            //"mic",              "",             DATA_STRING, "CHECKSUM",
+            "mic",              "",             DATA_STRING, "CRC",
             NULL);
     /* clang-format on */
 
@@ -120,7 +118,7 @@ static char *output_fields[] = {
         "pressure_kPa",
         "temperature_C",
         "code",
-        //"mic",
+        "mic",
         NULL,
 };
 
@@ -129,8 +127,8 @@ r_device tpms_jansite_solar = {
         .modulation  = FSK_PULSE_PCM,
         .short_width = 51,  // 12-13 samples @250k
         .long_width  = 51,  // FSK
-        .reset_limit = 150, // Maximum gap size before End Of Message [us].
+        .reset_limit = 5000, // Maximum gap size before End Of Message [us].
         .decode_fn   = &tpms_jansite_solar_callback,
-        .disabled    = 1, // Unknown checksum
+        .disabled    = 0,
         .fields      = output_fields,
 };
