2a91cb4fc0c8dd24946b044ace8b08204ce57a24
wst-tdb <github@trillium.house>
2021-07-26 22:53:34 +0100

    Corrected decoder options

--- src/devices/jasco.c
+++ src/devices/jasco.c
@@ -13,24 +13,18 @@ Jasco/GE Choice Alert Wireless Device Decoder.
 
 v0.1 based on the contact and water sensors Model 45131 / FCC ID QOB45131-3
 
-bit stream is 25-27 bits
-
-011-1011101110111011-0-1111101
-011-1100111110111011-0-011011
-011-1100011111111011-0-01001
-
-with a 3 bit preamble, 16 bit ID field, 1 status bit, and then remainder unknown. Currently decoding the unknown as a status string.
-Battery status does not seem to be part of the status (or at least swapping the batteries with mostly discharged ones doesn't change bit values)
+v0.2 corrected decoder
 
 */
 
 #include "decoder.h"
 
-#define JASCO_MSG_BIT_LEN 26
+#define JASCO_MSG_BIT_LEN 86
 
 static int jasco_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 {
-    if (bitbuffer->bits_per_row[0] <= JASCO_MSG_BIT_LEN-1 && bitbuffer->bits_per_row[0] >= JASCO_MSG_BIT_LEN+1) {
+
+    if (bitbuffer->bits_per_row[0] != JASCO_MSG_BIT_LEN && bitbuffer->bits_per_row[0] != JASCO_MSG_BIT_LEN+1) {
         if (decoder->verbose > 1 && bitbuffer->bits_per_row[0] > 0) {
             fprintf(stderr, "%s: invalid bit count %d\n", __func__,
                      bitbuffer->bits_per_row[0]);
@@ -38,44 +32,49 @@ static int jasco_decode(r_device *decoder, bitbuffer_t *bitbuffer)
         return DECODE_ABORT_LENGTH;
     }
 
-    uint8_t b[6];
+    uint8_t b[4];
+    uint8_t crc;
     uint32_t sensor_id = 0;
     int s_closed=0;
-    int battery=0;
+//    int battery=0;
+    bitbuffer_t packet_bits;
     data_t *data;
-    char message[6]="";
+    uint8_t const preamble[] = {0xfc, 0x0c};
+    unsigned bitpos = 0;
 
-    bitbuffer_extract_bytes(bitbuffer, 0, 0,b, 3);
-    if (b[0] != 0x60) {
-            return DECODE_FAIL_SANITY;
+    if (decoder->verbose > 1) {
+        bitbuffer_debug(bitbuffer);
+    }
+    bitpos = bitbuffer_search(bitbuffer, 0, 0, preamble, sizeof(preamble) * 8) + sizeof(preamble) * 8;
+
+    bitpos = bitbuffer_manchester_decode(bitbuffer, 0, bitpos, &packet_bits, 87);
+
+    bitbuffer_extract_bytes(&packet_bits, 0, 0,b, 32);
+
+    if (decoder->verbose > 1) {
+        bitbuffer_debug(&packet_bits);
     }
 
-    bitbuffer_extract_bytes(bitbuffer, 0, 3,b, 16);
+    bitbuffer_clear(&packet_bits);
+
+    crc = b[0] ^ b[1] ^ b[2];
+    if (crc != b[3]) {
+        return DECODE_FAIL_SANITY;
+    }
+
+
     sensor_id = (b[0] << 8)+ b[1];
 
-    bitbuffer_extract_bytes(bitbuffer, 0, 19,b, 1);
-    s_closed = (b[0]& 0x80) == 0x80;
+    s_closed = ((b[2] & 0xef) == 0xef);
 
-    // Not decoded yet
-    bitbuffer_extract_bytes(bitbuffer, 0, 20,b, 6);
-
-    for (unsigned bit = 0; bit < 6; ++bit) {
-        if (b[bit / 8] & (0x80 >> (bit % 8))) {
-            message[bit]= '1';
-        } else {
-            message[bit]= '0';
-        }
-    }
-
-//    bitbuffer_debug(bitbuffer);
 
     /* clang-format off */
     data = data_make(
             "model",            "",             DATA_STRING, "Jasco/GE Choice Alert Security Devices",
             "id",               "Id",           DATA_INT,    sensor_id,
-            "battery_ok",       "Battery",      DATA_INT,    battery,
+//            "battery_ok",       "Battery",      DATA_INT,    battery,
             "status",           "Closed",       DATA_INT,    s_closed,
-            "statusData",      "Status Data",  DATA_STRING, message ,
+            "CRC",              "CRC",          DATA_INT,    crc ,
             NULL);
     /* clang-format on */
 
@@ -86,21 +85,18 @@ static int jasco_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 static char *output_fields[] = {
         "model",
         "id",
-        "battery_ok",
+//        "battery_ok",
         "status",
-        "statusData",
+        "CRC",
         NULL,
 };
 
 r_device jasco = {
         .name        = "Jasco/GE Choice Alert Security Devices",
-        .modulation  = OOK_PULSE_PWM,
-        .short_width = 248,
-        .long_width  = 512,
-        .reset_limit = 1548, // Maximum gap size before End Of Message
-        .gap_limit   = 0,
-        .tolerance   = 0,
-        .sync_width  = 1540,
+        .modulation  = OOK_PULSE_PCM_RZ,
+        .short_width = 250,
+        .long_width  = 250,
+        .reset_limit = 1800, // Maximum gap size before End Of Message
         .decode_fn   = &jasco_decode,
         .fields      = output_fields,
 
