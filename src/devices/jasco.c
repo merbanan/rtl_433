5642c96931e9554c32187a2b89fdfe365f4dece9
wst-tdb <github@trillium.house>
2021-07-27 01:53:30 +0100

    no id was right to beign with

--- src/devices/jasco.c
+++ src/devices/jasco.c
@@ -71,8 +71,7 @@ static int jasco_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     /* clang-format off */
     data = data_make(
             "model",            "",             DATA_STRING, "Jasco/GE Choice Alert Security Devices",
-            "sensor_id",        "Id",           DATA_INT,    sensor_id,
-//            "battery_ok",       "Battery",      DATA_INT,    battery,
+            "id",               "Id",           DATA_INT,    sensor_id,
             "status",           "Closed",       DATA_INT,    s_closed,
             "CRC",              "CRC",          DATA_INT,    crc ,
             NULL);
@@ -84,8 +83,7 @@ static int jasco_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 
 static char *output_fields[] = {
         "model",
-        "sensor_id",
-//        "battery_ok",
+        "id",
         "status",
         "CRC",
         NULL,
