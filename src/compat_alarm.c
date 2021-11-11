53b139c522d92df5f248da108439c2125f4c6f79
obones <obones@free.fr>
2021-11-11 16:10:18 +0100

    Fix: Windows CreateTimerQueueTimer alarm handle is not valid for CloseHandle (#1859)

--- src/compat_alarm.c
+++ src/compat_alarm.c
@@ -36,7 +36,6 @@ static void alarm_delete(void)
        return;
     signal(SIGALRM, SIG_IGN);
     DeleteTimerQueueTimer(NULL, alarm_hnd, NULL);
-    CloseHandle(alarm_hnd);
     alarm_hnd = INVALID_HANDLE_VALUE;
 }
 
