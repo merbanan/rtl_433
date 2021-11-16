c86be2eb0d0bd7863531c3a30f03bea1d72e6ea2
OBones <obones@free.fr>
2021-11-16 09:25:16 +0100

    minor: annotations must be the same in the header and source files

--- src/term_ctl.c
+++ src/term_ctl.c
@@ -328,7 +328,7 @@ int term_puts(void *ctx, char const *buf)
     return len;
 }
 
-int term_printf(void *ctx, char const *format, ...)
+int term_printf(void *ctx, _Printf_format_string_ char const *format, ...)
 {
     int len;
     va_list args;
@@ -431,7 +431,7 @@ int term_help_puts(void *ctx, char const *buf)
     return len;
 }
 
-int term_help_printf(char const *format, ...)
+int term_help_printf(_Printf_format_string_ char const *format, ...)
 {
     int len;
     va_list args;
