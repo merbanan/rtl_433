b0c401237342114dd0fd841aa98c2bda80c75dac
snicker <ngordon779@gmail.com>
2021-11-24 04:24:00 +0000

    update value template for  to allow for float and double battery values

--- examples/rtl_433_mqtt_hass.py
+++ examples/rtl_433_mqtt_hass.py
@@ -172,7 +172,7 @@ mappings = {
             "device_class": "battery",
             "name": "Battery",
             "unit_of_measurement": "%",
-            "value_template": "{{ float(value|int) * 99 + 1 }}",
+            "value_template": "{{ float(value) * 99 + 1 }}",
             "state_class": "measurement"
         }
     },
