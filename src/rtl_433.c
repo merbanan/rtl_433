65a7e417c718e4a59ad5c985ad7d374d7bdb591c
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2021-12-19 16:24:35 +0100

    Add rtl_tcp pass-through output

--- src/rtl_433.c
+++ src/rtl_433.c
@@ -40,6 +40,7 @@
 #include "pulse_demod.h"
 #include "rfraw.h"
 #include "data.h"
+#include "raw_output.h"
 #include "r_util.h"
 #include "optparse.h"
 #include "abuf.h"
@@ -364,6 +365,12 @@ static void sdr_callback(unsigned char *iq_buf, uint32_t len, void *ctx)
     struct dm_state *demod = cfg->demod;
     char time_str[LOCAL_TIME_BUFLEN];
     unsigned long n_samples;
+
+    // do this here and not in sdr_handler so realtime replay can use rtl_tcp output
+    for (void **iter = cfg->raw_handler.elems; iter && *iter; ++iter) {
+        raw_output_t *output = *iter;
+        raw_output_frame(output, iq_buf, len);
+    }
 
     if ((cfg->bytes_to_read > 0) && (cfg->bytes_to_read <= len)) {
         len = cfg->bytes_to_read;
@@ -1143,6 +1150,9 @@ static void parse_conf_option(r_cfg_t *cfg, int opt, char *arg)
         }
         else if (strncmp(arg, "null", 4) == 0) {
             add_null_output(cfg, arg_param(arg));
+        }
+        else if (strncmp(arg, "rtl_tcp", 7) == 0) {
+            add_rtltcp_output(cfg, arg_param(arg));
         }
         else {
             fprintf(stderr, "Invalid output format %s\n", arg);
