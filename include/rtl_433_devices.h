f6e910095ccfcab8f8e7aa0589dc3e7efc008edf
David E. Tiller <3858971+dtiller@users.noreply.github.com>
2022-01-24 13:32:44 -0500

    First try.

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -219,6 +219,7 @@
     DECL(simplisafe_gen3) \
     DECL(yale_hsa) \
     DECL(regency_fan) \
+    DECL(heatilator_log) \
 
     /* Add new decoders here. */
 
--- src/devices/heatilator.c
+++ src/devices/heatilator.c
@@ -48,113 +48,60 @@ is in 'thermo' mode - this is a mode where the remote detects the room temperatu
 and commands the gas logs on/off to maintain the temperature selected on the remote.
 
 There are safety mechanisms afoot - whenever the gas logs are 'on', on with a timer,
-or on in thermo mode, occasional 'keepalive' messages are sent to the gas logs to 
+or on in thermo mode, occasional 'keepalive' messages are sent to the gas logs to
 guarantee that the remote is still in range and the batteries are not dead. Generally
 these messages are exactly the same as the last command that the remote sent - that is,
 if you turn the logs 'on' manually, the remote will send the same 'on' command every so
-often. 
+often.
 
 The COMMAND S and T bits have these meanings:
     S  T
     ----
     0  0 - Off, Manual mode
     0  1 - Off, Thermo mode (room is too warm)
-    1  0 - On,  Manual mode. 
+    1  0 - On,  Manual mode.
     1  1 - On,  Thermo more (room is too cold)
 
 */
 
-static int heatilator_fan_decode(r_device *decoder, bitbuffer_t *bitbuffer)
+static int heatilator_log_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 {
-    char const * const command_names[] = {
-            /* 0  */ "invalid",
-            /* 1  */ "fan_speed",
-            /* 2  */ "fan_speed",
-            /* 3  */ "invalid",
-            /* 4  */ "light_intensity",
-            /* 5  */ "light_delay",
-            /* 6  */ "fan_direction",
-            /* 7  */ "invalid",
-            /* 8  */ "invalid",
-            /* 9  */ "invalid",
-            /* 10 */ "invalid",
-            /* 11 */ "invalid",
-            /* 12 */ "invalid",
-            /* 13 */ "invalid",
-            /* 14 */ "invalid",
-            /* 15 */ "invalid",
+    char const * const mode_names[] = {
+            /* 0  */ "manual",
+            /* 1  */ "thermo",
+    };
+
+    char const * const state_names[] = {
+            /* 0  */ "flame_off",
+            /* 1  */ "flame_on",
     };
 
     int return_code = 0;
 
-    bitbuffer_invert(bitbuffer);
-
     for (int row = 0; row < bitbuffer->num_rows; row++) {
         int num_bits = bitbuffer->bits_per_row[row];
 
-        if (num_bits != 21) { // Max number of bits is 21
+        if (num_bits < 24 || num_bits > 25) {
             if (decoder->verbose > 1) {
-                fprintf(stderr, "%s: Expected %d bits, got %d.\n", __func__, 21, num_bits); // Max number of bits is 21
+                fprintf(stderr, "%s: Expected 24 or 25 bits, got %d.\n", __func__, num_bits);
             }
             continue;
         }
 
         uint8_t bytes[3]; // Max number of bytes is 3
-        bitbuffer_extract_bytes(bitbuffer, row, 1, bytes, 21); // Valid byte offset is 1, Max number of bits is 21
-        reflect_bytes(bytes, 3); // Max number of bytes is 3
+        bitbuffer_extract_bytes(bitbuffer, row, 0, bytes, 24); // Valid byte offset is 0, Max number of bits is 24
 
-        // Calculate nibble sum and compare
-        int checksum = add_nibbles(bytes, 2) & 0x0f;
-        if (checksum != bytes[2]) { // Sum is in byte 2
-            if (decoder->verbose > 1) {
-                fprintf(stderr, "%s: Checksum failure: expected %0x, got %0x\n", __func__, bytes[2], checksum); // Sum is in byte 2
-            }
-            continue;
-        }
-
-        // Now that message "envelope" has been validated, start parsing data.
-        int command = bytes[0] >> 4;    // Command and Channel are in byte 0
-        int channel = ~bytes[0] & 0x0f; // Command and Channel are in byte 0
-        int value   = bytes[1];         // Value is in byte 1
-
-        char value_string[64] = {0};
-
-        switch (command) {
-        case 1: // 1 is the command to STOP
-            sprintf(value_string, "stop");
-            break;
-
-        case 2: // 2 is the command to change fan speed
-            sprintf(value_string, "speed %d", value);
-            break;
-
-        case 4: // 4 is the command to change the light intensity
-            sprintf(value_string, "%d %%", value);
-            break;
-
-        case 5: // 5 is the command to set the light delay
-            sprintf(value_string, "%s", value == 0 ? "off" : "on");
-            break;
-
-        case 6: // 6 is the command to change fan direction
-            sprintf(value_string, "%s", value == 0x07 ? "clockwise" : "counter-clockwise");
-            break;
-
-        default:
-            if (decoder->verbose > 1) {
-                fprintf(stderr, "%s: Unknown command: %d\n", __func__, command);
-                continue;
-            }
-            break;
-        }
+        int serial_number = bytes[0] << 12 | bytes[1] << 4 | bytes[2] >> 4;
+        int command = bytes[2] & 0x0f;
+        int mode = command & 0x01;
+        int state = command >> 1 & 0x01;
 
         /* clang-format off */
         data_t *data = data_make(
                 "model",            "",     DATA_STRING,    "Heatilator-Remote",
-                "channel",          "",     DATA_INT,       channel,
-                "command",          "",     DATA_STRING,    command_names[command],
-                "value",            "",     DATA_STRING,    value_string,
-                "mic",              "",     DATA_STRING,    "CHECKSUM",
+                "serial_number",    "",     DATA_INT,       serial_number,
+                "mode",             "",     DATA_STRING,    mode_names[mode],
+                "state",            "",     DATA_STRING,    state_names[state],
                 NULL);
         /* clang-format on */
 
@@ -167,21 +114,18 @@ static int heatilator_fan_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 
 static char *output_fields[] = {
         "model",
-        "type",
-        "channel",
-        "command",
-        "value",
-        "mic",
+        "serial_number",
+        "mode",
+        "state",
         NULL,
 };
 
-r_device heatilator_fan = {
-        .name        = "Heatilator Ceiling Fan Remote (-f 303.75M to 303.96M)",
+r_device heatilator_log = {
+        .name        = "Heatilator Gas Log Remote",
         .modulation  = OOK_PULSE_PWM,
-        .short_width = 580,
-        .long_width  = 976,
-        .gap_limit   = 8000,
-        .reset_limit = 14000,
-        .decode_fn   = &heatilator_fan_decode,
+        .short_width = 380,
+        .long_width  = 1420,
+        .reset_limit = 1800,
+        .decode_fn   = &heatilator_log_decode,
         .fields      = output_fields,
 };
