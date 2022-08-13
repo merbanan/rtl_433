c4a34ca208203d1b5e90cf734b3a56d1b3271ca8
gc <nope>
2022-08-13 14:13:13 -0400

    add support for SRSmith SRS-2C-TX Pool Remote Control

--- src/devices/srsmith_pool_srs_2c_tx.c
+++ src/devices/srsmith_pool_srs_2c_tx.c
@@ -10,14 +10,6 @@
 */
 
 #include "decoder.h"
-#include "util.h"
-//#include <inttypes.h>
-//#include <string.h>
-
-#define BUTTON_ID_ONE 0x0d
-#define BUTTON_ID_TWO 0x1f
-#define BUTTON_ID_S   0x07
-#define BUTTON_ID_M   0x0b
 
 /**
 SRSmith Pool Light Remote Control, Model #SRS-2C-TX
@@ -44,44 +36,57 @@ Capture raw but often misshifted :| data. if you want to work with this data, th
     -f 915000000 -X n=SRSmith,m=FSK_PCM,s=100,l=100,r=4096,match=d391d391
 */
 
-static int srsmith_pool_srs_2c_tx_callback(r_device *decoder, bitbuffer_t *bitbuffer)
+// defines used by decode function follow
+#define PREAMBLE_SYNC_SUBSET \
+    { \
+        0xaa, 0xd3, 0x91, 0xd3, 0x91 \
+    }
+
+// size byte + 7 byte message + two byte crc == 10 bytes
+#define TOTAL_PACKET_SIZE_BYTES 10
+#define BUTTON_ID_ONE           0x0d
+#define BUTTON_ID_TWO           0x1f
+#define BUTTON_ID_S             0x07
+#define BUTTON_ID_M             0x0b
+
+static int srsmith_pool_srs_2c_tx_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 {
     data_t *data;
 
     if (bitbuffer->num_rows != 1)
         return DECODE_ABORT_EARLY;
 
-    if (bitbuffer->bits_per_row[0] != 144)
+    // minimum: TOTAL_PACKET_SIZE_BYTES * 8 + sync word length (4)*8 + preamble byte (1*8) == 120
+    // maximum: TOTAL_PACKET_SIZE_BYTES * 8 + sync word length (4)*8 + preamble bytes (4*8) == 144
+    if (bitbuffer->bits_per_row[0] < 120 || bitbuffer->bits_per_row[0] > 144)
         return DECODE_ABORT_LENGTH;
 
-    // this is heavily derived from Maverick XR30 BBQ Sensor
-    uint8_t b[18];
+    // going hunting for preamble + sync bits
+    uint8_t const preamble_sync_subset[] = PREAMBLE_SYNC_SUBSET;
 
-    // check for correct preamble/sync (0xaaaaaad391d391)
-    if (bitbuffer->bb[0][0] == 0x55) {
-        bitbuffer_extract_bytes(bitbuffer, 0, 7, b, 18 * 8); // shift in case first bit was not received properly
-    }
-    else if (bitbuffer->bb[0][0] == 0xaa) {
-        bitbuffer_extract_bytes(bitbuffer, 0, 8, b, 18 * 8); // shift in case first bit was received properly
-    }
-    else {
+    // next line does the search for the preamble+sync bits, returns the bit position where the preamble+sync bits START
+    // so we shift that by the number of the preamble+sync bits
+    unsigned start_pos = bitbuffer_search(bitbuffer, 0, 0, preamble_sync_subset, sizeof(preamble_sync_subset) * 8) + (sizeof(preamble_sync_subset) * 8);
+    if (start_pos == bitbuffer->bits_per_row[0]) {
         return DECODE_ABORT_EARLY; // preamble/sync missing
     }
-    if (b[0] != 0xaa || b[1] != 0xaa || b[2] != 0xaa || b[3] != 0xd3 || b[4] != 0x91 || b[5] != 0xd3 || b[6] != 0x91)
-        return DECODE_ABORT_EARLY; // preamble/sync missing
 
-    int totallength = bitbuffer->bits_per_row[0];
-    int packetsize  = (int)b[7];
+    // bytes are there, length is right, let's extract into b
+    uint8_t b[TOTAL_PACKET_SIZE_BYTES];
+    // now we're extracting the bytes -- we know what the total size /should/ be
+    bitbuffer_extract_bytes(bitbuffer, 0, start_pos, b, TOTAL_PACKET_SIZE_BYTES * 8);
+    int total_length      = bitbuffer->bits_per_row[0];
+    int sub_packet_length = b[0];
 
-    // packet actually starts at b[8]
-    uint32_t unknown_field = ((uint32_t)b[8] << 24) | (b[9] << 16) | (b[10] << 8) | (b[11]); // not sure what these four bytes are.
-
+    // sub-packet (packet within packet that has commands and baby parity) actually starts at b[1]
+    uint32_t unknown_field = ((uint32_t)b[1] << 24) | (b[2] << 16) | (b[3] << 8) | (b[4]); // not sure what these four bytes are.
+                                                                                           //
     // get xmitted pin (byte 12, inverted, reversed, bottom nibble)
-    uint8_t pin_container          = b[12];
+    uint8_t pin_container          = b[5];
     uint8_t inverted_pin_container = ~pin_container;                   // invert the pin
     uint8_t reversed_pin           = reverse8(inverted_pin_container); // reverse the bits
 
-    // convert just the first four bits to binary string
+    // convert just the first four bits to string that contains 4 bits that the pin is
     char pin_string[5] = {'\0'};
     snprintf(pin_string, sizeof(pin_string), "%d%d%d%d",
             (reversed_pin & 0x80 ? 1 : 0),
@@ -89,44 +94,45 @@ static int srsmith_pool_srs_2c_tx_callback(r_device *decoder, bitbuffer_t *bitbu
             (reversed_pin & 0x20 ? 1 : 0),
             (reversed_pin & 0x10 ? 1 : 0));
 
-    // button that was pushed
-    uint8_t button_id = b[13];
+    // button that was pressed
+    uint8_t button_id = b[6];
 
-    // get label
-    char button_string[50] = {'\0'};
+    // get label for the button that was pressed
+    char *button_string;
 
     switch (button_id) {
     case BUTTON_ID_ONE:
-        snprintf(button_string, sizeof(button_string), "On/Off Channel 1");
+        button_string = "On/Off Channel 1";
         break;
     case BUTTON_ID_TWO:
-        snprintf(button_string, sizeof(button_string), "On/Off Channel 2");
+        button_string = "On/Off Channel 2";
         break;
     case BUTTON_ID_S:
-        snprintf(button_string, sizeof(button_string), "Color Sync");
+        button_string = "Color Sync";
         break;
     case BUTTON_ID_M:
-        snprintf(button_string, sizeof(button_string), "ON/OFF Control - M");
+        button_string = "ON/OFF Control - M";
         break;
     default:
-        snprintf(button_string, sizeof(button_string), "Unknown");
+        button_string = "Unknown";
         break;
     }
 
-    // grab CRCs
-    // commenting out the below two lines because we're not using this crc
+    // grab CRCs/parity
+    // the "sub packet parity" is the parity for the sub-packet within the modem packet (e.g., between the size byte and the start of the crc-16)
     // see above for description.
-    //
-    // however, i'd still like the code to be here in case we need it in the future.
-    // uint8_t baby_crc            = b[14];
-    // uint8_t baby_calculated_crc = crc8(&b[8], 6, 1, 1);
+    uint8_t sub_packet_parity            = b[7];
+    uint8_t calculated_sub_packet_parity = crc8(&b[1], 6, 1, 1);
 
     // total CRC-16
     // this CRC represents the entire packet sent by the modem in the remote
-    uint16_t total_crc            = (b[15] << 8) | (b[16]);
-    uint16_t calculated_total_crt = crc16(&b[7], 8, 0x8005, 0xFFFF);
+    uint16_t total_crc            = (b[8] << 8) | (b[9]);
+    uint16_t calculated_total_crc = crc16(&b[0], 8, 0x8005, 0xFFFF);
+    decoder_logf(decoder, 1, __func__,
+            "total_length: %d, sub_packet_length: %d, sub_packet_parity: %hhx, calculated_sub_packet_parity: %hhx, total_crc: %04x, calculated_total_crc: %04x, button_id: %hhx, button_string: %s, pin_string: %s",
+            total_length, sub_packet_length, sub_packet_parity, calculated_sub_packet_parity, total_crc, calculated_total_crc, button_id, button_string, pin_string);
 
-    if (total_crc != calculated_total_crt) {
+    if (total_crc != calculated_total_crc) {
         // parity fail.
         return DECODE_FAIL_MIC;
     }
@@ -134,13 +140,9 @@ static int srsmith_pool_srs_2c_tx_callback(r_device *decoder, bitbuffer_t *bitbu
     /* clang-format off */
     data = data_make(
             "model",                  "",                         DATA_STRING, "SRSmith-SRS2CTX",
-            "id",                     "Id",                       DATA_STRING, pin_string, // technically peoples pins should be different on each remote
-            "totallength",            "Total Length",             DATA_INT, totallength,
-            "packetsize",             "Packet Size",              DATA_INT, packetsize,
-            "extracted_pin",          "Extracted PIN",            DATA_STRING, pin_string,
-            "button_pressed_id",      "Pushed Button ID",         DATA_FORMAT, "%02x", DATA_INT, button_id,
-            "button_pressed_string",  "Pushed Button String",     DATA_STRING, button_string,
-            "total_crc",              "Total CRC",                DATA_FORMAT, "%04x", DATA_INT, total_crc,
+            "id",                     "Id",                       DATA_INT, reversed_pin, // technically peoples pins should be different on each remote
+            "button_press",           "Pushed Button ID",         DATA_FORMAT, "%02x", DATA_INT, button_id,
+            "button_press_name",      "Pushed Button String",     DATA_STRING, button_string,
             "unknown",                "Unknown",                  DATA_FORMAT, "%08x", DATA_INT, unknown_field,
             "mic",                    "Integrity",                DATA_STRING, "CRC",
             NULL);
@@ -154,12 +156,8 @@ static char *output_fields[] = {
         "model",
         "mic",
         "id",
-        "totallength",
-        "packetsize",
-        "crc",
-        "calculated_crc",
-        "button_pressed_id",
-        "extracted_pin",
+        "button_press",
+        "button_press_name",
         "unknown",
         NULL,
 };
@@ -170,6 +168,6 @@ r_device srsmith_pool_srs_2c_tx = {
         .short_width = 100,
         .long_width  = 100,
         .reset_limit = 4096,
-        .decode_fn   = &srsmith_pool_srs_2c_tx_callback,
+        .decode_fn   = &srsmith_pool_srs_2c_tx_decode,
         .fields      = output_fields,
 };
