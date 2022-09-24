76c4022a8ce401502c793c2eb2764b29e67452dd
Tobias Thurnreiter <thurnreiter@gmail.com>
2022-09-24 19:52:41 +0200

    Resolve comments

--- src/devices/wec2103.c
+++ src/devices/wec2103.c
@@ -1,5 +1,7 @@
 /** @file
-    WEC-2103 temperature/humidity sensor
+    WEC-2103 temperature/humidity sensor.
+
+    Copyright (C) 2022 Tobias Thurnreiter
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -7,30 +9,31 @@
     (at your option) any later version.
  */
 
-/*
--X 'n=name,m=OOK_PPM,s=2000,l=4000,g=4400,r=9400'
+/**
+WEC-2103 temperature/humidity sensor.
 
 Similar to prologue, kedsum, esperanza_ews, s3318p
 Only available information for this device: https://fcc.report/FCC-ID/WEC-2103
 
+Data:
 
-Byte:      0        1        2        3        4        5
-Nibble:    1   2    3   4    5   6    7   8    9   10   11
-Type:      IIIIIIII XXXXFFFF TTTTTTTT TTTTHHHH HHHHCCCC ????
+    Byte:      0        1        2        3        4        5
+    Nibble:    1   2    3   4    5   6    7   8    9   10   11
+    Type:      IIIIIIII XXXXFFFF TTTTTTTT TTTTHHHH HHHHCCCC ????
 
-I: random device ID, changes on powercycle
-X: Checksum?
-F: Flags
-T: Temperature
-H: Humidity
-Flags: tx-button pressed|?|?|?
+    I: random device ID, changes on powercycle
+    X: Checksum?
+    F: Flags
+    T: Temperature
+    H: Humidity
+    Flags: tx-button pressed|?|?|?
 
-Example datagram:
- f2 90              6b5         96       1       8
-|ID|Checksum?+Flags|Temperature|Humidity|Channel|unknown
+    Example datagram:
+     f2 90              6b5         96       1       8
+    |ID|Checksum?+Flags|Temperature|Humidity|Channel|unknown
 
-Temperature in Fahrenheit*100+900->hex
-Example: 82.4F->824->1724->0x6bc
+    Temperature in Fahrenheit*100+900->hex
+    Example: 82.4F->824->1724->0x6bc
 */
 
 #include "decoder.h"
@@ -54,40 +57,41 @@ static int wec2103_callback(r_device *decoder, bitbuffer_t *bitbuffer)
     int humidity  = ((b[3] & 0x0f) * 10) + ((b[4] & 0xf0) >> 4);
     int button   = (b[1] & 0x08) >> 3;
 
+    /* clang-format off */
     data = data_make(
             "model",            "",             DATA_STRING, "WEC-2103",
             "id",               "ID",           DATA_INT,    device_id,
             "channel",          "Channel",      DATA_INT,    channel,
             //"battery_ok",       "Battery",      DATA_INT,    !battery_low,
-            "flags",            "Flags2",       DATA_INT,    flags,
+            "flags",            "Flags",       DATA_INT,    flags,
             "temperature_F",    "Temperature",  DATA_FORMAT, "%.02f F", DATA_DOUBLE, temp_f,
             "humidity",         "Humidity",     DATA_FORMAT, "%u %%", DATA_INT, humidity,
             "button",           "Button",       DATA_INT,    button,
             //"mic",              "Integrity",    DATA_STRING, "CRC",
             NULL);
+    /* clang-format on */
 
     decoder_output_data(decoder, data);
     return 1;
 }
 
 static char *output_fields[] = {
-    "model",
-    "id",
-    "channel",
-    "flags",
-    "temperature_F",
-    "humidity",
-    NULL,
+        "model",
+        "id",
+        "channel",
+        "flags",
+        "temperature_F",
+        "humidity",
+        NULL,
 };
 
 r_device wec2103 = {
-    .name           = "WEC-2103 temperature/humidity sensor",
-    .modulation     = OOK_PULSE_PPM,
-    .short_width    = 1900,
-    .long_width     = 3800,
-    .gap_limit      = 4400,
-    .reset_limit    = 9400,
-    .decode_fn      = &wec2103_callback,
-    .disabled       = 0,
-    .fields         = output_fields,
+        .name           = "WEC-2103 temperature/humidity sensor",
+        .modulation     = OOK_PULSE_PPM,
+        .short_width    = 1900,
+        .long_width     = 3800,
+        .gap_limit      = 4400,
+        .reset_limit    = 9400,
+        .decode_fn      = &wec2103_callback,
+        .fields         = output_fields,
 };
