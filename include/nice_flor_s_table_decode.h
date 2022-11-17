b7f64312cd70f27c5ce011d39a2a37e6d652a24e
Daniel Henzulea <zulea1@gmail.com>
2022-11-17 17:48:18 +0200

    Fix Nice Flor-s decoding Serial and Code

---
+++ include/nice_flor_s_table_decode.h
@@ -0,0 +1,8 @@
+
+#ifndef INCLUDE_NICE_FLOR_S_TABLE_DECODE_H_
+#define INCLUDE_NICE_FLOR_S_TABLE_DECODE_H_
+
+extern uint16_t nice_flor_s_table_decode[65536];
+
+#endif
+
---
+++ include/nice_flor_s_table_ki.h
@@ -0,0 +1,7 @@
+
+#ifndef INCLUDE_NICE_FLOR_S_TABLE_KI_H_
+#define INCLUDE_NICE_FLOR_S_TABLE_KI_H_
+
+extern uint8_t nice_flor_s_table_ki[256];
+
+#endif
