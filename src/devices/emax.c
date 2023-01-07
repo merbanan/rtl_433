1cace8c902d8bf518d010a505010fdb384906343
ProfBoc75 <nice.burger@free.fr>
2023-01-07 19:51:17 +0100

    correction of model references and other rewordings

--- src/devices/emax.c
+++ src/devices/emax.c
@@ -1,7 +1,7 @@
 /** @file
     First version was for Altronics X7064 temperature and humidity sensor.
     Then updated by Profboc75 with Optex 990040 (Emax full Weather station rain gauge/wind speed/wind direction ... ref EM3390W6 )
-    
+
     Copyright (C) 2022 Christian W. Zuckschwerdt <zany@triq.net>
     based on protocol decoding by Thomas White
 
@@ -14,14 +14,16 @@
 #include "decoder.h"
 
 /**
-Altronics X7064 / Emax temperature and humidity sensor.
+Fuzhou Emax Electronic W6 Professional Weather Station (other ref WEC-W6, 3390TX W6, EM3390W6)
+Rebrand ref : Altronics x7063/4, Optex 990040 SM-040, Infactory FWS-1200, Newentor Q9 Weather sensor
 
-S.a. issue #2000
+
+S.a. issue #2000 #2299 request #2300
 
 - Likely a rebranded device, sold by Altronics
 - Data length is 32 bytes with a preamble of 10 bytes (33 bytes for Rain/Wind Station)
 
---> EMAX product, sold by Altronics, Technoline, Optex, Infactory ... 
+--> Emax product, sold by Altronics, Technoline, Optex, Infactory ...
  Weather station EM3390W6 = Optex 990040 = Newentor Q9 = Infactory FWS-1200...
  Weather station are composed of a central weather display station with Wifi connection + External Temp/Hum sensor (same as Altronics X7064) + Multisensor station with Temp/Hum/Wind/Dir/Rain/UV/Lux.
 
@@ -33,7 +35,7 @@ Temp/Hum Sensor :
     AA AC II IB AT TA AT HH AA AA AA AA AA AA AA AA AA AA AA AA AA AA AA AA AA AA AA AA AA AA AA SS
 
 default empty = 0xAA
-    
+
 - K: (4 bit) Kind of device, = A if Temp/Hum Sensor or = 0 if Weather Rain/Wind station
 - C: (4 bit) channel ( = 4 for Weather Rain/wind station)
 - I: (12 bit) ID
@@ -64,7 +66,7 @@ Weather Rain/Wind station : hummidity not at same byte position.
     AA 04 II IB 0T TT HH 0W WW 0D DD RR RR 0U LL LL 04 05 06 07 08 09 10 11 12 13 14 15 16 17 xx SS yy
 
 default empty/null = 0x01 => value = 0
-  
+
 - K: (4 bit) Kind of device, = A if Temp/Hum Sensor or = 0 if Weather Rain/Wind station
 - C: (4 bit) channel ( = 4 for Weather Rain/wind station)
 - I: (12 bit) ID
@@ -82,7 +84,7 @@ default empty/null = 0x01 => value = 0
 - yy: incremental value each tx yy = xx + 1
 - S: (8 bit) checksum
 
-Raw Data: 
+Raw Data:
 
     ff ff 80 00 aa aa aa aa aa ca ca 54
     aa 04 59 41 06 1f 42 01 01 01 81 01 16 01 01 01 04 05 06 07 08 09 10 11 12 13 14 15 16 17 9d ad 9e
@@ -95,7 +97,7 @@ Format string:
 Decoded example:
 
     aa KD:0 CH:4 ID:594 FLAGS:0001 0 TEMP:61f (66.7F) HUM:42 (66%) Wind: 101 ( = 000 * 0.2 = 0 kmh) 0 Direction: 181 ( = 080 = 128°) Rain: 0116 ( 0015 * 0.2  = 4.2 mm) 0 UV: 1 (0 UV) Lux: 0101 (0 Lux) 04 05 ...16 17 xx:9d CHKSUM:ad yy:9e
-    
+
 */
 
 static int emax_decode(r_device *decoder, bitbuffer_t *bitbuffer)
@@ -132,22 +134,22 @@ static int emax_decode(r_device *decoder, bitbuffer_t *bitbuffer)
         }
 
         int channel     = (b[1] & 0x0f);
-        int kind        = ((b[1] & 0xf0) >> 4); 
+        int kind        = ((b[1] & 0xf0) >> 4);
         int id          = (b[2] << 4) | (b[3] >> 4);
         int battery_low = (b[3] & 0x08);
         int pairing     = (b[3] & 0x04);
-        
+
         // depend if external temp/hum sensor or Weather rain/wind station the values are not decode the same
-        
+
         if (kind != 0) {  // if not Rain/Wind ... sensor
-        
+
             int temp_raw    = ((b[4] & 0x0f) << 8) | (b[5] & 0xf0) | (b[6] & 0x0f); // weird format
             float temp_f    = (temp_raw - 900) * 0.1f;
             int humidity    = b[7];
 
             /* clang-format off */
             data_t *data = data_make(
-                    "model",            "",                 DATA_STRING, "EMAX Temperature Humidity sensor",
+                    "model",            "",                 DATA_STRING, "Altronics-X7064",
                     "id",               "",                 DATA_FORMAT, "%03x", DATA_INT,    id,
                     "channel",          "Channel",          DATA_INT,    channel,
                     "battery_ok",       "Battery_OK",       DATA_INT,    !battery_low,
@@ -162,25 +164,25 @@ static int emax_decode(r_device *decoder, bitbuffer_t *bitbuffer)
             return 1;
         }
         else {  // if Rain/Wind sensor
-        
+
             int temp_raw      = ((b[4] & 0x0f) << 8) | (b[5]); // weird format
             float temp_f      = (temp_raw - 900) * 0.1f;
             int humidity      = b[6];
             int wind_raw      = ((b[7] - 1) << 8 ) | (b[8] -1);   // all default is 01 so need to remove 1 from all bytes.
-            float speed_kmh     = wind_raw * 0.2f;        
+            float speed_kmh     = wind_raw * 0.2f;
             int direction_deg = (((b[9] & 0x0f) - 1) << 8) | (b[10] - 1);
             int rain_raw      = ((b[11] - 1) << 8 ) | (b[12] -1);
-            float rain_mm     = rain_raw * 0.2f;            
+            float rain_mm     = rain_raw * 0.2f;
             int uv_index      = (b[13] & 0x0f) - 1;
             int lux_multi     = ((b[14] & 0x80) >> 7);
             int light_lux     = ((b[14] & 0x7f) - 1) << 8 | (b[15] - 1);
             if (lux_multi == 1) {
                 light_lux = light_lux * 10;
             }
-            
+
             /* clang-format off */
             data_t *data = data_make(
-                    "model",            "",                 DATA_STRING, "EMAX Temp Hum Rain Wind UV Lux Sensor",
+                    "model",            "",                 DATA_STRING, "Emax Temp Hum Rain Wind UV Lux Sensor",
                     "id",               "",                 DATA_FORMAT, "%03x", DATA_INT,    id,
                     "channel",          "Channel",          DATA_INT,    channel,
                     "battery_ok",       "Battery_OK",       DATA_INT,    !battery_low,
@@ -198,7 +200,7 @@ static int emax_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 
             decoder_output_data(decoder, data);
             return 1;
-                        
+
         }
     }
     return ret;
@@ -222,7 +224,7 @@ static char *output_fields[] = {
 };
 
 r_device emax = {
-        .name        = "EMAX Weather sensor",
+        .name        = "Emax W6 and rebrand Altronics x7063/4, Optex 990040 SM-040, Infactory FWS-1200, Newentor Q9 Weather sensor",
         .modulation  = FSK_PULSE_PCM,
         .short_width = 90,
         .long_width  = 90,
