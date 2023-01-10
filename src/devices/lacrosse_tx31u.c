2231cd8f237608f39835e85ff462ead7b0d38f17
agspoon <agspoon@gmail.com>
2023-01-10 00:01:33 -0800

    Lacrosse TX31U-IT
    * First working version

--- src/devices/lacrosse_tx31u.c
+++ src/devices/lacrosse_tx31u.c
@@ -24,16 +24,16 @@ This transmitter uses a variable length protocol that includes 1-5 measurements
 of 2 bytes each.  The first nibble of each measurement identifies the sensor.
 
     Sensor    Code    Encoding
-    TEMP      0       BCD tenths of a degree C plus 400 offset.
-                      EX: 0x0653 is 25.3 degrees C 
-         TODO             EX:  0x0237 is -16.3 degrees C 
-    HUMID     1       BCD % relative humidity.
-                      EX: 0x1068 is 68%
-    UNKNOWN   2       This is probably reserved for a rain gauge (TX32U-IT)
-    WIND_AVG  3       BCD time averaged wind speed in M/sec.
-                      Second nibble is 0x1 if wind sensor was ever detected.
-    WIND_MAX  4       BCD maximum wind speed in M/sec during last reporting interval.
-                      Second nibble is 0x1 if wind sensor input is lost.
+    TEMP        0       BCD tenths of a degree C plus 400 offset.
+                            EX: 0x0653 is 25.3 degrees C 
+         TODO               EX:  0x0237 is -16.3 degrees C 
+    HUMID       1       BCD % relative humidity.
+                            EX: 0x1068 is 68%
+    UNKNOWN     2       This is probably reserved for a rain gauge (TX32U-IT)
+    WIND_SPEED  3       BCD time averaged wind speed in M/sec.
+                            Second nibble is 0x1 if wind sensor was ever detected.
+    WIND_GUST   4       BCD maximum wind speed in M/sec during last reporting interval.
+                            Second nibble is 0x1 if wind sensor input is lost.
 
 
 
@@ -77,68 +77,17 @@ of 2 bytes each.  The first nibble of each measurement identifies the sensor.
 
 #include "decoder.h"
 
+#define BIT(pos) ( 1<<(pos) )
+#define CHECK_BIT(y, pos) ( ( 0u == ( (y)&(BIT(pos)) ) ) ? 0u : 1u )
+#define SET_LSBITS(len) ( BIT(len)-1 ) // the first len bits are '1' and the rest are '0'
+#define BF_PREP(y, start, len) ( ((y)&SET_LSBITS(len)) << (start) ) // Prepare a bitmask
+#define BF_GET(y, start, len) ( ((y)>>(start)) & SET_LSBITS(len) )
+
+#define TX31U_MIN_LEN_BYTES 9       // assume at least one measurement
+#define TX31U_MAX_LEN_BYTES 20      // actually shouldn't be more than 18, but we'll be generous
+
 static int lacrosse_tx31u_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 {
- /*
-        // remove preamble and keep only five octets
-        uint8_t b[5];
-        bitbuffer_extract_bytes(bitbuffer, row, start_pos + 20, b, 40);
-
-        // Check message integrity
-        int r_crc = b[4];
-        int c_crc = crc8(b, 4, 0x31, 0x00);
-        if (r_crc != c_crc) {
-            decoder_logf(decoder, 1, __func__, "LaCrosse TX29/35 bad CRC: calculated %02x, received %02x", c_crc, r_crc);
-            // reject row
-            continue; // DECODE_FAIL_MIC
-        }
-
-        // message "envelope" has been validated, start parsing data
-        int sensor_id   = ((b[0] & 0x0f) << 2) | (b[1] >> 6);
-        float temp_c    = 10 * (b[1] & 0x0f) + 1 * ((b[2] >> 4) & 0x0f) + 0.1f * (b[2] & 0x0f) - 40.0f;
-        int new_batt    = (b[1] >> 5) & 1;
-        int battery_low = b[3] >> 7;
-        int humidity    = b[3] & 0x7f;
-
-        data_t *data;
-        if ((humidity == LACROSSE_TX29_NOHUMIDSENSOR) || (humidity == LACROSSE_TX25_PROBE_FLAG)) {
-            if (humidity == LACROSSE_TX25_PROBE_FLAG)
-                sensor_id += 0x40;      // Change ID to distinguish between the main and probe channels
-            data = data_make(
-                    "model",            "",             DATA_STRING, (device29or35 == 29 ? "LaCrosse-TX29IT" : "LaCrosse-TX35DTHIT"),
-                    "id",               "",             DATA_INT,    sensor_id,
-                    "battery_ok",       "Battery",      DATA_INT,    !battery_low,
-                    "newbattery",       "NewBattery",   DATA_INT,    new_batt,
-                    "temperature_C",    "Temperature",  DATA_FORMAT, "%.1f C", DATA_DOUBLE, temp_c,
-                    "mic",              "Integrity",    DATA_STRING, "CRC",
-                    NULL);
-        }
-        else {
-            data = data_make(
-                    "model",            "",             DATA_STRING, (device29or35 == 29 ? "LaCrosse-TX29IT" : "LaCrosse-TX35DTHIT"),
-                    "id",               "",             DATA_INT,    sensor_id,
-                    "battery_ok",       "Battery",      DATA_INT,    !battery_low,
-                    "newbattery",       "NewBattery",   DATA_INT,    new_batt,
-                    "temperature_C",    "Temperature",  DATA_FORMAT, "%.1f C", DATA_DOUBLE, temp_c,
-                    "humidity",         "Humidity",     DATA_FORMAT, "%u %%", DATA_INT, humidity,
-                    "mic",              "Integrity",    DATA_STRING, "CRC",
-                    NULL);
-        }
-
-        decoder_output_data(decoder, data);
-        events++;
-    }
-*/
-return DECODE_ABORT_LENGTH; //TODO
-    return 1;
-}
-
-static int lacrosse_tx31u_callback(r_device *decoder, bitbuffer_t *bitbuffer)
-{
-    // preamble + sync word - 32 bits
-    uint8_t const preamble[] = {0xaa, 0xaa, 0x2d, 0xd4};
-
-    int rtn = 0;
 
     // There will only be one row
     if (bitbuffer->num_rows > 1) {
@@ -146,41 +95,123 @@ static int lacrosse_tx31u_callback(r_device *decoder, bitbuffer_t *bitbuffer)
         return DECODE_FAIL_SANITY;
     }
 
-    //TODO
+//TODO remove unnecessary logging or up the level to 2,3,4
     decoder_logf(decoder, 1, __func__, "Something detected, buffer is %d bits length", bitbuffer->bits_per_row[0]);
 
-    // search for preamble
-    unsigned int start_pos = bitbuffer_search(bitbuffer, 0, 0, preamble, sizeof(preamble)*8);
-    if (start_pos >= bitbuffer->bits_per_row[0])
+    // search for expected start sequence
+    uint8_t const start_match[] = {0xaa, 0xaa, 0x2d, 0xd4}; // preamble + sync word (32 bits)
+    unsigned int start_pos = bitbuffer_search(bitbuffer, 0, 0, start_match, sizeof(start_match)*8);
+    if (start_pos >= bitbuffer->bits_per_row[0]) {
         return DECODE_ABORT_EARLY;
-
-    decoder_logf(decoder, 1, __func__, "LaCrosse TX31U-IT detected, buffer is %d bits length", bitbuffer->bits_per_row[0]);
-
-    uint8_t msg[20];
-    uint8_t lenbytes = (bitbuffer->bits_per_row[0] - start_pos)/8;
-    decoder_logf(decoder, 1, __func__, "lenbytes = %d", lenbytes);
-    bitbuffer_extract_bytes(bitbuffer, 0, start_pos, msg, lenbytes*8);
-    for (int i=0; i<lenbytes; ++i ) {
-	decoder_logf(decoder, 1, __func__, "Byte %d = %02x", i, msg[i]);
     }
-	decoder_logf(decoder, 1, __func__, "Count %d, Code_1 = %d, Temp = %x.%02x",
-		                	msg[5] & 0x7, ((msg[6]>>4) & 0xf) - 0x40, msg[6] & 0xf, msg[7] );
-    /*
-    int msg_len = bitbuffer->bits_per_row[0];
-    if (msg_len < 200) { // allows shorter preamble for LTV-R3
-        decoder_logf(decoder, 1, __func__, "Packet too short: %d bits", msg_len);
+    uint8_t msg_bytes = (bitbuffer->bits_per_row[0] - start_pos)/8;
+
+    if (msg_bytes < TX31U_MIN_LEN_BYTES) { 
+        decoder_logf(decoder, 1, __func__, "Packet too short: %d bytes", msg_bytes);
         return DECODE_ABORT_LENGTH;
-    } else if (msg_len > 272) {
-        decoder_logf(decoder, 1, __func__, "Packet too long: %d bits", msg_len);
+    } else if (msg_bytes > TX31U_MAX_LEN_BYTES) {
+        decoder_logf(decoder, 1, __func__, "Packet too long: %d bytes", msg_bytes);
         return DECODE_ABORT_LENGTH;
     } else {
-        decoder_logf(decoder, 1, __func__, "packet length: %d", msg_len);
+        decoder_logf(decoder, 1, __func__, "packet length: %d", msg_bytes); //TODO should be level 2
     }
-    */
 
-    rtn = lacrosse_tx31u_decode(decoder, bitbuffer);
+    decoder_log(decoder, 2, __func__, "LaCrosse TX31U-IT detected");
 
-    return rtn;
+    uint8_t msg[TX31U_MAX_LEN_BYTES];
+    bitbuffer_extract_bytes(bitbuffer, 0, start_pos, msg, msg_bytes*8);
+
+// TODO remove
+    for (int i=0; i<msg_bytes; ++i ) {
+	decoder_logf(decoder, 1, __func__, "Byte %d = %02x", i, msg[i]);
+    }
+//
+
+    //TODO int model = BF_GET(msg[4], 4, 4);
+    int sensor_id = (BF_GET(msg[4], 0, 4) << 2) | BF_GET(msg[5], 6, 2);
+    //TODO int training = CHECK_BIT(msg[5], 5);
+    int no_ext_sensor = CHECK_BIT(msg[5], 4); 
+    int battery_low = CHECK_BIT(msg[5], 3); 
+    int measurements = BF_GET(msg[5], 0, 3);
+
+//TODO    decoder_logf(decoder, 1, __func__, "model %d, id = 0x%02x, training = %d, no_ext_sensor = %d", model, sensor_id, training, no_ext_sensor );
+//TODO    decoder_logf(decoder, 1, __func__, "battery_low = %d, measurements = %d", battery_low, measurements );
+
+    // Check message integrity.  
+    int r_crc = msg[6+measurements*2];
+    int c_crc = crc8(&msg[4], 2 + measurements*2, 0x31, 0x00);
+    if (r_crc != c_crc) {
+	decoder_logf(decoder, 1, __func__, "LaCrosse TX31U-IT bad CRC: calculated %02x, received %02x", c_crc, r_crc);
+	return DECODE_FAIL_MIC;
+    }
+
+    /* clang-format off */
+    data_t *data = data_make(
+	    "model",            "",             DATA_STRING, "LaCrosse-TX31U-IT",
+	    "id",               "",             DATA_INT,    sensor_id,
+	    "battery_ok",       "Battery",      DATA_INT,    !battery_low,
+	    NULL);
+
+    // decode what measurements we get and append them.  All measurements are BCD coded.
+    enum sensor_type { TEMP=0, HUMIDITY, RAIN, WIND_SPEED, WIND_GUST };
+    for (int m=0; m<measurements; ++m ) {
+        uint8_t type = BF_GET(msg[6+m*2], 4, 4 );
+        uint8_t bcd1 = BF_GET(msg[6+m*2], 0, 4 );
+        uint8_t bcd2 = BF_GET(msg[7+m*2], 4, 4 );
+        uint8_t bcd3 = BF_GET(msg[7+m*2], 0, 4 );
+        switch (type) {
+	    case TEMP: { 
+		float temp_c = 10*bcd1 + bcd2 + 0.1f*bcd3 - 40.0f;
+		data = data_append( data,
+		    "temperature_C",    "Temperature",  DATA_FORMAT, "%.1f C", DATA_DOUBLE, temp_c,
+		    NULL);
+	    } break;
+	    case HUMIDITY: {
+		int humidity = 100*bcd1 + 10*bcd2 + bcd3;
+		data = data_append( data,
+		    "humidity",         "Humidity",     DATA_FORMAT, "%u %%", DATA_INT, humidity,
+		    NULL);
+	    } break;
+	    case RAIN: {
+		int raw_rain = 100*bcd1 + 10*bcd2 + bcd3;
+		if ( !no_ext_sensor && raw_rain > 0) { // most of these do not have rain gauges.  Surpress output if zero.
+		    data = data_append( data,
+			"rain",         "raw_rain",      DATA_FORMAT, "%03x", DATA_INT, raw_rain,
+			NULL);
+		}
+	    } break;
+	    case WIND_SPEED: {
+		int wind_sensor_detected = CHECK_BIT(bcd1, 0); // a sensor is attached
+		if ( !no_ext_sensor && wind_sensor_detected ) {
+		    float wind_speed = (10*bcd2 + bcd3) * 0.1f;
+		    data = data_append( data,
+			"wind_speed",  "Wind speed",    DATA_FORMAT, "%.1f km/h",  DATA_DOUBLE, wind_speed,
+			NULL);
+		}
+	    } break;
+	    case WIND_GUST: {
+                int wind_input_lost = CHECK_BIT(bcd1, 0); // a sensor was attched, but now not detected
+		if ( !no_ext_sensor && !wind_input_lost ) {
+		    float wind_gust = (10*bcd2 + bcd3) * 0.1f;
+		    data = data_append( data,
+			"wind_gust",  "Wind gust",    DATA_FORMAT, "%.1f km/h",  DATA_DOUBLE, wind_gust,
+			NULL);
+                }
+	    } break;
+	    default:
+		decoder_logf(decoder, 1, __func__, "LaCrosse TX31U-IT unknown sensor type %d", type);
+	    break;
+	}
+    }
+
+    data = data_append( data,
+	    "mic",              "Integrity",    DATA_STRING, "CRC",
+	    NULL);
+    /* clang-format on */
+
+    decoder_output_data(decoder, data);
+
+    return 1;
 }
 
 static char *output_fields[] = {
@@ -189,8 +220,8 @@ static char *output_fields[] = {
         "low_battery",
         "temperature",
         "humidity",
-        "avg_wind_spd",
-        "peak_wind_spd",
+        "wind_speed",
+        "wind_gust",
         "mic",
         NULL,
 };
@@ -202,6 +233,6 @@ r_device lacrosse_tx31u = {
         .short_width = 116,
         .long_width  = 116,
         .reset_limit = 20000,
-        .decode_fn   = &lacrosse_tx31u_callback,
+        .decode_fn   = &lacrosse_tx31u_decode,
         .fields      = output_fields,
 };
