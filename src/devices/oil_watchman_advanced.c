21deb4daff6901c78199c632443373de55e23e07
Gareth Potter <gareth.potter@me.com>
2023-01-18 22:20:51 +0000

    Watchman Sonic Advanced
    - fix whitespace

--- src/devices/oil_watchman_advanced.c
+++ src/devices/oil_watchman_advanced.c
@@ -1,6 +1,6 @@
 /** @file
     Watchman Sonic Advanced/Plus oil tank level monitor.
-    
+
     Copyright (C) 2023 Gareth Potter
 
     This program is free software; you can redistribute it and/or modify
@@ -19,6 +19,8 @@ The format might be most easily summarised in a BitBench string:
 
 PRE: 40b SYNC: 16h MODEL: 24h ID: 24d 8h TEMP: 8h 16h DEPTH: 8d 32h CRC: 16h
 
+In more detail:
+
 - 40 bits of preamble, i.e. 10101010 etc.
 - 0x2dd4 - 'standard' sync word
 - 0x0e0401 - presumably a model identifier, common at least to the two devices we have tested this on
@@ -28,7 +30,7 @@ PRE: 40b SYNC: 16h MODEL: 24h ID: 24d 8h TEMP: 8h 16h DEPTH: 8d 32h CRC: 16h
 - two more varying bytes currently of unknown purpose
 - 1 byte integer depth (i.e. the distance between the sensor and the oil in the tank)
 - 0x01050300 - 4 bytes of constant values
-- 2 byte CRC 
+- 2 byte CRC
 
 Tested devices:
 - Watchman Sonic Advanced
@@ -42,7 +44,7 @@ static int oil_watchman_advanced_decode(r_device *decoder, bitbuffer_t *bitbuffe
     static const uint8_t BODY_LENGTH_BITS = 128;
     // no need to match all the preamble; 24 bits worth should do
     uint8_t const match_pattern[8] = {0xaa, 0xaa, 0xaa, 0x2d, 0xd4, 0x0e, 0x04, 0x01};
-    
+
     unsigned bitpos      = 0;
     int events           = 0;
 
@@ -61,7 +63,7 @@ static int oil_watchman_advanced_decode(r_device *decoder, bitbuffer_t *bitbuffe
         }
 
         // as printed on the side of the unit
-        uint32_t serial_number = (b[3] << 16) | (b[4] << 8) | b[5];        
+        uint32_t serial_number = (b[3] << 16) | (b[4] << 8) | b[5];
         float temperature = 0.5 * (b[7] - 0x45); // seems about right; see discussion in issue #2306
         uint8_t depth = b[10];
 
@@ -70,7 +72,7 @@ static int oil_watchman_advanced_decode(r_device *decoder, bitbuffer_t *bitbuffe
                 "model",                "Model",        DATA_STRING, "Oil-SonicAdv",
                 "id",                   "ID",           DATA_FORMAT, "%08d", DATA_INT, serial_number,
                 "temperature_C",        "Temperature",  DATA_DOUBLE, temperature,
-                "depth_cm",             "Depth",        DATA_INT,    depth,                
+                "depth_cm",             "Depth",        DATA_INT,    depth,
                 "mic",                  "Integrity",    DATA_STRING, "CRC",
                 NULL);
         /* clang-format on */
