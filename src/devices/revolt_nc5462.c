8a4e0971e6fea5b76d5fb053e6a26a7bdabdaefe
Nicolai Hess <nicolaihess@gmail.com>
2023-01-26 23:29:23 +0100

    fix more code style errors

--- src/devices/revolt_nc5462.c
+++ src/devices/revolt_nc5462.c
@@ -12,7 +12,7 @@
 /**
   revolt energy power meter.
   sends on 433.92 MHz.
- 
+
   Pulse Width Modulation with startbit/delimiter
   Two Modes:
   Normal data mode:
@@ -43,20 +43,20 @@
    |flag      |high      |
    |          |yes/no    |
    +----------+----------+
-   
-  
- 
-  "Register" mode 
+
+
+
+  "Register" mode
   (after pushing button on energy meter)
   same 104 data pulses as in data mode, but first bit high and multiple rows of (the same)
   data.
- 
+
   Pulses
   sync ~ 10 ms high / 280 us low
   1-bit ~ 320 us high / 160 us low
   0-bit ~ 180 us high / 160 us low
   message end 180 us high / 100 ms low
- 
+
   rtl_433 demodulation output
   (normal data)
   short_width: 200, long_width: 330, reset_limit: 240, sync_width: 10044
@@ -77,7 +77,7 @@ static int check_bitbuffer_row1byte12(bitbuffer_t *bitbuffer)
       checksum = 0;
       byte12 = bitbuffer->bb[0][11];
       for(index=0; index<11;++index) {
-	checksum+=bitbuffer->bb[0][index];
+        checksum+=bitbuffer->bb[0][index];
       }
       return checksum == byte12;
     }
@@ -99,16 +99,16 @@ static int revolt_nc5462_callback(r_device *decoder, bitbuffer_t *bitbuffer) {
     uint16_t energy;
     uint8_t detect_flag;
     bitbuffer_invert(bitbuffer);
-    
+
     if (check_bitbuffer_row1byte12(bitbuffer)) {
       id = (bb[0][0]<<8) | (bb[0][1]);
       if((id & 0x8000) == 0x8000) {
-	detect_flag = 1;
-	id &= ~0x8000;
+        detect_flag = 1;
+        id &= ~0x8000;
       } else {
-	detect_flag = 0;
+        detect_flag = 0;
       }
-	  
+
       voltage = bb[0][2];
       current = bb[0][4] | bb[0][3]<<8;
       frequency = bb[0][5];
@@ -133,37 +133,27 @@ static int revolt_nc5462_callback(r_device *decoder, bitbuffer_t *bitbuffer) {
 }
 
 static char* output_fields[] = {
-	"time",
-	"model",
-	"id",
-	"voltage",
-	"current",
-	"frequency",
-	"power",
-	"power factor",
-	"energy",
-	"detect flag",
-	NULL
+        "time",
+        "model",
+        "id",
+        "voltage",
+        "current",
+        "frequency",
+        "power",
+        "power factor",
+        "energy",
+        "detect flag",
+        NULL
 };
 
 
-	/* .short_limit    = 260, */
-	/* .long_limit     = 5200, */
-	/* .reset_limit    = 288, */
-
-	/* .short_limit    = 68*4, */
-	/* .long_limit     = 1297*4, */
-	/* .reset_limit    = 61*4, */
-
 r_device revolt_nc5462 = {
-	.name           = "Revolt NC-5642",
-	.modulation     = OOK_PULSE_PWM,
-	.short_width    = 200, 
-	.long_width     = 320, 
-	.sync_width     = 10024,
-	.reset_limit	= 272,
-	//.json_callback  = &revolt_nc5462_callback,
-	//.demod_arg      = 2,
-	.decode_fn      = &revolt_nc5462_callback,
-	.fields         = output_fields,
+        .name           = "Revolt NC-5642",
+        .modulation     = OOK_PULSE_PWM,
+        .short_width    = 200,
+        .long_width     = 320,
+        .sync_width     = 10024,
+        .reset_limit    = 272,
+        .decode_fn      = &revolt_nc5462_callback,
+        .fields         = output_fields,
 };
