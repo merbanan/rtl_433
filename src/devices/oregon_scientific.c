573657b4b5c338f3a8dbd687054e3f70095720f3
andre <diepeterpan@gmail.com>
2023-03-12 11:13:31 +0200

    Remove trailing white space

--- src/devices/oregon_scientific.c
+++ src/devices/oregon_scientific.c
@@ -155,7 +155,7 @@ static unsigned int cm160_power(uint8_t const *msg)
 static double cm160_total(uint8_t const *msg)
 {
  double total = ((uint64_t) swap_nibbles(msg[10]) << 36) | ((uint64_t) swap_nibbles(msg[9]) << 28) |
-                   (swap_nibbles(msg[8]) << 20) | (swap_nibbles(msg[7]) << 12) | 
+                   (swap_nibbles(msg[8]) << 20) | (swap_nibbles(msg[7]) << 12) |
 		   (swap_nibbles(msg[6]) << 4) | (msg[5]&0xf);
  
  //fprintf(stderr, "Raw KWH Total: %x (%d) - %7.4f \n",(int) total, (int) total, total);
@@ -831,14 +831,14 @@ static int oregon_scientific_v3_decode(r_device *decoder, bitbuffer_t *bitbuffer
         if (validate_os_checksum(decoder, msg, 22) != 0)
             return DECODE_FAIL_MIC;
 
-        //channel add from https://github.com/magellannh/rtl-wx/blob/dbaf3924815903c47b230c65841d18b263421854/src/rtl-433fm-decode.c 
+        //channel add from https://github.com/magellannh/rtl-wx/blob/dbaf3924815903c47b230c65841d18b263421854/src/rtl-433fm-decode.c
         int channel = msg[0]>>4;
-        int id = msg[1] & 0x0F; 
+        int id = msg[1] & 0x0F;
 
-        //cm160 ipower rework & total energy added from https://github.com/magellannh/rtl-wx/blob/dbaf3924815903c47b230c65841d18b263421854/src/rtl-433fm-decode.c 
+        //cm160 ipower rework & total energy added from https://github.com/magellannh/rtl-wx/blob/dbaf3924815903c47b230c65841d18b263421854/src/rtl-433fm-decode.c
         unsigned int raw_ipower = cm160_power(msg); 
         unsigned int ipower = (raw_ipower / (0.27*230) * 1000); //Assuming device is running in 230V country
-        //Alternate formula = (raw_ipower * 0.07) * 230 - https://github.com/cornetp/eagle-owl/blob/master/src/cm160.c 
+        //Alternate formula = (raw_ipower * 0.07) * 230 - https://github.com/cornetp/eagle-owl/blob/master/src/cm160.c
 
         double raw_total_energy = cm160_total(msg);
         double total_kWh = (raw_total_energy * 230 ) / 3600.0 / 1000.0;  //Assuming device is running in 230V country
