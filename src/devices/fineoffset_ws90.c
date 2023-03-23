44cdbed92efa23063bc49ae0e3698212afbb6488
John Pochmara <127617714+jpochmara@users.noreply.github.com>
2023-03-23 10:01:27 -0700

    Update fineoffset_ws90.c
    Updated line 132 to be static char const *const type.

--- src/devices/fineoffset_ws90.c
+++ src/devices/fineoffset_ws90.c
@@ -129,7 +129,7 @@ static int fineoffset_ws90_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     return 1;
 }
 
-static char const *output_fields[] = {
+static char const *const output_fields[] = {
         "model",
         "id",
         "battery_ok",
