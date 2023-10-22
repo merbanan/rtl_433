312ada8b71cb17acef2ef6aa5fc19178af26eab4
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2023-10-22 10:57:58 +0200

    minor: Clean up style in Bresser-leakage

--- src/devices/bresser_leakage.c
+++ src/devices/bresser_leakage.c
@@ -9,9 +9,13 @@
     (at your option) any later version.
 */
 
+#include "decoder.h"
+
 #define SENSOR_TYPE_LEAKAGE 5
 
 /**
+Bresser Water Leakage Sensor.
+
 Decoder for Bresser Water Leakage outdoor sensor, PN 7009975
 
 see https://github.com/merbanan/rtl_433/issues/2576
@@ -21,6 +25,7 @@ Based on bresser_6in1.c
 Preamble: aa aa 2d d4
 
 Data layout:
+
     CCCCCCCC CCCCCCCC IIIIIIII IIIIIIII IIIIIIII IIIIIIII SSSSQHHH ANBBFFFF
 
 - C: 16-bit, crc16/xmodem, polynomial: 0x1021, init: 0x0000, range: byte 2...6
@@ -35,49 +40,38 @@ Data layout:
 
 
 Examples:
----------
-[Bresser Water Leakage Sensor, PN 7009975]
 
-[00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25]
+    [Bresser Water Leakage Sensor, PN 7009975]
 
- C7 70 35 97 04 08 57 70 00 00 00 00 00 00 00 00 03 FF FF FF FF FF FF FF FF FF [CH7]
- DF 7D 36 49 27 09 56 70 00 00 00 00 00 00 00 00 03 FF FF FF FF FF FF FF FF FF [CH6]
- 9E 30 79 84 33 06 55 70 00 00 00 00 00 00 00 00 03 FF FD DF FF BF FF DF FF FF [CH5]
- E2 C8 68 27 91 24 54 70 00 00 00 00 00 00 00 00 03 FF FF FF FF FF FF FF FF FF [CH4]
- B3 DA 55 57 17 40 53 70 00 00 00 00 00 00 00 00 03 FF FF FF FF FF FF FF FF FB [CH3]
- 37 FA 84 73 03 02 52 70 00 00 00 00 00 00 00 00 03 FF FF FF DF FF FF FF FF FF [CH2]
- 27 F3 80 02 52 88 51 70 00 00 00 00 00 00 00 00 03 FF FF FF FF FF DF FF FF FF [CH1]
- A6 FB 80 02 52 88 59 70 00 00 00 00 00 00 00 00 03 FD F7 FF FF BF FF FF FF FF [CH1+NSTARTUP]
- A6 FB 80 02 52 88 59 B0 00 00 00 00 00 00 00 00 03 FF FF FF FD FF F7 FF FF FF [CH1+NSTARTUP+ALARM]
- A6 FB 80 02 52 88 59 70 00 00 00 00 00 00 00 00 03 FF FF BF F7 F7 FD 7F FF FF [CH1+NSTARTUP]
- [Reset]
- C0 10 36 79 37 09 51 70 00 00 00 00 00 00 00 00 01 1E FD FD FF FF FF DF FF FF [CH1]
- C0 10 36 79 37 09 51 B0 00 00 00 00 00 00 00 00 03 FE FD FF AF FF FF FF FF FD [CH1+ALARM]
- [Reset]
- 71 9C 54 81 72 09 51 40 00 00 00 00 00 00 00 00 0F FF FF FF FF FF FF DF FF FE [CH1+BATT_LO]
- 71 9C 54 81 72 09 51 40 00 00 00 00 00 00 00 00 0F FE FF FF FF FF FB FF FF FF
- 71 9C 54 81 72 09 51 40 00 00 00 00 00 00 00 00 07 FD F7 FF DF FF FF DF FF FF
- 71 9C 54 81 72 09 51 80 00 00 00 00 00 00 00 00 1F FF FF F7 FF FF FF FF FF FF [CH1+BATT_LO+ALARM]
- F0 94 54 81 72 09 59 40 00 00 00 00 00 00 00 00 0F FF DF FF FF FF FF BF FD F7 [CH1+BATT_LO+NSTARTUP]
- F0 94 54 81 72 09 59 80 00 00 00 00 00 00 00 00 03 FF B7 FF ED FF FF FF DF FF [CH1+BATT_LO+NSTARTUP+ALARM]
+    [00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25]
+
+     C7 70 35 97 04 08 57 70 00 00 00 00 00 00 00 00 03 FF FF FF FF FF FF FF FF FF [CH7]
+     DF 7D 36 49 27 09 56 70 00 00 00 00 00 00 00 00 03 FF FF FF FF FF FF FF FF FF [CH6]
+     9E 30 79 84 33 06 55 70 00 00 00 00 00 00 00 00 03 FF FD DF FF BF FF DF FF FF [CH5]
+     E2 C8 68 27 91 24 54 70 00 00 00 00 00 00 00 00 03 FF FF FF FF FF FF FF FF FF [CH4]
+     B3 DA 55 57 17 40 53 70 00 00 00 00 00 00 00 00 03 FF FF FF FF FF FF FF FF FB [CH3]
+     37 FA 84 73 03 02 52 70 00 00 00 00 00 00 00 00 03 FF FF FF DF FF FF FF FF FF [CH2]
+     27 F3 80 02 52 88 51 70 00 00 00 00 00 00 00 00 03 FF FF FF FF FF DF FF FF FF [CH1]
+     A6 FB 80 02 52 88 59 70 00 00 00 00 00 00 00 00 03 FD F7 FF FF BF FF FF FF FF [CH1+NSTARTUP]
+     A6 FB 80 02 52 88 59 B0 00 00 00 00 00 00 00 00 03 FF FF FF FD FF F7 FF FF FF [CH1+NSTARTUP+ALARM]
+     A6 FB 80 02 52 88 59 70 00 00 00 00 00 00 00 00 03 FF FF BF F7 F7 FD 7F FF FF [CH1+NSTARTUP]
+     [Reset]
+     C0 10 36 79 37 09 51 70 00 00 00 00 00 00 00 00 01 1E FD FD FF FF FF DF FF FF [CH1]
+     C0 10 36 79 37 09 51 B0 00 00 00 00 00 00 00 00 03 FE FD FF AF FF FF FF FF FD [CH1+ALARM]
+     [Reset]
+     71 9C 54 81 72 09 51 40 00 00 00 00 00 00 00 00 0F FF FF FF FF FF FF DF FF FE [CH1+BATT_LO]
+     71 9C 54 81 72 09 51 40 00 00 00 00 00 00 00 00 0F FE FF FF FF FF FB FF FF FF
+     71 9C 54 81 72 09 51 40 00 00 00 00 00 00 00 00 07 FD F7 FF DF FF FF DF FF FF
+     71 9C 54 81 72 09 51 80 00 00 00 00 00 00 00 00 1F FF FF F7 FF FF FF FF FF FF [CH1+BATT_LO+ALARM]
+     F0 94 54 81 72 09 59 40 00 00 00 00 00 00 00 00 0F FF DF FF FF FF FF BF FD F7 [CH1+BATT_LO+NSTARTUP]
+     F0 94 54 81 72 09 59 80 00 00 00 00 00 00 00 00 03 FF B7 FF ED FF FF FF DF FF [CH1+BATT_LO+NSTARTUP+ALARM]
 
  */
-
-#include "decoder.h"
 
 static int bresser_leakage_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 {
     uint8_t const preamble_pattern[] = {0xaa, 0xaa, 0x2d, 0xd4};
-    data_t *data;
     uint8_t msg[18];
-    uint32_t sensor_id;
-    uint8_t s_type;
-    uint8_t chan;
-    uint8_t startup;
-    uint8_t battery_ok;
-    int alarm;
-    int no_alarm;
-    int decode_ok;
 
     if (bitbuffer->num_rows != 1
             || bitbuffer->bits_per_row[0] < 160
@@ -113,24 +107,23 @@ static int bresser_leakage_decode(r_device *decoder, bitbuffer_t *bitbuffer)
         return DECODE_FAIL_MIC;
     }
 
-    sensor_id     = ((uint32_t)msg[2] << 24) | (msg[3] << 16) | (msg[4] << 8) | (msg[5]);
-    s_type        = msg[6] >> 4;
-    chan          = (msg[6] & 0x7);
-    battery_ok    = ((msg[7] & 0x30) != 0x00) ? 1 : 0;
-    startup       = (msg[6] >> 3) & 1;
-    alarm         = ((msg[7] & 0x80) == 0x80) ? 1 : 0;
-    no_alarm      = ((msg[7] & 0x40) == 0x40) ? 1 : 0;
+    uint32_t sensor_id = ((uint32_t)msg[2] << 24) | (msg[3] << 16) | (msg[4] << 8) | (msg[5]);
+    int s_type         = msg[6] >> 4;
+    int chan           = (msg[6] & 0x7);
+    int battery_ok     = ((msg[7] & 0x30) != 0x00) ? 1 : 0;
+    int startup        = (msg[6] >> 3) & 1;
+    int alarm          = ((msg[7] & 0x80) == 0x80) ? 1 : 0;
+    int no_alarm       = ((msg[7] & 0x40) == 0x40) ? 1 : 0;
 
     // Sanity checks
-    decode_ok = (s_type == SENSOR_TYPE_LEAKAGE) &&
-            (alarm != no_alarm) &&
-            (chan != 0);
-
-   if (!decode_ok)
-       return 0;
+    if (s_type != SENSOR_TYPE_LEAKAGE
+            || alarm == no_alarm
+            || chan == 0) {
+        return DECODE_FAIL_SANITY;
+    }
 
     /* clang-format off */
-    data = data_make(
+    data_t *data = data_make(
             "model",            "",             DATA_STRING, "Bresser-Leakage",
             "id",               "",             DATA_FORMAT, "%08x",   DATA_INT,    sensor_id,
             "channel",          "",             DATA_INT,    chan,
@@ -149,8 +142,8 @@ static char const *const output_fields[] = {
         "id",
         "channel",
         "battery_ok",
-        "startup",
         "alarm",
+        "startup",
         NULL,
 };
 
