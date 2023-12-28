353c7fd0ff466a15595296093b71e83b7bcf245b
GusGorman <gusgorman402@gmail.com>
2023-12-27 22:35:49 -0600

    Update honda_kr5vxx.c
    Added more decoder info to comments

--- src/devices/honda_kr5vxx.c
+++ src/devices/honda_kr5vxx.c
@@ -17,6 +17,21 @@
 	Frequency 313.55 MHz or 314.15 MHz
 
 	Signal is 2FSK, 15 kHz deviation, datarate(baud) 16.66 kbps
+ 	Device uses Manchester encoded pulses of 60 us and 120 us
+  	Data packet starts with sync of 0xFFFFFFFFFFF
+  	Data layout after sync:
+           MMMMMM HH DDDDDDDD EE NNNNNN RRRRRRRR CC
+
+ 	- M: 24 bit Manufacturer ID
+  	- H: 8 bit indicator of packet number (keyfob button press sends packet 2 times, reciever requires both packets. 0x08 is first packet, 0x0a is second packet)
+   	- D: 32 bit Device ID of keyfob
+    	- E: 8 bit Keyfob command (event)
+     	- N: 24 bit counter
+      	- R: 32 bit Rolling Code
+       	- C: 8 bit CRC, OPENSAFETY poly 0x2f init 0x00
+
+ 	Flex decoder: rtl_433 -f 433657000 -R 0 -X 'n=honda,m=FSK_MC_ZEROBIT,s=60,l=120,r=75000,preamble={32}0xffffec0f'
+  
 */
 
 
@@ -27,7 +42,7 @@ static int honda_decode(r_device *decoder, bitbuffer_t *bitbuffer){
 
 	if (bitbuffer->num_rows > 1){ return DECODE_ABORT_EARLY; } //should only be 1 row
 
-	if( bitbuffer->bits_per_row[0] < 150 || bitbuffer->bits_per_row[0] > 184 ){ return DECODE_ABORT_EARLY; } //honda signals are usually 182 bits
+	if( bitbuffer->bits_per_row[0] < 150 || bitbuffer->bits_per_row[0] > 184 ){ return DECODE_ABORT_EARLY; }
 
 	uint16_t bit_offset;
         uint8_t const preamble[] = {0xEC, 0x0F, 0x62};  //honda keyfob manufacture code
@@ -37,7 +52,7 @@ static int honda_decode(r_device *decoder, bitbuffer_t *bitbuffer){
 
 
 	uint8_t b[16];
-	bitbuffer_extract_bytes( bitbuffer, 0, bit_offset + 16, b, 120); //extract 120 bits starting at position ~45, excluding first 2 bytes of manufacture code
+	bitbuffer_extract_bytes( bitbuffer, 0, bit_offset + 16, b, 120); //extract 120 bits, excluding first 2 bytes of manufacture code
 	
 	char const *event = "Unknown";
 	int device_id = ((unsigned)b[2] << 24) | (b[3] << 16) | (b[4] << 8) | (b[5]);
@@ -98,7 +113,7 @@ r_device const honda_keyfob = {
 	.name        = "Honda Keyfob",
 	.modulation  = FSK_PULSE_MANCHESTER_ZEROBIT,
 	.short_width = 60,
-	.long_width  = 124,
+	.long_width  = 120,
 	.gap_limit   = 1000,  //this gap is kinda irrelevant
 	.reset_limit = 75000,
 	.decode_fn   = &honda_decode,
