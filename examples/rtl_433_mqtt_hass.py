fe439627fec94ad74cd5b3d0806940945b898604
Ian Evans <ian@techne.net>
2024-01-22 13:40:42 -0800

    Fix rtl_433_mqtt_hass.py rain_in value template (#2801)

--- examples/rtl_433_mqtt_hass.py
+++ examples/rtl_433_mqtt_hass.py
@@ -434,8 +434,8 @@ mappings = {
         "config": {
             "device_class": "precipitation",
             "name": "Rain Total",
-            "unit_of_measurement": "mm",
-            "value_template": "{{ (float(value|float) * 25.4) | round(2) }}",
+            "unit_of_measurement": "in",
+            "value_template": "{{ value|float|round(2) }}",
             "state_class": "total_increasing"
         }
     },
