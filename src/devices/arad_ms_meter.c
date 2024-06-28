eef2d02dcca40728e2fd391b6f283da5a97c48d7
avicarmeli <32562196+avicarmeli@users.noreply.github.com>
2024-06-28 06:57:05 +0300

    Update arad_ms_meter.c

--- src/devices/arad_ms_meter.c
+++ src/devices/arad_ms_meter.c
@@ -3,7 +3,7 @@
     Arad/Master Meter Dialog3G water utility meter.
     
     Copyright (C) 2022 avicarmeli
-    Additional credits to participants of THE-THREAD
+    
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
