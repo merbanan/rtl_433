2b8693ea24da35eb26897ab1690754e74a137630
Christian W. Zuckschwerdt <christian@zuckschwerdt.org>
2024-07-14 15:44:52 +0200

    minor: Remove Oregon-THGR810 humidity limits (closes #2996)

--- src/devices/oregon_scientific.c
+++ src/devices/oregon_scientific.c
@@ -657,7 +657,7 @@ static int oregon_scientific_v3_decode(r_device *decoder, bitbuffer_t *bitbuffer
         float temp_c = get_os_temperature(msg);
         int humidity = get_os_humidity(msg);
         // Sanity check values
-        if (temp_c > 70 || temp_c < -50 || humidity < 0 || humidity > 98) {
+        if (temp_c > 70 || temp_c < -50) {
             decoder_logf(decoder, 1, __func__, "THGR810 Message failed values sanity check: temperature_C %.1fC humidity %d%%.", temp_c, humidity);
             return DECODE_FAIL_SANITY;
         }
@@ -772,12 +772,12 @@ static int oregon_scientific_v3_decode(r_device *decoder, bitbuffer_t *bitbuffer
             return DECODE_FAIL_SANITY;
         }
 
-        float gustWindspeed = (msg[5]&0x0f) /10.0F + ((msg[6]>>4)&0x0f) *1.0F + (msg[6]&0x0f) * 10.0F;
-        float avgWindspeed = ((msg[7]>>4)&0x0f) / 10.0F + (msg[7]&0x0f) *1.0F + ((msg[8]>>4)&0x0f) * 10.0F;
-        float quadrant = (0x0f&(msg[4]>>4))*22.5F;
+        float gustWindspeed = (msg[5] & 0x0f) / 10.0f + ((msg[6] >> 4) & 0x0f) * 1.0f + (msg[6] & 0x0f) * 10.0f;
+        float avgWindspeed  = ((msg[7] >> 4) & 0x0f) / 10.0f + (msg[7] & 0x0f) * 1.0f + ((msg[8] >> 4) & 0x0f) * 10.0f;
+        float quadrant      = ((msg[4] >> 4) & 0x0f) * 22.5f;
 
         // Sanity check values
-        if (gustWindspeed < 0 || gustWindspeed > 56 || avgWindspeed < 0 || avgWindspeed > 56 || quadrant < 0 || quadrant > 337.5) {
+        if (gustWindspeed < 0 || gustWindspeed > 56 || avgWindspeed < 0 || avgWindspeed > 56) {
             decoder_logf(decoder, 1, __func__, "WGR800 Message failed values sanity check: wind_max_m_s %.1f wind_avg_m_s %.1f wind_dir_deg %.1f.", gustWindspeed, avgWindspeed, quadrant);
             return DECODE_FAIL_SANITY;
         }
