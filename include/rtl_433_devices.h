f70358350ea4ebd4aab1c3a30aa54ce74c53640e
Joan Maso <joan.maso@uab.cat>
2024-07-28 09:00:59 +0200

    tab to spaces

--- include/rtl_433_devices.h
+++ include/rtl_433_devices.h
@@ -269,8 +269,8 @@
     DECL(thermopro_tp829b) \
     DECL(arad_ms_meter) \
     DECL(geevon) \
-	DECL(fineoffset_wh46) \
-	
+    DECL(fineoffset_wh46) \
+    
     /* Add new decoders here. */
 
 #define DECL(name) extern r_device name;
