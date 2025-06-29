188a13bd147b8c13edf30b1e2a35500caee4bc5c
ProfBoc75 <nice.burger@free.fr>
2025-06-29 11:50:30 +0200

    Improve digest checksum coding, Add low bat and button status

--- src/devices/thermopro_tx7b.c
+++ src/devices/thermopro_tx7b.c
@@ -30,13 +30,17 @@ Flex decoder:
 Data layout:
 
     Byte Position              0  1  2  3  4  5  6  7  8  9 10 11 12 13 14
-                              II CF 11 12 22 aa 55 aa CC TT TT TT TT TT TT
     Sample        d2 55 2d d4 e8 00 29 30 17 aa 55 aa 83 d2 d2 d2 d2 d2 00
     Sample        d2 55 2d d4 25 20 2c a0 0d aa 55 aa bb d2 d2 d2 d2 d2 00
+                              II BF 11 12 22 aa 55 aa CC TT TT TT TT TT TT
+                                 X
+                                 C
 
 - II:  {8} Sensor ID,
-- C:   {4} Channel 0x0 = 1, 0x1 = 2, 0x2 = 3 (3 sensors max are supported by station)
-- F:   {4} Unknown flags, always 0x0, to be confirmed as the battery low not identified,
+- B:   {1} Low Battery = 1, Normal Battery = 0
+- X:   {1} TX Button , 1 = pressed for immediate rf transmit.
+- C:   {2} Channel offset -1, 0x0 = CH 1, 0x1 = CH 2, 0x2 = CH 3 (3 sensors max are supported by station)
+- F:   {4} Unknown flags, always 0x0,
 - 111:{12} Temperature, °C, offset 400, scale 10,
 - 222:{12} Humidity, %,
 - aa55aa:{24} fixed value 0xaa55aa
@@ -78,11 +82,7 @@ static int thermopro_tx7b_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     bitbuffer_extract_bytes(bitbuffer, 0, offset, b, 9 * 8);
 
     // checksum is a Galois bit reflect and byte reflect, gen 0x98, key 0x25, final XOR 0x00
-    uint8_t b_reflect[8];
-    for (int p = 7; p != -1; p += -1)
-        b_reflect[7 - p] = b[p];
-
-    int checksum = lfsr_digest8(b_reflect, 8, 0x98, 0x25);
+    int checksum = lfsr_digest8_reverse(b, 8, 0x98, 0x25);
 
     if (checksum != b[8]) {
         decoder_logf(decoder, 1, __func__, "Checksum error, calculated %x, expected %x", checksum, b[8]);
@@ -92,7 +92,9 @@ static int thermopro_tx7b_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     decoder_log_bitrow(decoder, 2, __func__, b, 72, "MSG");
 
     int id        = b[0];
-    int channel   = ((b[1] & 0xF0) >> 4) + 1;
+    int channel   = ((b[1] & 0x30) >> 4) + 1;
+    int low_bat   = b[1] >> 7;
+    int tx_button = (b[1] & 0x40) >> 6;
     int flags     = b[1] & 0xF;
     int temp_raw  = b[2] << 4 | (b[3] & 0xF0) >> 4;
     int humidity  = b[4];
@@ -101,9 +103,11 @@ static int thermopro_tx7b_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     /* clang-format off */
     data_t *data = data_make(
             "model",          "",             DATA_STRING, "ThermoPro-TX7B",
-            "id",             "",             DATA_FORMAT, "%02x", DATA_INT,    id,
+            "id",             "",             DATA_FORMAT, "%02x",    DATA_INT,    id,
+            "battery_ok",     "Battery",      DATA_INT,    !low_bat,
+            "button",         "Button",       DATA_INT,    tx_button,
             "channel",        "Channel",      DATA_INT,    channel,
-            "flags",          "Flags",        DATA_FORMAT, "%04b", DATA_INT,    flags,
+            "flags",          "Flags",        DATA_FORMAT, "%04b",    DATA_INT,    flags,
             "temperature_C",  "Temperature",  DATA_FORMAT, "%.1f C",  DATA_DOUBLE, temp,
             "humidity",       "Humidity",     DATA_FORMAT, "%d %%",   DATA_INT, humidity,
             "mic",            "Integrity",    DATA_STRING, "CHECKSUM",
@@ -117,6 +121,8 @@ static int thermopro_tx7b_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 static char const *const tx7b_output_fields[] = {
         "model",
         "id",
+        "battery_ok",
+        "button",
         "channel",
         "flags",
         "temperature_C",
