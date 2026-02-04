3f78fb70992c63b5fe0e77add4954887c26875d1
ProfBoc75 <nice.burger@free.fr>
2026-02-04 18:14:49 +0100

    Correct counter and rain gauge data length of 16 bit and add battery_ok

--- src/devices/saintlogic_sa8.c
+++ src/devices/saintlogic_sa8.c
@@ -35,19 +35,22 @@ Data layout:
 
     Byte Position   0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40
     Sample         46 54 24 cd ab 26 0c d0 bd c3 75 39 e3 39 e3 e8 44 f3 00 6f 00 3d 00 00 00 00 00 b4 00 7e 00 41 00 53 00 00 00 f1 10 17 1d
-                   II II II II II II II II II II II II II II II CC CC CC CC TT TT HH 00 00 00 00 00 GG GG WW WW DD DD RR RR RR RR ?? ?? XX XX
+                   II II II II II II II II II II II II II II II CC CC ?? ?? TT TT HH 00 00 00 00 00 GG GG WW WW DD DD RR RR ?? ?? LL BB XX XX
 
 
 - II:{120} Fixed value, ID / MAC address of the Outdor Weather Station, to be confirmed
-- CC: {32} little endian LSB/MSB, Counter, +1 each message transmit
+- CC: {16} little endian LSB/MSB, Counter, +1 each message transmit
+- ??: {16} fixed value 0xf300
 - TT: {16} little endian LSB/MSB, signed value, Temp °C, scale 10
 - HH:  {8} Humidity %
 - 00: {40} Fixed value to 0
 - GG: {16} little endian LSB/MSB, Wind Gust in m/s, scale 100
 - WW: {16} little endian LSB/MSB, Wind Average in m/s, scale 100
 - DD: {16} little endian LSB/MSB, Wind Direction in °, 0 = North, 180 = South
-- RR: {32} little endian LSB/MSB, Rain Gauge in mm scale 0.15
-- ??: {16} Unknown value, probably battery information here ?
+- RR: {16} little endian LSB/MSB, Rain Gauge in mm scale 0.42893617f
+- ??: {16} little endian LSB/MSB, another unknown counter
+- LL:  {8} To be confirmed, probably battery level information here.
+- BB:  {8} Battery flags 0x10 = Battery OK, 0x01 = Battery KO or Missing
 - XX: {16} CRC or Checksum, not yet guessed
 
 */
@@ -86,14 +89,16 @@ static int saintlogic_sa8_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     // ID b[0] to b[14], 15 bytes
     char ID[15 * 2 + 1];
     bitrow_snprint(b, 15 * 8, ID, sizeof (ID));
-    // counter 18 15
-    uint32_t counter  = b[18] << 24 | b[17] << 16 | b[16] << 8 | b[15];
-    int temp_raw      = b[20] << 8  | b[19];
+    // counter 16 15
+    uint16_t counter  = b[16] << 8 | b[15];
+    int temp_raw      = b[20] << 8 | b[19];
     int humidity      = b[21];
-    int gust_raw      = b[28] << 8  | b[27];
-    int wind_raw      = b[30] << 8  | b[29];
-    int dir_degree    = b[32] << 8  | b[31];
-    uint32_t rain_raw = b[36] << 24 | b[35] << 16 | b[34] << 8 | b[33];
+    int gust_raw      = b[28] << 8 | b[27];
+    int wind_raw      = b[30] << 8 | b[29];
+    int dir_degree    = b[32] << 8 | b[31];
+    uint16_t rain_raw = b[34] << 8 | b[33];
+    uint16_t unknown  = b[36] << 8 | b[35]; // may be rain/h counter
+    int battery_ok    = (b[38] & 0x10) >> 4;
     int unknown_flags = b[37] << 8  | b[38]; // big endian until guessed
     //int unknown_chk   = b[39] << 8  | b[40]; // big endian until guessed
 
@@ -104,18 +109,19 @@ static int saintlogic_sa8_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 
     /* clang-format off */
     data_t *data = data_make(
-            "model",           "",                 DATA_STRING, "Saintlogic-SA8",
-            "id",              "",                 DATA_STRING, ID,
-            //"battery_ok",      "Battery_OK",       DATA_INT,    !battery_low,
-            "counter",         "Counter",          DATA_INT, counter,
-            "temperature_C",   "Temperature",      DATA_FORMAT, "%.1f C",    DATA_DOUBLE, temp_c,
-            "humidity",        "Humidity",         DATA_FORMAT, "%u %%",     DATA_INT,    humidity,
-            "wind_avg_km_h",    "Wind avg speed",  DATA_FORMAT, "%.1f km/h", DATA_DOUBLE, wind_km_h,
-            "wind_max_km_h",    "Wind max speed",  DATA_FORMAT, "%.1f km/h", DATA_DOUBLE, gust_km_h,
-            "wind_dir_deg",    "Wind Direction",   DATA_INT,    dir_degree,
-            "rain_mm",         "Total rainfall",   DATA_FORMAT, "%.1f mm",   DATA_DOUBLE, rain_mm,
-            "flags",           "Flags",            DATA_FORMAT, "%04x",      DATA_INT,    unknown_flags,
-            "mic",             "Integrity",        DATA_STRING, "NO CHECKSUM", //until formula is guessed
+            "model",           "",               DATA_STRING, "Saintlogic-SA8",
+            "id",              "",               DATA_STRING, ID,
+            "battery_ok",      "Battery_OK",     DATA_INT,    battery_ok,
+            "counter",         "Counter",        DATA_INT,    counter,
+            "temperature_C",   "Temperature",    DATA_FORMAT, "%.1f C",    DATA_DOUBLE, temp_c,
+            "humidity",        "Humidity",       DATA_FORMAT, "%u %%",     DATA_INT,    humidity,
+            "wind_avg_km_h",   "Wind avg speed", DATA_FORMAT, "%.1f km/h", DATA_DOUBLE, wind_km_h,
+            "wind_max_km_h",   "Wind max speed", DATA_FORMAT, "%.1f km/h", DATA_DOUBLE, gust_km_h,
+            "wind_dir_deg",    "Wind Direction", DATA_INT,    dir_degree,
+            "rain_mm",         "Total rainfall", DATA_FORMAT, "%.1f mm",   DATA_DOUBLE, rain_mm,
+            "unknown",         "Unknown",        DATA_FORMAT, "%04x",      DATA_INT,    unknown,
+            "flags",           "Flags",          DATA_FORMAT, "%04x",      DATA_INT,    unknown_flags,
+            "mic",             "Integrity",      DATA_STRING, "NO CHECKSUM", //until formula is guessed
             NULL);
     /* clang-format on */
 
@@ -126,7 +132,7 @@ static int saintlogic_sa8_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 static char const *const output_fields[] = {
         "model",
         "id",
-        //"battery_ok",
+        "battery_ok",
         "counter",
         "temperature_C",
         "humidity",
@@ -134,6 +140,8 @@ static char const *const output_fields[] = {
         "wind_max_m_s",
         "wind_dir_deg",
         "rain_mm",
+        "unknown",
+        "flags",
         "mic",
         NULL,
 };
