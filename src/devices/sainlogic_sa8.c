5644721e8e0424cadf36753386d8c5d071d0cbec
ProfBoc75 <nice.burger@free.fr>
2026-02-15 16:29:15 +0100

    Add CRC formula, rollback battery information, reduce ID size, update doc accordingly

--- src/devices/sainlogic_sa8.c
+++ src/devices/sainlogic_sa8.c
@@ -38,10 +38,14 @@ Data layout:
 
     Byte Position   0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40
     Sample         46 54 24 cd ab 26 0c d0 bd c3 75 39 e3 39 e3 e8 44 f3 00 6f 00 3d 00 00 00 00 00 b4 00 7e 00 41 00 53 00 00 00 f1 10 17 1d
-                   II II II II II II II II II II II II II II II CC CC ?? ?? TT TT HH 00 00 00 00 00 GG GG WW WW DD DD RR RR ?? ?? BB BB XX XX
+                   SS SS SS[II II II II II II ?? ?? ?? ?? ?? ?? CC CC ?? ?? TT TT HH 00 00 00 00 00 GG GG WW WW DD DD RR RR ?? ?? BB BB]XX XX
 
 
-- II:{120} Fixed value, ID / MAC address of the Outdor Weather Station, to be confirmed
+- SS: {24} Fixed value 0x465424, synchro word, not part of the CRC16.
+- II: {48} Fixed value, ID / MAC address of the Outdor Weather Station, to be confirmed
+- ??: {16} fixed value, 0xc375
+- ??: {16} fixed value, 0x39e3
+- ??: {16} fixed value, 0x39e3, repeated value above
 - CC: {16} little endian LSB/MSB, Counter, +1 each message transmit
 - ??: {16} fixed value 0xf300
 - TT: {16} little endian LSB/MSB, signed value, Temp °C, scale 10
@@ -53,7 +57,7 @@ Data layout:
 - RR: {16} little endian LSB/MSB, Rain Gauge in mm scale 0.42893617f
 - ??: {16} little endian LSB/MSB, another unknown counter
 - BB: {16} little endian LSB/MSB, looks battery level in mV. From first byte, battery flags 0x10 = battery OK, 0x01 = battery KO or missing
-- XX: {16} CRC or Checksum, not yet guessed
+- XX: {16} little endian LSB/MSB, CRC 16 of [previous bytes except 3 first ones], poly 0x8005, init 0xffff, XOROUT 0x0000
 
 */
 
@@ -87,23 +91,24 @@ static int sainlogic_sa8_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     decoder_log_bitrow(decoder, 1, __func__, b, sizeof (b) * 8, "UART decoded MSG");
 
     // crc checksum here when guessed
+    uint16_t crc_calculated = crc16(&b[3], 36, 0x8005, 0xffff);
+    if ( crc_calculated != (b[40] << 8 | b[39])) {
+        decoder_log(decoder, 2, __func__, "CRC error");
+    }
 
     // ID b[0] to b[14], 15 bytes
-    char ID[15 * 2 + 1];
-    bitrow_snprint(b, 15 * 8, ID, sizeof (ID));
-    // counter 16 15
-    uint16_t counter  = b[16] << 8 | b[15];
-    int temp_raw      = b[20] << 8 | b[19];
-    int humidity      = b[21];
-    int gust_raw      = b[28] << 8 | b[27];
-    int wind_raw      = b[30] << 8 | b[29];
-    int dir_degree    = b[32] << 8 | b[31];
-    uint16_t rain_raw = b[34] << 8 | b[33];
-    uint16_t unknown  = b[36] << 8 | b[35]; // may be rain/h counter
-    int battery_ok    = (b[38] & 0x10) >> 4;
-    uint16_t bat_mv   = b[38] << 8 | b[37];
-    //int unknown_flags = b[37] << 8  | b[38]; // big endian until guessed
-    //int unknown_chk   = b[39] << 8  | b[40]; // big endian until guessed
+    char ID[6 * 2 + 1];
+    snprintf(ID, sizeof(ID), "%02x%02x%02x%02x%02x%02x", b[4], b[3], b[6], b[5], b[8], b[7]);
+    uint16_t counter   = b[16] << 8 | b[15];
+    int16_t temp_raw   = b[20] << 8 | b[19];
+    int humidity       = b[21];
+    int gust_raw       = b[28] << 8 | b[27];
+    int wind_raw       = b[30] << 8 | b[29];
+    int dir_degree     = b[32] << 8 | b[31];
+    uint16_t rain_raw  = b[34] << 8 | b[33];
+    uint16_t unknown   = b[36] << 8 | b[35]; // may be rain/h counter
+    int battery_ok     = (b[38] & 0x10) >> 4;
+    uint16_t bat_mv    = b[38] << 8 | b[37]; // bat level not confirmed, kept as battery flags
 
     float temp_c      = temp_raw * 0.1f;
     float gust_km_h   = gust_raw * 0.036f; // orignal value is m/s but for Customary conversion, km/h is better
@@ -115,7 +120,7 @@ static int sainlogic_sa8_decode(r_device *decoder, bitbuffer_t *bitbuffer)
             "model",           "",               DATA_STRING, "Sainlogic-SA8",
             "id",              "",               DATA_STRING, ID,
             "battery_ok",      "Battery_OK",     DATA_INT,    battery_ok,
-            "battery_mV",      "Battery Voltage",DATA_FORMAT, "%u mV",     DATA_INT,    bat_mv,
+            //"battery_mV",      "Battery Voltage",DATA_FORMAT, "%u mV",     DATA_INT,    bat_mv, // not confirmed
             "counter",         "Counter",        DATA_INT,    counter,
             "temperature_C",   "Temperature",    DATA_FORMAT, "%.1f C",    DATA_DOUBLE, temp_c,
             "humidity",        "Humidity",       DATA_FORMAT, "%u %%",     DATA_INT,    humidity,
@@ -124,8 +129,8 @@ static int sainlogic_sa8_decode(r_device *decoder, bitbuffer_t *bitbuffer)
             "wind_dir_deg",    "Wind Direction", DATA_INT,    dir_degree,
             "rain_mm",         "Total rainfall", DATA_FORMAT, "%.1f mm",   DATA_DOUBLE, rain_mm,
             "unknown",         "Unknown",        DATA_FORMAT, "%04x",      DATA_INT,    unknown,
-            //"flags",           "Flags",          DATA_FORMAT, "%04x",      DATA_INT,    unknown_flags,
-            "mic",             "Integrity",      DATA_STRING, "NO CHECKSUM", //until formula is guessed
+            "flags",           "Flags",          DATA_FORMAT, "%04x",      DATA_INT,    bat_mv,
+            "mic",             "Integrity",      DATA_STRING, "CRC",
             NULL);
     /* clang-format on */
 
@@ -146,7 +151,7 @@ static char const *const output_fields[] = {
         "wind_dir_deg",
         "rain_mm",
         "unknown",
-        //"flags",
+        "flags",
         "mic",
         NULL,
 };
@@ -158,6 +163,5 @@ r_device const sainlogic_sa8 = {
         .long_width  = 200,
         .reset_limit = 2500,
         .decode_fn   = &sainlogic_sa8_decode,
-        .disabled    = 1, // no MIC, disabled by default
         .fields      = output_fields,
 };
