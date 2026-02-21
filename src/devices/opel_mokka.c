4eb141acbc2c85fe85657fd3a8d3e452897d34f4
Vidar Madsen <vidarino@gmail.com>
2026-02-21 14:39:40 +0100

    Description of format and some formatting fixes

--- src/devices/opel_mokka.c
+++ src/devices/opel_mokka.c
@@ -1,6 +1,8 @@
 /** @file
     Opel Mokka Car Key.
 
+    Copyright (C) 2026 Vidar Madsen
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
@@ -15,9 +17,20 @@ very well pick up other compatible brands as well.
 
 Only extracts key id and event type. There's no decryption of payload.
 
+Each frame looks like this, after a preamble of 88 zeros:
+1 10000110010 11010 0000010100011111101001100000010100101011001000011100100101010110 1 000000000000000 1 10000110010 11010 0000010100011111101001100000010100101011001000011100100101010110 1
+S iiiiiiiiiii ttttt cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc E ............... S iiiiiiiiiii ttttt cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc E
+
+S =  1 start bit
+i = 11 bits, key ID
+t =  5 bits, packet type
+c = 64 bits, encrypted payload
+E =  1 end bit
+
 Event type is the same for both lock and unlock (26), so the actual user
 action is unknown. The key fob periodically sends a zero-filled packet
-with a different type (3) as well. Can be used for presence tracking, maybe.
+with a different type (3) as well, possibly as a proximity signal to the
+vehicle.
 */
 
 #include "decoder.h"
@@ -45,20 +58,19 @@ static int opel_mokka_callback(r_device *decoder, bitbuffer_t *bitbuffer)
         }
 
         bitbuffer_extract_bytes(bitbuffer, i, 90, temp_id, 11);
-        key_id = ((temp_id[0] << 8) | temp_id[1]) >> 5;
+        key_id = (temp_id[0] << 3) | (temp_id[1] >> 5);
 
         // Payload is sent twice, so verify that ids match
         bitbuffer_extract_bytes(bitbuffer, i, 90 + 12*8 + 1, temp_id, 11);
-        int check_id = ((temp_id[0] << 8) | temp_id[1]) >> 5;
+        int check_id = (temp_id[0] << 3) | (temp_id[1] >> 5);
         if (key_id != check_id) {
             continue;
         }
 
-        event_type = (((bytes[12] & 0x07) << 8) | (bytes[13] & 0xc0)) >> 6;
+        event_type = ((bytes[12] & 0x07) << 2) | (bytes[13] & 0xc0) >> 6;
 
         bitbuffer_extract_bytes(bitbuffer, i, 90 + 17, code, 64);
-        snprintf(code_str, sizeof(code_str), "%02x%02x%02x%02x%02x%02x%02x%02x",
-            code[0], code[1], code[2], code[3], code[4], code[5], code[6], code[7]);
+        bitrow_snprint(code, 8 * 8, code_str, sizeof(code_str));
 
         /* clang-format off */
         data = data_make(
@@ -76,19 +88,19 @@ static int opel_mokka_callback(r_device *decoder, bitbuffer_t *bitbuffer)
 }
 
 static char const *const output_fields[] = {
-    "model",
-    "id",
-    "type",
-    "code",
-    NULL,
+        "model",
+        "id",
+        "type",
+        "code",
+        NULL,
 };
 
 r_device const opel_mokka = {
-    .name        = "Opel Mokka Car Key",
-    .modulation  = FSK_PULSE_MANCHESTER_ZEROBIT,
-    .short_width = 100,
-    .long_width  = 100,
-    .reset_limit = 1000,
-    .decode_fn   = &opel_mokka_callback,
-    .fields      = output_fields,
+        .name        = "Opel Mokka Car Key",
+        .modulation  = FSK_PULSE_MANCHESTER_ZEROBIT,
+        .short_width = 100,
+        .long_width  = 100,
+        .reset_limit = 1000,
+        .decode_fn   = &opel_mokka_callback,
+        .fields      = output_fields,
 };
