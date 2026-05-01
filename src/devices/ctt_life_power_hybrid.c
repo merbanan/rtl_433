e03ef96a01968f6cbf0a77d90fa631c3fb80059f
Jonathan Caicedo <jonathan@jcaicedo.com>
2026-04-30 21:37:40 -0400

    fix(ctt): review feedback

--- src/devices/ctt_life_power_hybrid.c
+++ src/devices/ctt_life_power_hybrid.c
@@ -9,7 +9,7 @@
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
 */
-/**
+/** @fn static int ctt_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 Cellular Tracking Technologies (https://celltracktech.com/) LifeTag/PowerTag/HybridTag.
 
 The CTT LifeTag/PowerTag/HybridTag is a lightweight transmitter used for wildlife tracking and research - most commonly used with the Motus Wildlife Tracking System (https://motus.org/).
@@ -17,10 +17,10 @@ The tags transmit a unique identifier (ID) at a fixed bitrate of 25 kbps using F
 
 The packet format consists of:
 
-    • PREAMBLE: 24 bits of alternating 1/0 (0xAA if byte-aligned) for receiver bit-clock sync (preamble length can be shorter, depending on hardware)
-    • SYNC:     2 bytes fixed pattern 0xD3, 0x91 marking the packet start
-    • ID:       20-bit tag ID encoded into 4 bytes (5 bits per byte) using a 32-entry dictionary
-    • CRC:      1-byte SMBus CRC-8 over the 4 encoded ID bytes
+- PREAMBLE: 24 bits of alternating 1/0 (0xAA if byte-aligned) for receiver bit-clock sync (preamble length can be shorter, depending on hardware)
+- SYNC:     2 bytes fixed pattern 0xD3, 0x91 marking the packet start
+- ID:       20-bit tag ID encoded into 4 bytes (5 bits per byte) using a 32-entry dictionary
+- CRC:      1-byte SMBus CRC-8 over the 4 encoded ID bytes
 
     AA AA AA   D3 91   78 55 4C 33   58
    |--------| |-----| |-----------| |--|
@@ -28,9 +28,9 @@ The packet format consists of:
 
     A beep is a single packet.
 
-    LifeTag - programmed with a standard 5-second beep rate.
-    PowerTag - user-defined beep rate
-    HybridTag - transmits a beep every 2-15 seconds
+- LifeTag: programmed with a standard 5-second beep rate.
+- PowerTag: user-defined beep rate.
+- HybridTag: transmits a beep every 2-15 seconds.
 
 There's a 20-bit subset of the 32-bit ID space set aside for Motus tag use. We set `valid_motus` to true if all 4 bytes of the ID are present in the Motus code dictionary.
 However, `valid_motus` not being set doesn't mean that a tag is invalid, just that it's not recognized as a tag used with Motus.
@@ -59,7 +59,6 @@ static int byte_in_motus_code(uint8_t b)
 
 static int ctt_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 {
-    data_t *data;
     int events = 0;
     int saw_bad_crc = 0;
 
@@ -109,7 +108,7 @@ static int ctt_decode(r_device *decoder, bitbuffer_t *bitbuffer)
                 byte_in_motus_code(payload[3]);
 
         /* clang-format off */
-        data = data_make(
+        data_t *data = data_make(
             "model",       "",                        DATA_STRING, "CTT-Tag",
             "id",          "Tag ID",                  DATA_FORMAT, "0x%08X", DATA_INT, id,
             "valid_motus", "Valid Motus tag",         DATA_INT, motus_tag,
