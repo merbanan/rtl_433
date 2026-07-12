5292ef07fe1cdaa22b961f701842cb9c7699804f
David Todd <hdtodd@gmail.com>
2026-07-12 15:13:38 -0400

    Reformat code to standards

--- src/devices/omni.c
+++ src/devices/omni.c
@@ -217,6 +217,7 @@ static int omni_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 static char const *const output_fields[] = {
         "model",
         "fmt",
+	"channel",
         "id",
         "temperature_C",
         "temperature_2_C",
