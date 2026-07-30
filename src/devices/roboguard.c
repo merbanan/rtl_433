cba508cd367ae2cb956359f48756dee33dc97d81
Cale-Torino <cale@techrad.co.za>
2026-07-31 00:19:27 +0200

    Added filtering for preamble and combined into 1 row (216 bits)

--- src/devices/roboguard.c
+++ src/devices/roboguard.c
@@ -58,13 +58,11 @@ static int roboguard_ibis_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 {
     // Initialize vars here
     data_t *data;
-    uint8_t b[24]; // 24 for 192 bits, 27 for 216 bits
+    uint8_t b[27]; // 24 for 192 bits, 27 for 216 bits
     uint8_t const *type_str = "";
     uint8_t filter_count    = 0;
     uint8_t filter_error    = 0;
 
-    bitbuffer_invert(bitbuffer);    // This device sends data inverted relative to
-                                    // The OOK_PWM decoder output.
 
     if (bitbuffer->num_rows != 1)   // Only one message per transmission
     {
@@ -72,37 +70,47 @@ static int roboguard_ibis_decode(r_device *decoder, bitbuffer_t *bitbuffer)
     }
 
     // Check correct data length
-    // 72/8 = 9
     // 8x27 = 216 bits
     // first 24 bits = preamble
     // 216 bits with preamble, 192 bits without
     // 216-24 = 192 bits
-    if (bitbuffer->bits_per_row[0] != 192) // 216 bits = 27 bytes,192 bits = 24 bytes 8bits = 1byte
+    if (bitbuffer->bits_per_row[0] != 216) // 216 bits = 27 bytes
     {
         return DECODE_ABORT_LENGTH;
     }
 
+    bitbuffer_invert(bitbuffer);    // This device sends data inverted relative to
+                                    // The OOK_PWM decoder output.
+
     bitbuffer_extract_bytes(bitbuffer, 0, 0, b, sizeof(b) * 8);
 
+    // ffffff = 111111111111111111111111 24 high bits but inverted 000000 = 000000000000000000000000 24 low bits
+    // We first check the preambe first 3 bytes 24 bits make sure they are all 0
+    // p0:0, p1:0, p2:0
+    if (b[0] != 0x00 || b[1] != 0x00 || b[2] != 0x00) // Preamble check
+    {
+        return DECODE_FAIL_MIC; // Wrong preamble or noise
+    }
+
     // Signal repeats a transmission 8x so we check for 8 matches excluding the preamble, reduces error.
-    // If any of the 8 don't match then reject
+    // If 4 of the 8 don't match then reject
     // If (b[3] != b[6] || b[9] != b[12] || b[15] != b[18] || b[21] != b[24])
-    // If none of the repeating signals match we know its an error
-    for (uint8_t i = 0; i < 22; i += 3) {
-        if (b[i + filter_count] != b[i + 3 + filter_count]) // 0 3, 6 9, 12 15, 18 21
+    // If none of the repeating signals match we know its 100% an error
+    for (uint8_t i = 3; i < 24; i += 3) {
+        if (b[i + filter_count] != b[i + 3 + filter_count]) // 3 6, 9 12, 15 18, 21 24
         {
             filter_error++;
             if (filter_error > 3) // 4
             {
-                return DECODE_FAIL_MIC; // false positive
+                return DECODE_FAIL_MIC; // If more than 4 don't match. Can be fine tuned
             }
         }
-        filter_count + 3; // 0, 3, 6, 9, 12 15, 18 21
+        filter_count + 3; // 3 6, 9 12, 15 18, 21 24
     }
 
-    //
     // ffffff32bd0832bd0832bd0832bd0832bd0832bd0832bd0832bd08
-    // ffffff 32bd08 32bd083 2bd083 2bd083 2bd083 2bd083 2bd083 2bd08
+    // 000000cd42f7cd42f7cd42f7cd42f7cd42f7cd42f7cd42f7cd42f7 when inverted
+    // 000000 cd42f7 cd42f7 cd42f7 cd42f7 cd42f7 cd42f7 cd42f7 cd42f7
     // 110110100100 = 1100
     // Getting data from the second repeat b[3]     b[4]     b[5]
     int32_t id       = ((b[3] & 0x0F) << 16) | (b[4] << 8) | b[5]; // Combine 3 bytes to get device_id
